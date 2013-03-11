04a9d3b0f35152c4e833d7d6a3f9133ae2b1b50c
James Parkhurst <james.parkhurst@diamond.ac.uk>
2013-03-11 16:46:46 +0000

    Fixed beam_factory redefinition and print beam crash.

--- model/beam.py
+++ model/beam.py
@@ -25,12 +25,11 @@ class beam_factory:
         pass
 
     @staticmethod
-    def make_beam(s0):
-        return Beam(tuple(map(float, s0)))
-
-    @staticmethod
-    def make_beam(direction, wavelength):
-        return Beam(tuple(map(float, direction)), float(wavelength))
+    def make_beam(direction, wavelength=None):
+        if wavelength == None:
+            return Beam(tuple(map(float, direction)))
+        else:
+            return Beam(tuple(map(float, direction)), float(wavelength))
 
     @staticmethod
     def make_polarized_beam(direction, wavelength, polarization, polarization_fraction):
