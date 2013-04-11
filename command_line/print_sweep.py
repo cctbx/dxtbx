eed7d580a0b981c4fda5ab485e2c31945100c92b
Graeme Winter <graeme.winter@gmail.com>
2013-04-11 12:55:57 +0000

    Updated syntax following changes to sweep factory name

--- command_line/print_sweep.py
+++ command_line/print_sweep.py
@@ -4,9 +4,9 @@ from __future__ import print_function
 
 def print_sweep(list_of_images):
 
-    from dxtbx.sweep import sweep_factory
+    from dxtbx.sweep import SweepFactory
 
-    s = sweep_factory.sweep(list_of_images)
+    s = SweepFactory.sweep(list_of_images)
 
     print(s.get_detector())
     print(s.get_beam())
