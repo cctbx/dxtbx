e2e7a2f68c6f065af1d38e6375dd825c627b9316
Nicholas Sauter <nksauter@lbl.gov>
2013-05-01 22:29:55 +0000

    clean clutter

--- format/FormatSMVRigaku.py
+++ format/FormatSMVRigaku.py
@@ -42,7 +42,7 @@ class FormatSMVRigaku(FormatSMV):
         detector_prefixes = header["DETECTOR_NAMES"].split()
         try:
             detector_number = int(header["DETECTOR_NUMBER"].strip())
-        except:  # intentional
+        except (KeyError, AttributeError, ValueError) as e:
             return False
 
         if detector_number != len(detector_prefixes):
