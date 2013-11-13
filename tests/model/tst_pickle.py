143a4beb632ddee69bf6f361191bbf59282ff3e5
James Parkhurst <james.parkhurst@diamond.ac.uk>
2013-11-13 18:16:29 +0000

    Added pickle support for both the panel and detector. References to panels in the detector list and hierarchy should still be consistent after unpickling.

--- tests/model/tst_pickle.py
+++ tests/model/tst_pickle.py
@@ -1,7 +1,7 @@
 from __future__ import division
 from __future__ import print_function
 from dxtbx.model import Beam, Goniometer
-from dxtbx.model import Panel, Detector, Scan
+from dxtbx.model import Panel, Detector, Scan, PanelGroup
 
 
 def pickle_then_unpickle(obj):
@@ -37,23 +37,30 @@ def tst_goniometer():
     print("OK")
 
 
-# def tst_panel():
-#    '''Test pickling the panel object.'''
-#    obj1 = Panel()
-#    obj1.set_frame((1, 0, 0), (0, 1, 0), (0, 0, 1))
-#    obj2 = pickle_then_unpickle(obj1)
-#    assert(obj1 == obj2)
-#    print "OK"
+def tst_panel():
+    """Test pickling the panel object."""
+    obj1 = Panel()
+    obj1.set_local_frame((1, 0, 0), (0, 1, 0), (0, 0, 1))
+    obj2 = pickle_then_unpickle(obj1)
+    assert obj1 == obj2
+    print("OK")
 
-# def tst_detector():
-#    '''Test pickling the detector object.'''
-#    p = Panel()
-#    p.set_frame((1, 0, 0), (0, 1, 0), (0, 0, 1))
-#    obj1 = Detector(p)
-#    obj1.set_frame((1, 0, 0), (0, 1, 0), (0, 0, 1))
-#    obj2 = pickle_then_unpickle(obj1)
-#    assert(obj1 == obj2)
-#    print "OK"
+
+def tst_detector():
+    """Test pickling the detector object."""
+    p = Panel()
+    p.set_local_frame((1, 0, 0), (0, 1, 0), (0, 0, 1))
+    obj1 = Detector(p)
+    root = obj1.hierarchy()
+    root.add_panel(obj1[0])
+    root.add_group()
+    obj2 = pickle_then_unpickle(obj1)
+    assert obj2.hierarchy()._container == obj2
+    assert obj2.hierarchy()[0] == obj2[0]
+    assert obj2.hierarchy()[0] in obj2
+    assert type(obj2.hierarchy()[1]) == PanelGroup
+    assert obj1 == obj2
+    print("OK")
 
 
 def tst_scan():
@@ -68,8 +75,8 @@ def run():
     """Run all the tests"""
     tst_beam()
     tst_goniometer()
-    #    tst_panel()
-    #    tst_detector()
+    tst_panel()
+    tst_detector()
     tst_scan()
 
 
