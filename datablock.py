3609133f3abfad019acaf9d41d1141851e8ae746
James Parkhurst <james.parkhurst@diamond.ac.uk>
2013-11-26 17:23:56 +0000

    Added a check when adding a file to a datablock whether a child format understands the file better than the current format. If it does, start a new datablock. This is to ensure that we don't get stuck on something like FormatCBF when we actually want a FormatCBFPilatus or something.

--- datablock.py
+++ datablock.py
@@ -155,12 +155,24 @@ class DataBlock(object):
         format class, otherwise an exception will be raised."""
         from os.path import abspath
 
-        # Check the image is not already here and can be understood
+        # Recursively check whether any of the children understand
+        # image_file, in which case they are preferred over the parent
+        # format.
+        def better_understood_by_child(format_class, image_file):
+            for child in format_class._children:
+                if child.understand(image_file):
+                    return True
+            return False
+
+        # Check the image is not already here and can be understood and is not
+        # better understood by a child format class
         filename = abspath(filename)
         if filename in self._images:
             raise RuntimeError("%s already in data block" % filename)
         if not self.understand(filename):
             raise RuntimeError("cannot understand file: %s" % filename)
+        if better_understood_by_child(self.format_class(), filename):
+            raise RuntimeError("%s is better understood by a child format" % filename)
 
         # Get the meta data
         if len(self._images) > 0:
@@ -286,12 +298,14 @@ class DataBlockFactory(object):
         """ Create a list of data blocks from a list of filenames. """
         datablock_list = []
         for f in filenames:
-            if verbose:
-                print("Loading file: %s" % f)
             try:
                 datablock_list[-1].append(f)
             except Exception:
+                if verbose:
+                    print("Starting datablock %d" % len(datablock_list))
                 datablock_list.append(DataBlockFactory.create_single([f]))
+            if verbose:
+                print("Loaded file: %s" % f)
         return datablock_list
 
     @staticmethod
