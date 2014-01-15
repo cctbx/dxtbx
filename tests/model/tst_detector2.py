3bc28c843a2fae9a5a796e7a010cc0f4f390f1b6
James Parkhurst <james.parkhurst@diamond.ac.uk>
2014-01-15 16:54:09 +0000

    Trying to tidy up split model

--- tests/model/tst_detector2.py
+++ tests/model/tst_detector2.py
@@ -1,7 +1,9 @@
 from __future__ import division
 from __future__ import print_function
-from dxtbx.model import HierarchicalDetector, PanelTreeNode, VirtualPanel
-from dxtbx.model import PanelGroup
+from dxtbx.model import HierarchicalDetector, VirtualPanel
+from dxtbx.model import PanelGroup, MakePanelTreeNodeClass
+
+PanelTreeNode = MakePanelTreeNodeClass(VirtualPanel)
 
 
 class TestPanelTreeNode(object):
