3981bcc3ccd1df244f58b7aad7371e277889a9c9
Nathaniel Echols <nathaniel.echols@gmail.com>
2014-03-28 21:09:45 +0000

    clutter policing

--- datablock.py
+++ datablock.py
@@ -595,7 +595,7 @@ class DataBlockImageSetImporter(object):
         for imageset in imagesets:
             try:
                 self.datablocks[-1].append(imageset)
-            except:
+            except Exception:
                 self.datablocks.append(DataBlock([imageset]))
 
 
