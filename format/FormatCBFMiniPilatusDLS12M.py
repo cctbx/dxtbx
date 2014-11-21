5e3ddbd5aa70c5f3004d051b264972099791a480
Graeme Winter <graeme.winter@gmail.com>
2014-11-21 09:27:07 +0000

    Putative fix to correctly interpret detector distance

--- format/FormatCBFMiniPilatusDLS12M.py
+++ format/FormatCBFMiniPilatusDLS12M.py
@@ -87,10 +87,6 @@ class FormatCBFMiniPilatusDLS12M(FormatCBFMiniPilatus):
         from scitbx import matrix
         import math
 
-        detector = HierarchicalDetector()
-        root = detector.hierarchy()
-        root.set_frame((1, 0, 0), (0, 1, 0), (0, 0, -250))
-
         x = matrix.col((1, 0, 0))
         y = matrix.col((0, 1, 0))
         z = matrix.col((0, 0, 1))
@@ -99,13 +95,17 @@ class FormatCBFMiniPilatusDLS12M(FormatCBFMiniPilatus):
         ideal_beam_y = 2594
         beam_shift_y = 0.172 * (2594 - 2587)
 
-        distance = float(self._cif_header_dictionary["Detector_distance"].split()[0])
-
-        # FIXME verify DISTANCE used sensibly...
+        distance = (
+            float(self._cif_header_dictionary["Detector_distance"].split()[0]) * 1000.0
+        )
 
         wavelength = float(self._cif_header_dictionary["Wavelength"].split()[0])
 
         thickness = float(self._cif_header_dictionary["Silicon"].split()[2]) * 1000.0
+
+        detector = HierarchicalDetector()
+        root = detector.hierarchy()
+        root.set_frame((1, 0, 0), (0, 1, 0), (0, 0, -(250 + distance)))
 
         from cctbx.eltbx import attenuation_coefficient
 
