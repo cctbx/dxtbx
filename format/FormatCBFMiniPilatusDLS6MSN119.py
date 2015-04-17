d4c3d4beed47e708a4085d66889439d4340281a9
Graeme Winter <graeme.winter@gmail.com>
2015-04-17 16:38:27 +0000

    Remove unused imports

--- format/FormatCBFMiniPilatusDLS6MSN119.py
+++ format/FormatCBFMiniPilatusDLS6MSN119.py
@@ -12,8 +12,6 @@ from __future__ import division
 from __future__ import print_function
 
 from dxtbx.format.FormatCBFMiniPilatus import FormatCBFMiniPilatus
-from dxtbx.model import ParallaxCorrectedPxMmStrategy
-from dxtbx.format.FormatPilatusHelpers import determine_pilatus_mask
 
 import os
 
