abde3a6b25f5ef5c28f9ef3c7068e2d0d8f4660a
Graeme Winter <graeme.winter@gmail.com>
2015-05-27 20:37:15 +0000

    Document bare except; should replace with proper except once Exception to trap identified will clean up

--- format/FormatCBFFullPilatus.py
+++ format/FormatCBFFullPilatus.py
@@ -86,7 +86,7 @@ class FormatCBFFullPilatus(FormatCBFFull):
                 # a header only CBF file will not have a beam object
                 beam = self._beam()
 
-            except:
+            except:  # no beam; please update to correct exception
                 pass
 
             if beam:
