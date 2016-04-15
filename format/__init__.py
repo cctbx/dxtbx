5e3b6a3ccd72f99fa2e0a38e5ee20f22d63099f8
Richard Gildea <richard.gildea@diamond.ac.uk>
2016-04-15 12:42:52 +0000

    portable way of finding base/lib directory

--- format/__init__.py
+++ format/__init__.py
@@ -4,9 +4,7 @@ from __future__ import division
 def setup_hdf5_plugin_path():
     import os, libtbx.load_env
 
-    plugin_path = os.path.abspath(
-        os.path.join(abs(libtbx.env.python_exe), "..", "..", "lib")
-    )
+    plugin_path = libtbx.env.under_base("lib")
     try:
         os.environ["HDF5_PLUGIN_PATH"] = (
             os.environ["HDF5_PLUGIN_PATH"] + ":" + plugin_path
