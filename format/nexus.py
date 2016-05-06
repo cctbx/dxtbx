688e95dca8eeeec9d193d1a0adf6eed9162e8b96
James Parkhurst <james.parkhurst@diamond.ac.uk>
2016-05-06 15:50:57 +0000

    Added epochs and exposure times to eiger format

--- format/nexus.py
+++ format/nexus.py
@@ -953,8 +953,9 @@ class ScanFactory(object):
 
     """
 
-    def __init__(self, obj):
+    def __init__(self, obj, detector_obj):
         from dxtbx.model import Scan
+        from scitbx.array_family import flex
 
         # Get the image and oscillation range
         phi = obj.handle.file[obj.handle["depends_on"][()]]
@@ -964,8 +965,20 @@ class ScanFactory(object):
         else:
             oscillation = (float(phi[0]), 0.0)
 
+        # Get the exposure time
+        num_images = len(phi)
+        if "frame_time" in detector_obj.handle:
+            frame_time = float(detector_obj.handle["frame_time"][()])
+            exposure_time = flex.double(num_images, frame_time)
+            epochs = flex.double(num_images)
+            for i in range(1, len(epochs)):
+                epochs[i] = epochs[i - 1] + exposure_time[i - 1]
+        else:
+            exposure_time = flex.double(num_images, 0)
+            epochs = flex.double(num_images, 0)
+
         # Construct the model
-        self.model = Scan(image_range, oscillation)
+        self.model = Scan(image_range, oscillation, exposure_time, epochs)
 
 
 class CrystalFactory(object):
