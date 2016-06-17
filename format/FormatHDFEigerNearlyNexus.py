8569232dfc8e6517716a08cdf8c93e3b383bc41f
David Waterman <dgwaterman@gmail.com>
2016-06-17 13:27:55 +0000

    increase specificity for EIGER detectors

--- format/FormatHDFEigerNearlyNexus.py
+++ format/FormatHDFEigerNearlyNexus.py
@@ -42,7 +42,7 @@ def find_entries(nx_file):
 
 def is_eiger_nearly_nexus_file(filename):
     """
-    A hacky function to check if this is a nexus file
+    A hacky function to check if this is an EIGER-flavoured nexus file
 
     """
     import h5py
@@ -53,8 +53,12 @@ def is_eiger_nearly_nexus_file(filename):
     # Find the NXmx entries
     entry = find_entries(handle)
     if entry is not None:
-        if "instrument" in entry:
-            return True
+        try:
+            return (
+                "Dectris Eiger" in entry["instrument"]["detector"]["description"].value
+            )
+        except KeyError:
+            pass
     return False
 
 
