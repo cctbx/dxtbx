4bcd552d3c5a0a9469d9b704fa2ba2e65c9f075e
Graeme Winter <graeme.winter@gmail.com>
2016-08-02 06:50:36 +0000

    A dxtbx debugging tool to aid memory management debugging; dev.dxtbx.debug_memory; used as: Graemes-MacBook-Pro-4:tmp graeme$ dev.dxtbx.debug_memory junk_0001.cbf 1 51515392 2 51527680 4 51531776 8 51531776 16 51535872 32 51683328 64 51822592 i.e. prints resident stack size after that many dxtbx loads of that file @NKS @ASB maybe useful for you

---
+++ command_line/debug_memory.py
@@ -0,0 +1,22 @@
+# LIBTBX_SET_DISPATCHER_NAME dev.dxtbx.debug_memory
+from __future__ import division
+from __future__ import print_function
+
+import resource
+import dxtbx
+import sys
+
+
+def main():
+    frame = sys.argv[1]
+    powers = [2 ** n for n in range(20)]
+
+    for j in range(powers[-1] + 1):
+        i = dxtbx.load(frame)
+        mem = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+        if j in powers:
+            print(j, mem)
+
+
+if __name__ == "__main__":
+    main()
