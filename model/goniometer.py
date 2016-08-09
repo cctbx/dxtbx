dcf8f53c61052f783ec49f000dd1fa3f117c3245
Richard Gildea <richard.gildea@diamond.ac.uk>
2016-08-09 16:04:00 +0000

    cleaning clutter broke the tests :(

--- model/goniometer.py
+++ model/goniometer.py
@@ -14,6 +14,8 @@ from __future__ import division
 import pycbf
 from dxtbx_model_ext import Goniometer, KappaGoniometer, MultiAxisGoniometer
 
+import scitbx.math  # import dependency
+
 
 class goniometer_factory:
     """A factory class for goniometer objects, which will encapsulate
