352c133b5b6013e82dd3ca861581d3029a18d5eb
Aaron Brewster <asbrewster@lbl.gov>
2017-04-03 09:34:13 -0700

    Swap priority of HDF5 plugin path such that our HDF5 plugins take priority over the users.
    Leftover commit from a discussion last summer.

--- format/__init__.py
+++ format/__init__.py
@@ -7,7 +7,7 @@ def setup_hdf5_plugin_path():
     plugin_path = libtbx.env.under_base("lib")
     try:
         os.environ["HDF5_PLUGIN_PATH"] = (
-            os.environ["HDF5_PLUGIN_PATH"] + ":" + plugin_path
+            plugin_path + ":" + os.environ["HDF5_PLUGIN_PATH"]
         )
     except Exception:
         os.environ["HDF5_PLUGIN_PATH"] = plugin_path
