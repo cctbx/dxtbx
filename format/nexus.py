9852191da2b08478d79969b8f775a04e5cfacf79
Graeme Winter <graeme.winter@gmail.com>
2017-10-03 20:41:46 +0100

    Include offset from module offset in ... offset

--- format/nexus.py
+++ format/nexus.py
@@ -323,12 +323,17 @@ def construct_vector(nx_file, item, vector=None):
         units = nx_file[item].attrs["units"]
         ttype = nx_file[item].attrs["transformation_type"]
         vector = nx_file[item].attrs["vector"]
+        if "offset" in nx_file[item].attrs:
+            offset = nx_file[item].attrs["offset"]
+        else:
+            offset = vector * 0.0
         if ttype == "translation":
             value = convert_units(value, units, "mm")
             try:
                 vector = vector * value
             except ValueError as e:
                 vector = vector * value[0]
+            vector += offset
 
     else:
         pass
