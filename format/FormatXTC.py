99c8f6423ba968a9af2522bbdaea38704df9af56
Asmit Bhowmick <asmit3@gmail.com>
2017-12-08 15:55:58 -0800

    correcting for PR comments by Aaron:v2

--- format/FormatXTC.py
+++ format/FormatXTC.py
@@ -38,11 +38,9 @@ class FormatXTC(FormatMultiImage, FormatStill, Format):
             from psana import DataSource, DetNames
 
             try:
-                user_input = parse(file_name=image_file)
+                params = FormatXTC.params_from_phil(image_file)
             except Exception as e:
                 return False
-            working_phil = locator_scope.fetch(sources=[user_input])
-            params = working_phil.extract()
             if params.locator is None:
                 return False
             else:
@@ -85,7 +83,8 @@ class FormatXTC(FormatMultiImage, FormatStill, Format):
         except Exception as e:
             return False
 
-    def params_from_phil(self, image_file):
+    @staticmethod
+    def params_from_phil(image_file):
         try:
             user_input = parse(file_name=image_file)
             working_phil = locator_scope.fetch(sources=[user_input])
