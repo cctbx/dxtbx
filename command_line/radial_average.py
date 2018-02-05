70b2c5250072a21ecbf43e288a51161a2e9984ce
Aaron Brewster <asbrewster@lbl.gov>
2018-02-05 09:20:57 -0800

    Better check for bad values

--- command_line/radial_average.py
+++ command_line/radial_average.py
@@ -232,12 +232,17 @@ def run(args, image=None):
                 )
 
             # calculate standard devations
-            std_devs = [
-                math.sqrt((sums_sq[i] - sums[i] * results[i]) / counts[i])
-                if counts[i] > 0
-                else 0
-                for i in xrange(len(sums))
-            ]
+            stddev_sel = ((sums_sq - sums * results) >= 0) & (counts > 0)
+            std_devs = flex.double(len(sums), 0)
+            std_devs.set_selected(
+                stddev_sel,
+                (
+                    sums_sq.select(stddev_sel)
+                    - sums.select(stddev_sel) * results.select(stddev_sel)
+                )
+                / counts.select(stddev_sel).as_double(),
+            )
+            std_devs = flex.sqrt(std_devs)
 
             twotheta = (
                 flex.double(xrange(len(results))) * extent_two_theta / params.n_bins
