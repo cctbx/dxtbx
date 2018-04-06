2b5e5750f9eef64cff96baadc9d7488e48c31a0b
Richard Gildea <richard.gildea@diamond.ac.uk>
2018-04-06 14:34:20 +0100

    bug fix for h5 files: write template expected by xds

--- serialize/xds.py
+++ serialize/xds.py
@@ -12,10 +12,12 @@ from __future__ import print_function
 #  This code is distributed under the BSD license, a copy of which is
 #  included in the root directory of this package.
 
+import glob
 import sys
 from scitbx import matrix
 from rstbx.cftbx.coordinate_frame_helpers import align_reference_frame
 from dxtbx.model.detector_helpers_types import detector_helpers_types
+from dxtbx.sweep_filenames import template_regex
 
 
 def to_imageset(input_filename, extra_filename=None):
@@ -380,10 +382,14 @@ class to_xds(object):
                 % self.get_beam().get_polarization_normal(),
                 file=out,
             )
-        print(
-            "NAME_TEMPLATE_OF_DATA_FRAMES= %s" % self.get_template().replace("#", "?"),
-            file=out,
-        )
+        template = self.get_template()
+        if template.endswith("master.h5"):
+            master_file = template
+            g = glob.glob(template.split("master.h5")[0] + "data_*[0-9].h5")
+            assert len(g), "No associated data files found for %s" % master_file
+            template = template_regex(g[0])[0]
+            template = master_file.split("master.h5")[0] + template.split("data_")[-1]
+        print("NAME_TEMPLATE_OF_DATA_FRAMES= %s" % template.replace("#", "?"), file=out)
         print("TRUSTED_REGION= 0.0 1.41", file=out)
         for f0, s0, f1, s1 in self.get_detector()[0].get_mask():
             print(
