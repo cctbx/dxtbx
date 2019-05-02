8094491e1b239c9c16543ea7e0e1b47e40b2c8a5
Nicholas Devenish <ndevenish@gmail.com>
2019-05-02 15:06:16 +0100

    Combine all __future__ imports into one statement
    Honestly surprised how few there were!

--- format/FormatHDF5Sacla.py
+++ format/FormatHDF5Sacla.py
@@ -1,5 +1,4 @@
-from __future__ import absolute_import, division
-from __future__ import print_function
+from __future__ import absolute_import, division, print_function
 from dxtbx.format.Format import Format
 from dxtbx.format.FormatHDF5 import FormatHDF5
 from dxtbx.format.FormatStill import FormatStill
