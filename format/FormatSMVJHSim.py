692baf71679a1dacb93bdefe54be4dbf14715862
dgwaterman <dgwaterman@gmail.com>
2019-05-10 09:39:11 +0100

    Revert FormatSMVJHSim
    This does not inherit from FormatSMVADSC

--- format/FormatSMVJHSim.py
+++ format/FormatSMVJHSim.py
@@ -88,7 +88,8 @@ class FormatSMVJHSim(FormatSMV):
             image_pedestal = float(self._header_dictionary["ADC_OFFSET"])
         except (KeyError):
             pass
-        trusted_range = self._adsc_trusted_range(image_pedestal)
+        overload = 65535 - image_pedestal
+        underload = 1 - image_pedestal
 
         # interpret beam center conventions
         image_width_mm = pixel_size * image_size[0]
@@ -122,7 +123,7 @@ class FormatSMVJHSim(FormatSMV):
             "-y",
             (pixel_size, pixel_size),
             image_size,
-            trusted_range,
+            (underload, overload),
             [],
         )
 
