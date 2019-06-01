8e77cf14eb157b13140af13ac8c6a72a12ada43e
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2019-06-02 00:39:54 +0100

    register all format classes using entry points #26
    Enumerate all format classes at configure time rather than at run time
    and register them under the 'dxtbx.format' entry point.
    
    This allows
     * using format classes without having to import each one
     * having format classes in external packages
    and removes the need to have magic metaclasses that registers subclasses
    with a directory at definition time.
    
    Part 1 of the work required for #26

--- libtbx_refresh.py
+++ libtbx_refresh.py
@@ -6,3 +6,53 @@ try:
     dials.precommitbx.nagger.nag()
 except ImportError:
     pass
+
+import ast
+import imp
+import pkgutil
+
+import dxtbx.format
+import libtbx.pkg_utils
+
+# --- format class registration ---
+
+print("Enumerating format classes:")
+format_classes = []
+for _, name, _ in pkgutil.iter_modules(dxtbx.format.__path__):
+    if not name.startswith("Format"):
+        continue
+    try:
+        fid, pathname, desc = imp.find_module(name, dxtbx.format.__path__)
+    except Exception:
+        fid = None
+    if not fid:
+        print("  *** Could not read %s" % name)
+        continue
+    if desc[0] == ".pyc":
+        print("  *** %s only present in compiled form, ignoring" % name)
+        continue
+    content = fid.read()
+    fid.close()
+    try:
+        parsetree = ast.parse(content)
+    except Exception:
+        print("  *** Could not parse %s" % name)
+        continue
+    for top_level_def in parsetree.body:
+        if not isinstance(top_level_def, ast.ClassDef):
+            continue
+        base_names = [
+            baseclass.id
+            for baseclass in top_level_def.bases
+            if isinstance(baseclass, ast.Name)
+        ]
+        if any(n.startswith("Format") for n in base_names):
+            classname = top_level_def.name
+            format_classes.append(
+                "{classname} = dxtbx.format.{modulename}:{classname}".format(
+                    classname=classname, modulename=name
+                )
+            )
+            print("  found", classname, " based on ", str(base_names))
+
+libtbx.pkg_utils.define_entry_points({"dxtbx.format": sorted(format_classes)})
