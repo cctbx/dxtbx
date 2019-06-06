ed81e0fdf99cea377215782a27df4f0784c56160
Richard Gildea <richard.gildea@diamond.ac.uk>
2019-06-06 11:01:15 +0100

    add test for deprecated function

--- tests/serialize/test_filename.py
+++ tests/serialize/test_filename.py
@@ -1,8 +1,9 @@
 from __future__ import absolute_import, division, print_function
 
+import pytest
 import os
 
-from dxtbx.serialize.filename import resolve_path
+from dxtbx.serialize.filename import load_path, resolve_path
 
 
 def test_resolve_path():
@@ -13,3 +14,11 @@ def test_resolve_path():
     new_path = os.path.join("$HELLO_WORLD", "path")
     path = resolve_path(new_path)
     assert path == os.path.abspath(os.path.join("EXPANDED", "path"))
+
+
+def test_load_path_deprecated():
+    os.environ["HELLO_WORLD"] = "EXPANDED"
+    new_path = os.path.join("~", "$HELLO_WORLD", "path")
+    with pytest.deprecated_call():
+        path = load_path(new_path)
+    assert path == resolve_path(new_path)
