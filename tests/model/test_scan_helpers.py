1f2118a4297958a7eef92d98e63012f27f20fefe
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2019-06-14 16:43:20 +0100

    refactor a few tests

--- tests/model/test_scan_helpers.py
+++ tests/model/test_scan_helpers.py
@@ -1,5 +1,7 @@
 from __future__ import absolute_import, division, print_function
 
+import random
+
 from dxtbx.model import is_angle_in_range
 from dxtbx.model import get_range_of_mod2pi_angles
 from dxtbx.model import get_mod2pi_angles_in_range
@@ -7,22 +9,15 @@ from dxtbx.model import get_mod2pi_angles_in_range
 # Run tests for the scan_helpers.h module.
 
 
-def test_is_angle_in_range():
+def test_is_angle_in_random_range():
     """Test that for a range of angles and angular ranges, the
     is_angle_in_range function correctly calculates if the angle
     is in the range."""
-    from random import random
 
-    # Some helper lambda functions
-    mod_360 = lambda x: x - 360.0 * floor(x / 360.0)
-    mod_360_range = lambda x: (mod_360(x[0]), mod_360(x[1]))
-    random_0_360 = lambda: int(random() * 360.0)
-
-    # Create a number of ranges between 0 and 360 and check 360 degrees worth
-    # of angles to see if they are within the range
+    # Create a number of ranges between 0 and 360 to see if they are within range
     num_range = 100
     for n in range(num_range):
-        angular_range = (random_0_360(), random_0_360())
+        angular_range = (int(random.random() * 360), int(random.random()) * 360)
 
         # If A < B or A > B
         if angular_range[0] < angular_range[1]:
@@ -50,6 +45,8 @@ def test_is_angle_in_range():
             for angle in range(angular_range[0], 360):
                 assert is_angle_in_range(angular_range, angle, True) is True
 
+
+def test_is_angle_sweep_in_range():
     # Create a range over 360 and make sure all angles are valid
     angular_range = (-10, 370)
     for angle in range(0, 360):
