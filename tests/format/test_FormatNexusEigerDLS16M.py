5a8bb5bff1fdbcc57fe3aa1067129086090bc4f1
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2019-08-28 09:09:52 +0100

    Remove test xfailing for unknown reason
    closes #55

--- tests/format/test_FormatNexusEigerDLS16M.py
+++ tests/format/test_FormatNexusEigerDLS16M.py
@@ -106,34 +106,3 @@ def test_screening():
     imagesets = datablocks[0].extract_imagesets()
     assert len(imagesets) == 3
     assert imagesets[0].get_format_class() == FormatNexusEigerDLS16M
-
-
-@pytest.mark.xfail
-def test_units():
-    master_h5 = "/dls/i04/data/2019/cm23004-1/20190114/Eiger/grid/Se_Thaum/Se_Thaum_12/Se_Thaum_12_2_master.h5"
-    assert FormatNexusEigerDLS16M.understand(master_h5)
-
-    datablocks = DataBlockFactory.from_filenames([master_h5])
-    imageset = datablocks[0].extract_imagesets()[0]
-    assert imageset.get_format_class() == FormatNexusEigerDLS16M
-
-    detector = imageset.get_detector()
-    scan = imageset.get_scan()
-    beam = imageset.get_beam()
-
-    panel = detector[0]
-    assert panel.get_pixel_size() == (0.075, 0.075)
-    assert panel.get_fast_axis() == (1, 0, 0)
-    assert panel.get_slow_axis() == (0, -1, 0)
-    # XXX Need to check and update expected values here, however the ones
-    # dxtbx is currently generating from the file are very wrong
-    assert panel.get_origin() == pytest.approx(
-        (-167.35570274412459, 172.4729262553403, -339.9887931971389)
-    )
-    assert panel.get_distance() == pytest.approx(339.9887931971389)
-
-    assert scan is None
-
-    assert beam.get_wavelength() == pytest.approx(0.979499)
-    assert beam.get_s0() == pytest.approx((0, 0, -1 / beam.get_wavelength()))
-    assert panel.get_beam_centre_px(beam.get_s0()) == pytest.approx((2231.41, 2299.64))
