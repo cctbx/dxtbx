a71f730a760a9f84922361a0acd32e3bbf5200d0
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2019-08-29 21:32:59 +0100

    This is allowed per contributor guidelines
    as this file only provides access to C++ extensions,
    so silence flake8 warnings.

--- format/image.py
+++ format/image.py
@@ -1,8 +1,8 @@
 from __future__ import absolute_import, division, print_function
 
-from dxtbx_format_image_ext import *
+from dxtbx_format_image_ext import *  # noqa: F403
 
-__all__ = (
+__all__ = (  # noqa: F405
     "CBFFastImageListReader",
     "CBFFastReader",
     "CBFImageListReader",
