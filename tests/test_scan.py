e3cce9deaa37a3537a8732b3636787e50f955ee4
Nicholas Devenish <ndevenish@gmail.com>
2019-08-29 14:37:37 +0100

    Remove empty 'test' files from tests/
    Refactored test_scan to create them in a temporary folder via fixture
    instead

--- tests/test_scan.py
+++ tests/test_scan.py
@@ -5,37 +5,47 @@ from __future__ import absolute_import, division, print_function
 from builtins import range
 import os
 
-import dxtbx
 from dxtbx.model.scan import ScanFactory
 from dxtbx.model.scan_helpers import scan_helper_image_files
 from dxtbx.model.scan_helpers import scan_helper_image_formats
 import pytest
 
 
-def test_helper_image_files():
+@pytest.fixture
+def image_test_dir(tmp_path):
+    """Create a sequence of 20 'images' for scan tests to pick up"""
+    for i in range(1, 21):
+        image_file = tmp_path / "image_{:03d}.dat".format(i)
+        image_file.touch()
+    return str(tmp_path)
+
+
+def test_helper_image_files(image_test_dir):
     """Test the static methods in scan_helper_image_files."""
     assert scan_helper_image_files()
-
-    directory = os.path.join(os.path.split(dxtbx.__file__)[0], "tests")
 
     template = "image_###.dat"
 
     assert (
-        len(scan_helper_image_files.template_directory_to_indices(template, directory))
+        len(
+            scan_helper_image_files.template_directory_to_indices(
+                template, image_test_dir
+            )
+        )
         == 20
     )
 
     assert scan_helper_image_files.template_directory_index_to_image(
-        template, directory, 1
-    ) == os.path.join(directory, "image_001.dat")
+        template, image_test_dir, 1
+    ) == os.path.join(image_test_dir, "image_001.dat")
 
     assert (
         scan_helper_image_files.template_index_to_image(template, 1) == "image_001.dat"
     )
 
     assert scan_helper_image_files.image_to_template_directory(
-        os.path.join(directory, "image_001.dat")
-    ) == (template, directory)
+        os.path.join(image_test_dir, "image_001.dat")
+    ) == (template, image_test_dir)
 
     assert scan_helper_image_files.image_to_index("image_001.dat") == 1
 
@@ -52,17 +62,15 @@ def test_helper_image_formats():
     # assert(not(scan_helper_image_formats.check_format('CBF')))
 
 
-def test_xScanFactory():
+def test_xScanFactory(image_test_dir):
     """Test out the ScanFactory."""
-
-    directory = os.path.join(os.path.split(dxtbx.__file__)[0], "tests")
 
     template = "image_###.dat"
 
     xscans = [
         ScanFactory.single(
             scan_helper_image_files.template_directory_index_to_image(
-                template, directory, j + 1
+                template, image_test_dir, j + 1
             ),
             scan_helper_image_formats.FORMAT_CBF,
             1.0,
@@ -88,7 +96,7 @@ def test_xScanFactory():
     a + b
 
     filename = scan_helper_image_files.template_directory_index_to_image(
-        template, directory, 1
+        template, image_test_dir, 1
     )
 
     assert len(ScanFactory.search(filename)) == 20
