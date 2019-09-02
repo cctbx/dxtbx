f58caf88f6c5a89005a66a6dbf5de63b20b92081
Graeme Winter <graeme.winter@gmail.com>
2019-09-02 18:16:54 +0100

    Slightly hacked dlsnxs2cbf (#81)
    Add new command dxtbx.dlsnxs2cbf that converts Nexus files created at
    Diamond Light Source to .cbf files.
    
    Co-authored-by: Graeme Winter <graeme.winter@gmail.com>
    Co-authored-by: Richard Gildea <richard.gildea@diamond.ac.uk>
    Co-authored-by: Markus Gerstel <markus.gerstel@diamond.ac.uk>

---
+++ newsfragments/81.feature
@@ -0,0 +1,2 @@
+Add new command dxtbx.dlsnxs2cbf which converts Nexus files created at
+Diamond Light Source to .cbf files.
---
+++ tests/command_line/test_dlsnxs2cbf.py
@@ -0,0 +1,22 @@
+from __future__ import absolute_import, division, print_function
+
+import procrunner
+
+
+def test_dlsnxs2cbf(dials_data, tmpdir):
+    screen = dials_data("thaumatin_eiger_screen")
+    master = screen.join("Therm_6_1_master.h5")
+    result = procrunner.run(
+        ["dxtbx.dlsnxs2cbf", master, "junk_%04d.cbf"], working_directory=tmpdir
+    )
+    assert not result.returncode and not result.stderr
+
+    expected_output = "\n".join("junk_%04d.cbf" % j for j in (1, 2, 3))
+
+    for record in expected_output.split("\n"):
+        assert record.strip().encode("latin-1") in result.stdout, record
+
+    # check files on disk
+
+    for j in (1, 2, 3):
+        assert tmpdir.join("junk_%04d.cbf" % j).check()
