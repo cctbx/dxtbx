ae3a94f42798ba9cc8a4ff6de9c6e1668fdbe39f
Nicholas Devenish <ndevenish@gmail.com>
2019-09-11 15:08:52 +0100

    Exclude ext.py from sorting
    Although it doesn't _currently_ seem to be causing test failures,
    historically this was doing something important. Because it's core,
    leave behaviour as-is and leave until later to investigate.

--- ext.py
+++ ext.py
@@ -2,6 +2,5 @@ from __future__ import absolute_import, division, print_function
 
 import boost.python
 
-from dxtbx_ext import *  # noqa: F401,F403
-
 ext = boost.python.import_ext("dxtbx_ext")
+from dxtbx_ext import *  # isort:skip # noqa: F401,F403,E402
