14750930037e2b655bb68642cad67a3a80a22f54
Richard Gildea <richard.gildea@diamond.ac.uk>
2019-10-17 11:51:30 +0100

    External class to staticmethod of ExperimentListFactory

--- model/experiment_list.py
+++ model/experiment_list.py
@@ -690,14 +690,13 @@ class ExperimentListFactory(object):
         # Now try as a pickle file
         return ExperimentListFactory.from_pickle_file(filename)
 
-
-class ExperimentListTemplateImporter(object):
-    """ A class to import an experiment list from a template. """
-
-    def __init__(self, templates, verbose=False, **kwargs):
+    @staticmethod
+    def from_templates(templates, verbose=False, **kwargs):
+        """Import an experiment list from templates"""
         importer = DataBlockTemplateImporter(templates, verbose=verbose, **kwargs)
-        self.experiments = ExperimentList()
+        experiments = ExperimentList()
         for db in importer.datablocks:
-            self.experiments.extend(
+            experiments.extend(
                 ExperimentListFactory.from_datablock_and_crystal(db, None)
             )
+        return experiments
