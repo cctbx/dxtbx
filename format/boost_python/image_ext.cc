86bcf90f4b471c82d4424b2bf7a11062b093b7c2
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2019-12-01 13:52:14 +0000

    Retire format readers

--- format/boost_python/image_ext.cc
+++ format/boost_python/image_ext.cc
@@ -17,11 +17,6 @@
 #include <scitbx/array_family/flex_types.h>
 #include <dxtbx/error.h>
 #include <dxtbx/format/image.h>
-#include <dxtbx/format/image_reader.h>
-#include <dxtbx/format/smv_reader.h>
-#include <dxtbx/format/tiff_reader.h>
-#include <dxtbx/format/cbf_reader.h>
-#include <dxtbx/format/hdf5_reader.h>
 #include <vector>
 #include <hdf5.h>
 
@@ -30,17 +25,6 @@
 namespace dxtbx { namespace format { namespace boost_python {
 
   using namespace boost::python;
-
-  template <typename ImageReaderType>
-  void image_list_reader_suite(const char *name) {
-    typedef ImageListReader<ImageReaderType> image_list_reader;
-
-    class_<image_list_reader>(name, no_init)
-      .def(init<const scitbx::af::const_ref<std::string> &>((arg("filenames"))))
-      .def("filenames", &image_list_reader::filenames)
-      .def("image", &image_list_reader::image, (arg("index")))
-      .def("__len__", &image_list_reader::size);
-  }
 
   template <typename T>
   boost::shared_ptr<ImageTile<T> > make_image_tile(
@@ -171,44 +155,6 @@ namespace dxtbx { namespace format { namespace boost_python {
       .def("as_int", &ImageBuffer::as_int)
       .def("as_float", &ImageBuffer::as_float)
       .def("as_double", &ImageBuffer::as_double);
-
-    class_<ImageReader>("ImageReader", no_init)
-      .def("filename", &ImageReader::filename)
-      .def("image", &ImageReader::image);
-
-    class_<SMVReader, bases<ImageReader> >("SMVReader", no_init)
-      .def(init<const char *>((arg("filename"))))
-      .def("byte_order", &SMVReader::byte_order);
-
-    class_<TIFFReader, bases<ImageReader> >("TIFFReader", no_init)
-      .def(init<const char *>((arg("filename"))));
-
-    class_<CBFFastReader, bases<ImageReader> >("CBFFastReader", no_init)
-      .def(init<const char *>((arg("filename"))));
-
-    class_<CBFReader, bases<ImageReader> >("CBFReader", no_init)
-      .def(init<const char *>((arg("filename"))));
-
-    class_<HDF5Reader>("HDF5Reader", no_init)
-      .def(init<hid_t, const scitbx::af::const_ref<std::string> &>(
-        (arg("handle"), arg("datasets"))))
-      .def(
-        init<hid_t,
-             const scitbx::af::const_ref<std::string> &,
-             std::size_t,
-             std::size_t>((arg("handle"), arg("datasets"), arg("first"), arg("last"))))
-      .def("filename", &HDF5Reader::filename)
-      .def("handle", &HDF5Reader::handle)
-      .def("datasets", &HDF5Reader::datasets)
-      .def("first", &HDF5Reader::first)
-      .def("last", &HDF5Reader::last)
-      .def("image", &HDF5Reader::image)
-      .def("__len__", &HDF5Reader::size);
-
-    image_list_reader_suite<SMVReader>("SMVImageListReader");
-    image_list_reader_suite<TIFFReader>("TIFFImageListReader");
-    image_list_reader_suite<CBFFastReader>("CBFFastImageListReader");
-    image_list_reader_suite<CBFReader>("CBFImageListReader");
 
     export_cbf_read_buffer();
   }
