8d90935df93bf854981257ae3f41b36f465f8350
Graeme Winter <graeme.winter@gmail.com>
2019-12-20 09:15:48 +0000

    test for print header

---
+++ tests/command_line/test_print_header.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import, division, print_function
+
+import procrunner
+
+
+def test_print_header(dials_data):
+    screen = dials_data("thaumatin_eiger_screen")
+    master = screen.join("Therm_6_1_master.h5")
+    result = procrunner.run(["dxtbx.print_header", master])
+    assert not result.returncode and not result.stderr
+
+    expected_output = [
+        "=== %s ===" % master,
+        "Using header reader: FormatNexusEigerDLS16M",
+    ]
+
+    for record in expected_output:
+        assert record.strip().encode("latin-1") in result.stdout, record
