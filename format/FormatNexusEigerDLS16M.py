8f0b6aee79f386fcae0953931a24469e396cfc93
Graeme Winter <graeme.winter@gmail.com>
2020-02-07 13:46:16 +0000

    For i03-365 - look for cutoff in meta file (#136)
    For i03-365 - look for cutoff in meta file
    
    At the moment this is not linkable from the proper h5 file structure however
    is included in the meta file as the config dataset, a text file which
    comes from the detector - look this up as a workaround
    
    Assert limit > 0 - if not AssertionError will be trapped
    
    If *anything* goes wrong, ignore by design
    
    Co-Authored-By: Markus Gerstel <2102431+Anthchirp@users.noreply.github.com>

--- format/FormatNexusEigerDLS16M.py
+++ format/FormatNexusEigerDLS16M.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import, division, print_function
 
+import ast
 import sys
 
 import h5py
@@ -9,6 +10,15 @@ import libtbx
 from dxtbx.format.FormatNexus import FormatNexus
 from dxtbx.masking import GoniometerMaskerFactory
 from dxtbx.model import MultiAxisGoniometer
+
+
+def get_count_limit_from_meta(meta_file_name):
+    with h5py.File(meta_file_name, "r") as f:
+
+        config = f["/config"][()]
+        config_data = ast.literal_eval(config.decode("utf-8"))
+
+    return config_data["countrate_correction_count_cutoff"]
 
 
 class FormatNexusEigerDLS16M(FormatNexus):
@@ -50,6 +60,30 @@ class FormatNexusEigerDLS16M(FormatNexus):
 
         super(FormatNexusEigerDLS16M, self).__init__(image_file, **kwargs)
         self._dynamic_shadowing = self.has_dynamic_shadowing(**kwargs)
+
+    def get_detector(self, index=None):
+        if not self._image_file.endswith("_master.h5"):
+            return self._detector()
+
+        # workaround for https://jira.diamond.ac.uk/browse/I03-365
+        # read the count limit from the meta file - if anything goes
+        # wrong, do nothing
+
+        detector = self._detector()
+
+        try:
+            meta = self._image_file.replace("_master.h5", "_meta.h5")
+            limit = get_count_limit_from_meta(meta)
+
+            assert limit > 0
+
+            for panel in detector:
+                trusted = panel.get_trusted_range()
+                panel.set_trusted_range((trusted[0], limit))
+        except Exception:
+            pass
+
+        return detector
 
     def get_goniometer_shadow_masker(self, goniometer=None):
         if not self._dynamic_shadowing:
