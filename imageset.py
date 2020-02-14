df9383c885bb1456f3c472ced67f6bd11b41a922
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2020-02-14 10:53:54 +0000

    proposed changes

--- imageset.py
+++ imageset.py
@@ -226,26 +226,27 @@ class _(object):
 
         """
         if isinstance(item, slice):
+            offset = self.get_scan().get_batch_offset()
             if item.step is not None:
                 raise IndexError("Sequences must be sequential")
 
             # nasty workaround for https://github.com/dials/dials/issues/1153
             # slices with -1 in them are meaningful :-/ so grab the original
-            # constructor arguments
-            if self.get_scan().get_batch_offset() < 0:
+            # constructor arguments of the slice object.
+            # item.start and item.stop may have been compromised at this point.
+            if offset < 0:
                 start, stop, step = item.__reduce__()[1]
                 if start is None:
                     start = 0
                 else:
-                    start -= self.get_scan().get_batch_offset()
+                    start -= offset
                 if stop is None:
                     stop = len(self)
                 else:
-                    stop -= self.get_scan().get_batch_offset()
+                    stop -= offset
                 return self.partial_set(start, stop)
             else:
                 start = item.start or 0
-                offset = self.get_scan().get_batch_offset()
                 stop = item.stop or (len(self) + offset)
                 return self.partial_set(start - offset, stop - offset)
         else:
