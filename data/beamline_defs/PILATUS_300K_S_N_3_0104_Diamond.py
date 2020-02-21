fe00c0c25c6c7cc5c896ae69c36301390853e9db
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2020-02-21 11:04:29 +0000

    Update beamline CIF definitions
    * change _diffrn_radiation_type to _diffrn_source
    * change _diffrn_radiation.source to _diffrn_source_type
    * add support for beamline references
    * further update I19 CIF definitions

--- data/beamline_defs/PILATUS_300K_S_N_3_0104_Diamond.py
+++ data/beamline_defs/PILATUS_300K_S_N_3_0104_Diamond.py
@@ -31,19 +31,23 @@ class get_definition(dxtbx.data.beamline_defs.template):
 
         b = iotbx.cif.model.block()
         b[lookup("df.detector")] = "Photon counting pixel array"
-        b[lookup("df.rad.type")] = "Synchrotron"
+        b[lookup("df.detector.type")] = "Dectris PILATUS 300K"
+        b[lookup("df.src")] = "Synchrotron"
 
         return b, lookup
 
     def _at_I19(self, mmcif=False):
         b, lookup = self._base(mmcif)
 
-        b[lookup("df.m.dev")] = "Newport IS4CCD (4 circle)"
-        b[lookup("df.m.dev_type")] = "Photon counting pixel array, PILATUS 300K"
+        b[lookup("df.m.dev")] = "4-circle \\k-geometry diffractometer"
+        b[lookup("df.m.dev.type")] = "Newport IS4CCD"
         b[lookup("df.m.method")] = "shutterless scans"
-        #   b[lookup('df.m.spec_supp')] = 'MiTeGen MicroMount'
-        b[lookup("df.rad.source")] = "Diamond Light Source Beamline I19-2"
         b[lookup("df.rad.mono")] = "Silicon 111"
+        b[lookup("df.src.details")] = "Nowell et al. (2012)"
+        b[lookup("df.src.type")] = "Diamond Light Source Beamline I19-2"
+        b[
+            lookup("references")
+        ] = "Nowell, H. et al. (2012) J. Synchrotron Rad. 19, 435-441."
         b[lookup("sw.collection")] = "GDA - generic data acquisition software"
 
         return b
