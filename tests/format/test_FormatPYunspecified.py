fe0494b84202b0dbd28f0c9cc7c56ce100ca0c8e
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2020-04-06 10:30:43 +0100

    Remove xfailing test added in #73
    Test has never passed, and test file will not load on Python 3 anyway

--- tests/format/test_FormatPYunspecified.py
+++ tests/format/test_FormatPYunspecified.py
@@ -2,14 +2,7 @@ from __future__ import absolute_import, division, print_function
 
 import os
 
-import pytest
-import six.moves.cPickle as pickle
-
-from dxtbx.format.FormatPYunspecifiedStill import (
-    FormatPYunspecifiedStill,
-    FormatPYunspecifiedStillInMemory,
-)
-from dxtbx.imageset import ImageSet, ImageSetData, MemReader
+from dxtbx.format.FormatPYunspecifiedStill import FormatPYunspecifiedStill
 from dxtbx.model.experiment_list import ExperimentListFactory
 
 
@@ -29,20 +22,3 @@ def test_static_mask(dials_regression):
         mask = imageset.get_mask(0)
         assert len(mask) == 1
         assert mask[0].count(False) == 867109
-
-
-@pytest.mark.xfail
-def test_FormatPYunspecifiedStillInMemory(dials_regression):
-    filename = os.path.join(
-        dials_regression,
-        "image_examples/LCLS_CXI/shot-s00-2011-12-02T21_07Z29.723_00569.pickle",
-    )
-    assert not FormatPYunspecifiedStillInMemory.understand(filename)
-    with open(filename, "rb") as f:
-        d = pickle.load(f)
-    assert FormatPYunspecifiedStillInMemory.understand(d)
-    img = FormatPYunspecifiedStillInMemory(d)
-    imageset = ImageSet(ImageSetData(MemReader([img]), None))
-    mask = imageset.get_mask(0)
-    assert len(mask) == 1
-    assert mask[0].count(False) == 867109
