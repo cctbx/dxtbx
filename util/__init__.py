fc9af5f194ba5076848096ccbb1bd57576a98d67
Graeme Winter <graeme.winter@gmail.com>
2020-05-01 14:15:39 +0100

    Make cell formatting sensible
    If standard_uncertainty < minimum then return value rounded to - log10(minimum) dp

---
+++ util/__init__.py
@@ -0,0 +1,44 @@
+from __future__ import absolute_import, division, print_function
+
+import math
+
+from libtbx.math_utils import round2
+
+
+def format_float_with_standard_uncertainty(value, standard_uncertainty, minimum=1e-15):
+    """
+    Formats a float, including the uncertainty in its value.
+
+    Parameters
+    ----------
+    value : float
+    standard_uncertainty : float
+    minimum : float
+
+    Returns
+    -------
+    str
+
+    Examples
+    --------
+    >>> format_float_with_standard_uncertainty(5e-3, 1e-3)
+    '0.0050(10)'
+    >>> format_float_with_standard_uncertainty(5e-3, 1e-6)
+    '0.0050000(10)'
+    """
+    if standard_uncertainty <= minimum:
+        dp = -int(math.log10(minimum))
+        return str(round(value, dp))
+    precision = -int(round2(math.log10(standard_uncertainty)))
+    if precision > -1:
+        su = standard_uncertainty * math.pow(10, precision)
+        if round2(su, 1) < 2:
+            su *= 10
+            precision += 1
+        fmt_str = "%%.%if(%%i)" % precision
+        return fmt_str % (value, round2(su))
+    else:
+        precision += 1
+        su = int(round2(standard_uncertainty, precision))
+        fmt_str = "%.0f(%i)"
+        return fmt_str % (round2(value, precision), su)
