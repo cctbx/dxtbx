a51264d1354fc31ea0be5211cb5a28b0f3529e90
Graeme Winter <graeme.winter@gmail.com>
2020-05-01 14:32:51 +0100

    Add test

---
+++ tests/test_util.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import, division, print_function
+
+from dxtbx.util import format_float_with_standard_uncertainty as ffwsu
+
+
+def test_format_float_with_standard_uncertainty():
+    """Test format_float_with_standard_uncertainty."""
+
+    assert ffwsu(21.234567, 0.0013) == "21.2346(13)"
+    assert ffwsu(21.234567, 0.0023) == "21.235(2)"
+    assert ffwsu(12345, 45) == "12350(50)"
+    assert ffwsu(12.3, 1.2) == "12.3(12)"
+    assert ffwsu(-0.2451, 0.8135) == "-0.2(8)"
+    assert ffwsu(1.234, 0.196) == "1.2(2)"
+    assert ffwsu(1.234, 0.193) == "1.23(19)"
