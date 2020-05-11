1aa673c2f905f2e1aa94199f087e612402101193
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2020-05-11 10:28:34 +0100

    no longer required following merge of cctbx/cctbx_project#324

--- __init__.py
+++ __init__.py
@@ -22,20 +22,6 @@ os.environ["HDF5_PLUGIN_PATH"] = (
     plugin_path + os.pathsep + os.getenv("HDF5_PLUGIN_PATH", "")
 )
 
-# Invert FPE trap defaults, https://github.com/cctbx/cctbx_project/pull/324
-if "boost.python" in sys.modules:
-    import boost.python
-
-    boost.python.ext.trap_exceptions(
-        bool(os.getenv("BOOST_ADAPTBX_TRAP_FPE")),
-        bool(os.getenv("BOOST_ADAPTBX_TRAP_INVALID")),
-        bool(os.getenv("BOOST_ADAPTBX_TRAP_OVERFLOW")),
-    )
-elif not os.getenv("BOOST_ADAPTBX_TRAP_FPE") and not os.getenv(
-    "BOOST_ADAPTBX_TRAP_OVERFLOW"
-):
-    os.environ["BOOST_ADAPTBX_FPE_DEFAULT"] = "1"
-
 logging.getLogger("dxtbx").addHandler(logging.NullHandler())
 
 
