8a698e75cf2b3196b8f274b0eee8c49a8f27c4a9
dermen <dermen@lbl.gov>
2020-06-03 13:27:26 -0700

    serializes Beam spectrum_h5 with absolute path

--- model/__init__.py
+++ model/__init__.py
@@ -890,6 +890,7 @@ class _(object):
             expt.crystal = expt.crystal.change_basis(cb_op)
         return return_expts
 
+
 def beam_spectra_to_h5(filename, obj):
     """
     Helper function to take an experiment list dictionary and move the
@@ -930,7 +931,7 @@ def beam_spectra_to_h5(filename, obj):
     import h5py
 
     h5_filename = os.path.splitext(filename)[0] + "_spectrum.h5"
-    obj["spectra_h5"] = h5_filename
+    obj["spectra_h5"] = os.path.abspath(h5_filename)
     f = h5py.File(h5_filename, "w")
 
     def get_stacked_data(data):
