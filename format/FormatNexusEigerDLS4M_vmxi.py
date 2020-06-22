f04a075a4ae88b6c0a96054c29369594ac668e24
Graeme Winter <graeme.winter@gmail.com>
2020-06-22 10:06:44 +0100

    No multi-axis gonio, and manually patch pixels

--- format/FormatNexusEigerDLS4M_vmxi.py
+++ format/FormatNexusEigerDLS4M_vmxi.py
@@ -8,7 +8,6 @@ import h5py
 import libtbx
 
 from dxtbx.format.FormatNexus import FormatNexus
-from dxtbx.masking import GoniometerMaskerFactory
 from dxtbx.model import MultiAxisGoniometer
 
 
@@ -19,6 +18,15 @@ def get_count_limit_from_meta(meta_file_name):
         config_data = ast.literal_eval(config.decode("utf-8"))
 
     return config_data["countrate_correction_count_cutoff"]
+
+
+def get_bit_depth_from_meta(meta_file_name):
+    with h5py.File(meta_file_name, "r") as f:
+
+        config = f["/config"][()]
+        config_data = ast.literal_eval(config.decode("utf-8"))
+
+    return config_data["bit_depth_image"]
 
 
 class FormatNexusEigerDLS4M_vmxi(FormatNexus):
@@ -45,6 +53,14 @@ class FormatNexusEigerDLS4M_vmxi(FormatNexus):
 
         super(FormatNexusEigerDLS4M_vmxi, self).__init__(image_file, **kwargs)
         self._dynamic_shadowing = self.has_dynamic_shadowing(**kwargs)
+        try:
+            if self._image_file.endswith("_master.h5"):
+                meta = self._image_file.replace("_master.h5", "_meta.h5")
+            elif self._image_file.endswith(".nxs"):
+                meta = self._image_file.replace(".nxs", "_meta.h5")
+            self._bit_depth_image = get_bit_depth_from_meta(meta)
+        except Exception:
+            self._bit_depth_image = 0
 
     def get_detector(self, index=None):
         # workaround for https://jira.diamond.ac.uk/browse/I03-365
@@ -70,24 +86,20 @@ class FormatNexusEigerDLS4M_vmxi(FormatNexus):
 
         return detector
 
-    def get_goniometer_shadow_masker(self, goniometer=None):
-        if not self._dynamic_shadowing:
-            return None
+    def get_raw_data(self, index):
+        data = self._raw_data[index]
+        if self._bit_depth_image:
+            # if 32 bit then it is a signed int, I think if 8, 16 then it is
+            # unsigned with the highest two values assigned as masking values
+            if self._bit_depth_image == 32:
+                top = 2 ** 31
+            else:
+                top = 2 ** self._bit_depth_image
+            d1d = data.as_1d()
+            d1d.set_selected(d1d == top - 1, -1)
+            d1d.set_selected(d1d == top - 2, -2)
 
-        if goniometer is None:
-            goniometer = self.get_goniometer()
-
-        assert goniometer is not None
-
-        if goniometer.get_names()[1] == "chi":
-            return GoniometerMaskerFactory.smargon(goniometer)
-        elif goniometer.get_names()[1] == "kappa":
-            return GoniometerMaskerFactory.mini_kappa(goniometer)
-
-        else:
-            raise RuntimeError(
-                "Don't understand this goniometer: %s" % list(goniometer.get_names())
-            )
+        return data
 
 
 if __name__ == "__main__":
