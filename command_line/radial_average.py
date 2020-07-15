30d8a136e64e02389bdb6c4da928d0eb626f464f
Aaron Brewster <asbrewster@lbl.gov>
2020-07-15 10:23:45 -0700

    Add feature to predict miller index positions given a uc/sg and tidy up
    - Add ability to predict miller index positions given a cell and space group
    - Remove unused parameters beam_x and beam_y
    - Add help strings and display help strings when usage is raised
    - Don't catch Exception, but use TypeError in run method
    - Matplotlib fix. set_color_cycle is removed in later versions.

--- command_line/radial_average.py
+++ command_line/radial_average.py
@@ -10,7 +10,7 @@ from builtins import range
 
 import numpy as np
 
-import libtbx.phil
+import iotbx.phil
 from libtbx import easy_pickle
 from libtbx.utils import Sorry, Usage
 from scitbx.array_family import flex
@@ -20,47 +20,75 @@ from xfel import radial_average
 import dxtbx.datablock
 from dxtbx.model.experiment_list import ExperimentListFactory
 
-master_phil = libtbx.phil.parse(
+from cctbx.crystal import symmetry
+import cctbx.miller
+
+master_phil = iotbx.phil.parse(
     """
   file_path = None
     .type = str
     .multiple = True
-  beam_x = None
-    .type = float
-  beam_y = None
-    .type = float
+    .help = Files to read
   n_bins = 0
     .type = int
+    .help = Number of bins in the radial average. Auto determined if set to 0
   verbose = True
     .type = bool
+    .help = Extra logging information
   output_bins = True
     .type = bool
+    .help = Whether to print values for each bin
   output_file = None
     .type = str
+    .help = Output file for logging results
   plot_x_max = None
     .type = int
+    .help = Max value for x axis
   plot_y_max = None
     .type = int
+    .help = Max value for xyaxis
   low_max_two_theta_limit = None
     .type = float
+    .help = Low two theta cutoff
   normalize = False
     .type = bool
+    .help = Whether to normalize the Y values to 1
   show_plots = True
     .type = bool
+    .help = Whether to show the radial average plot
   mask = None
     .type = str
+    .help = DIALS style pixel mask. Average will skip these pixels
   median_filter_size = None
     .type = int
+    .help = If not none, applies a scipy.ndimage median_filter to the average
   x_axis = *two_theta q resolution
     .type = choice
+    .help = Units for x axis
   image_number = None
     .type = int
+    .help = When supplying a composite file, which image to show. Otherwise \
+            shows all images in the file.
   panel = None
     .type = int
+    .help = Only use data from the specified panel
   max_images = None
     .type = int
+    .help = When supplying a composite file, only show up to max_images images
   reference_geometry = None
     .type = path
+    .help = Apply this geometry before creating average
+  unit_cell = None
+    .type = unit_cell
+    .help = Show positions of miller indices from this unit_cell and space \
+            group.
+  space_group = None
+    .type = space_group
+    .help = Show positions of miller indices from this unit_cell and space \
+            group.
+  d_min = 4
+    .type = float
+    .help = Resolution limit for miller index display
 """
 )
 
@@ -69,21 +97,21 @@ def run(args, imageset=None):
     # Parse input
     try:
         len(args)
-    except Exception:
+    except TypeError:
         params = args
     else:
         user_phil = []
         for arg in args:
             if "=" in arg:
                 try:
-                    user_phil.append(libtbx.phil.parse(arg))
+                    user_phil.append(iotbx.phil.parse(arg))
                 except RuntimeError as e:
                     raise Sorry(
                         "Unrecognized argument '%s' (error: %s)" % (arg, str(e))
                     )
             else:
                 try:
-                    user_phil.append(libtbx.phil.parse("""file_path=%s""" % arg))
+                    user_phil.append(iotbx.phil.parse("""file_path=%s""" % arg))
                 except ValueError:
                     raise Sorry("Unrecognized argument '%s'" % arg)
         params = master_phil.fetch(sources=user_phil).extract()
@@ -93,7 +121,7 @@ def run(args, imageset=None):
             or len(params.file_path) == 0
             or not all(os.path.isfile(f) for f in params.file_path)
         ):
-            master_phil.show()
+            master_phil.show(attributes_level=2)
             raise Usage(
                 "file_path must be defined (either file_path=XXX, or the path alone)."
             )
@@ -121,8 +149,8 @@ def run(args, imageset=None):
         from matplotlib import pyplot as plt
 
         colormap = plt.cm.gist_ncar
-        plt.gca().set_color_cycle(
-            [colormap(i) for i in np.linspace(0, 0.9, len(params.file_path))]
+        plt.gca().set_prop_cycle(
+            color=[colormap(i) for i in np.linspace(0, 0.9, len(params.file_path))]
         )
 
     if params.mask is not None:
@@ -355,6 +383,37 @@ def run(args, imageset=None):
                     plt.ylim(0, params.plot_y_max)
 
     if params.show_plots:
+        if params.unit_cell or params.space_group:
+            assert params.unit_cell and params.space_group
+            sym = symmetry(
+                unit_cell=params.unit_cell, space_group=params.space_group.group()
+            )
+            hkl_list = cctbx.miller.build_set(sym, False, d_min=params.d_min)
+            tt = sym.unit_cell().two_theta(
+                hkl_list.indices(), beam.get_wavelength(), deg=True
+            )
+            if params.x_axis == "q":
+                q_vals = (
+                    4 * math.pi * flex.sin(math.pi * tt / 360) / beam.get_wavelength()
+                )
+                vals = q_vals
+            elif params.x_axis == "resolution":
+                # nlmbda = 2dsin(theta)
+                resolution = flex.double(len(tt), 0)
+                nonzero = tt > 0
+                resolution.set_selected(
+                    nonzero,
+                    beam.get_wavelength()
+                    / (2 * flex.asin((math.pi / 180) * tt.select(nonzero) / 2)),
+                )
+                vals = resolution
+                vals = 1 / (vals ** 2)
+            elif params.x_axis == "two_theta":
+                vals = tt
+
+            for val in vals:
+                plt.plot([val, val], [0, 50], "r")
+
         # plt.legend([os.path.basename(os.path.splitext(f)[0]) for f in params.file_path], ncol=2)
         plt.show()
 
