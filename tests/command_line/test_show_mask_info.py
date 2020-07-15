ef07f3c2ef50db6e4d1b34a3f0a41004db229670
Graeme Winter <graeme.winter@gmail.com>
2020-07-15 12:07:14 +0100

    Test for command line tool

---
+++ tests/command_line/test_show_mask_info.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import, division, print_function
+
+import procrunner
+
+
+def test_show_mask_info(dials_data):
+
+    data = dials_data("image_examples") / "dectris_eiger_master.h5"
+
+    result = procrunner.run(["dev.dxtbx.show_mask_info", data.strpath])
+    assert not result.returncode and not result.stderr
+
+    assert b"Module 0 has 637992 masked pixels of 10166590" in result.stdout
