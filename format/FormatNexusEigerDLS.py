447f10ebb59f0d5dc2d68715a56bea8faa5a0a43
James Beilsten-Edmands <jbeilstenedmands@gmail.com>
2020-09-24 11:21:34 +0100

    Define image_as_flex function using lz4 decompression

--- format/FormatNexusEigerDLS.py
+++ format/FormatNexusEigerDLS.py
@@ -4,8 +4,70 @@ import ast
 import os
 
 import h5py
+import numpy
 
 from dxtbx.format.FormatNexusEiger import FormatNexusEiger
+from dxtbx.format.nexus import (
+    BeamFactory,
+    DataFactory,
+    DetectorFactory,
+    DetectorFactoryFromGroup,
+    NXmxReader,
+    detectorgroupdatafactory,
+)
+
+try:
+    from dxtbx_format_nexus_ext import (
+        image_as_flex_double,
+        image_as_flex_float,
+        image_as_flex_int,
+    )
+except ImportError:
+    # Workaround for psana build, which doesn't link HDF5 properly
+    if "SIT_ROOT" not in os.environ:
+        raise
+
+
+def image_as_flex(dataset, index):
+    if numpy.issubdtype(dataset.dtype, numpy.integer):
+        return image_as_flex_int(dataset.id.id, index)
+    else:
+        assert numpy.issubdtype(dataset.dtype, numpy.floating)
+        if dataset.dtype in [
+            numpy.half,
+            numpy.single,
+            numpy.float_,
+            numpy.float16,
+            numpy.float32,
+        ]:
+            return image_as_flex_float(dataset.id.id, index)
+        elif dataset.dtype in [
+            numpy.double,
+            numpy.longfloat,
+            numpy.float64,
+            numpy.float96,
+            numpy.float128,
+        ]:
+            return image_as_flex_double(dataset.id.id, index)
+        else:
+            assert False, "unknown floating data type (%s)" % str(dataset.dtype)
+
+
+class DLSEigerDataFactory(DataFactory):
+    def __getitem__(self, index):
+        d = self._lookup[index]
+        i = index - self._offset[d]
+
+        # a lock-free most-recently-used file handle cache based on
+        # immutability of python tuples
+        cached_handle = self._cache
+        if self._datasets[d].file == cached_handle[0]:
+            data = cached_handle[1]
+        else:
+            data = self._datasets[d].accessor()
+            self._cache = (self._datasets[d].file, data)
+
+        return image_as_flex(dataset=data, index=i)
 
 
 def get_count_limit_from_meta(meta_file_name):
@@ -68,6 +130,61 @@ class FormatNexusEigerDLS(FormatNexusEiger):
             self._bit_depth_image = get_bit_depth_from_meta(self._meta)
         except Exception:
             self._bit_depth_image = 16
+
+    def _start(self):
+
+        # Read the file structure
+        self._reader = reader = NXmxReader(self._image_file)
+
+        # Only support 1 set of models at the moment
+        assert len(reader.entries) == 1, "Currently only supports 1 NXmx entry"
+        assert len(reader.entries[0].data) == 1, "Currently only supports 1 NXdata"
+        assert (
+            len(reader.entries[0].instruments) == 1
+        ), "Currently only supports 1 NXinstrument"
+        assert len(reader.entries[0].samples) == 1, "Currently only supports 1 NXsample"
+        assert (
+            len(reader.entries[0].samples[0].beams) == 1
+            or len(reader.entries[0].instruments[0].beams) == 1
+        ), "Currently only supports 1 NXbeam"
+
+        # Get the NXmx model objects
+        entry = reader.entries[0]
+        self.instrument = instrument = entry.instruments[0]
+        detector = instrument.detectors[0]
+        sample = entry.samples[0]
+        beam = sample.beams[0] if sample.beams else instrument.beams[0]
+        data = entry.data[0]
+
+        # Construct the models
+        self._beam_factory = BeamFactory(beam)
+        self._beam_factory.load_model(0)
+
+        self._setup_gonio_and_scan(sample, detector)
+
+        if self._scan_model:
+            array_range = self._scan_model.get_array_range()
+            num_images = array_range[1] - array_range[0]
+        else:
+            num_images = 0
+
+        if len(instrument.detector_groups) == 0:
+            assert (
+                len(reader.entries[0].instruments[0].detectors) == 1
+            ), "Currently only supports 1 NXdetector unless in a detector group"
+            assert (
+                len(reader.entries[0].instruments[0].detectors[0].modules) == 1
+            ), "Currently only supports 1 NXdetector_module unless in a detector group"
+
+            self._raw_data = DLSEigerDataFactory(data, max_size=num_images)
+            self._detector_model = DetectorFactory(
+                detector, self._beam_factory.model, shape=self._raw_data.shape()
+            ).model
+        else:
+            self._raw_data = detectorgroupdatafactory(data, instrument)
+            self._detector_model = DetectorFactoryFromGroup(
+                instrument, self._beam_factory.model
+            ).model
 
     def get_detector(self, index=None):
         # workaround for https://jira.diamond.ac.uk/browse/I03-365
