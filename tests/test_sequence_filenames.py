2cb12ea79d1dca80dcba5a8d3cfe44359e032b95
Richard Gildea <richard.gildea@diamond.ac.uk>
2020-10-13 21:45:36 +0100

    Refactor test
    * Parametrize test to avoid for loop inside test
    * Test all return values of template_regex function

--- tests/test_sequence_filenames.py
+++ tests/test_sequence_filenames.py
@@ -1,19 +1,21 @@
 from __future__ import absolute_import, division, print_function
 
+import pytest
+
 from dxtbx.sequence_filenames import template_regex
 
 
-def test_template_regex():
-    questions_answers = {
-        "foo_bar_001.img": "foo_bar_###.img",
-        "foo_bar001.img": "foo_bar###.img",
-        "foo_bar_1.8A_001.img": "foo_bar_1.8A_###.img",
-        "foo_bar.001": "foo_bar.###",
-        "foo_bar_001.img1000": "foo_bar_###.img1000",
-        "foo_bar_00001.img": "foo_bar_#####.img",
-        "image00001": "image#####",
-    }
-
-    for filename in questions_answers:
-        answer = template_regex(filename)
-        assert answer[0] == questions_answers[filename]
+@pytest.mark.parametrize(
+    "filename,template,digits",
+    [
+        ("foo_bar_001.img", "foo_bar_###.img", 1),
+        ("foo_bar001.img", "foo_bar###.img", 1),
+        ("foo_bar_1.8A_004.img", "foo_bar_1.8A_###.img", 4),
+        ("foo_bar.001", "foo_bar.###", 1),
+        ("foo_bar_002.img1000", "foo_bar_###.img1000", 2),
+        ("foo_bar_00005.img", "foo_bar_#####.img", 5),
+        ("image0010", "image####", 10),
+    ],
+)
+def test_template_regex(filename, template, digits):
+    assert template_regex(filename) == (template, digits)
