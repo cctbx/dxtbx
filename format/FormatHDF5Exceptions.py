df5e4fa4b470ff0601aafb15dcdd98cd0fd4dd50
Graeme Winter <graeme.winter@gmail.com>
2020-11-10 14:34:49 +0000

    Update format/FormatHDF5Exceptions.py
    Co-authored-by: Markus Gerstel <2102431+Anthchirp@users.noreply.github.com>

--- format/FormatHDF5Exceptions.py
+++ format/FormatHDF5Exceptions.py
@@ -2,33 +2,35 @@ import h5py
 
 # static enumerations for useless file types
 
-HDF5_UNKNOWN = -1
-HDF5_NXS_FILE = 0
-HDF5_DATA_FILE = 1
-HDF5_META_FILE = 2
-HDF5_MPCCD = 0
+from enum import Enum, auto
+class HDF5_FileType(Enum):
+    DATA=auto()
+    META=auto()
+    MPCCD=auto()
+    NXS=auto()
+    UNKNOWN=auto()
 
 
-def hdf5_file_type(hdf5_filename):
+def get_hdf5_file_type(hdf5_filename):
     """Open the named file, decide if it is probably not a useful one for
     processing..."""
 
     with h5py.File(hdf5_filename, "r") as f:
         top_level = set(f.keys())
         # case HDF5 data file
-        if top_level == set(["data"]):
-            return HDF5_DATA_FILE
+        if top_level == {"data"}:
+            return HDF5_FileType.DATA
 
         # case Diamond "meta" file
         if "config" in top_level and "frame_written" in top_level:
-            return HDF5_META_FILE
+            return HDF5_FileType.META
 
         # NeXus type files contain only /entry at the top (at the moment)
-        if top_level == set(["entry"]):
-            return HDF5_NXS_FILE
+        if top_level == {"entry"}:
+            return HDF5_FileType.NXS
 
         # SACLA MPCCD contains "metadata" and ... other stuff
         if "metadata" in top_level:
-            return HDF5_MPCCD
+            return HDF5_FileType.MPCCD
 
-    return HDF5_UNKNOWN
+    return HDF5_FileType.UNKNOWN
