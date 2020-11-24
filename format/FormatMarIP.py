c7718deb156f94816594f302b625fbc02a5276bb
Nicholas Devenish <ndevenish@gmail.com>
2020-11-24 19:26:12 +0000

    Move MARIPImage detectorbase into dxtbx

--- format/FormatMarIP.py
+++ format/FormatMarIP.py
@@ -4,10 +4,55 @@ from __future__ import absolute_import, division, print_function
 
 import sys
 
-from iotbx.detectors.marIP import MARIPImage
+from iotbx.detectors.detectorbase import DetectorImageBase
 
 from dxtbx import IncorrectFormatError
 from dxtbx.format.Format import Format
+from dxtbx.format.image import Mar345Adaptor
+
+
+class MARIPImage(DetectorImageBase):
+    def __init__(self, filename):
+        DetectorImageBase.__init__(self, filename)
+        self.adaptor = Mar345Adaptor(filename)
+        self.vendortype = "MARIP"
+
+    def readHeader(self):
+        self.parameters = {
+            "SIZE1": self.adaptor.size1(),
+            "SIZE2": self.adaptor.size2(),
+            "CCD_IMAGE_SATURATION": self.adaptor.overload(),
+            "PIXEL_SIZE": self.adaptor.pixel_size(),
+            "OSC_START": self.adaptor.osc_start(),
+            "DISTANCE": self.adaptor.distance(),
+            "WAVELENGTH": self.adaptor.wavelength(),
+            "BEAM_CENTER_X": self.beam_center_slow(),
+            "BEAM_CENTER_Y": self.beam_center_fast(),
+            "OSC_RANGE": self.adaptor.osc_range(),
+            "TWOTHETA": self.adaptor.twotheta(),
+            "DETECTOR_SN": 0,
+        }
+
+    def beam_center_slow(self):
+        return self.adaptor.size1() * self.adaptor.pixel_size() / 2.0
+
+    def beam_center_fast(self):
+        return self.adaptor.size2() * self.adaptor.pixel_size() / 2.0
+
+    def fileLength(self):
+        return 0
+
+    def getEndian(self):
+        return 0
+
+    def read(self):
+        self.bin_safe_set_data(self.adaptor.rawdata())
+
+    def dataoffset(self):
+        return 0
+
+    def integerdepth(self):
+        return 0
 
 
 class FormatMarIP(Format):
