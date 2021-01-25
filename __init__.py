dac0dbf75b823688c147b403e593b918315c00b5
Ben Williams <benjaminhwilliams@users.noreply.github.com>
2021-01-25 15:10:17 +0000

    Correct hdf5 plugins warning message (#282)
    - Change the deprecation schedule to 3.4
    
    Co-authored-by: Nicholas Devenish <ndevenish@gmail.com>

--- __init__.py
+++ __init__.py
@@ -24,7 +24,7 @@ if sys.version_info.major == 2:
 # conda_base folder, so it needs to be updated manually.
 
 # Warn if user is using an old hdf5-external-filter-plugins
-# Remove after DIALS 3.3 release branch is made
+# Remove after DIALS 3.4 release branch is made
 _legacy_plugin_path = libtbx.env.under_base(os.path.join("lib", "plugins"))
 _new_plugin_path = libtbx.env.under_base(os.path.join("lib", "hdf5", "plugin"))
 
@@ -34,7 +34,8 @@ if os.path.exists(_legacy_plugin_path) and not os.path.exists(_new_plugin_path):
     _hdf5_plugin_path = _legacy_plugin_path
     warnings.warn(
         "You are using an outdated version of the hdf5-external-filter-plugins package.\n"
-        "Please update your environment using 'conda install hdf5-external-filter-plugins'",
+        "Please update your environment using\n"
+        "'conda install \"hdf5-external-filter-plugins==0.1.0[build_number='>=5']\"'",
         UserWarning,
     )
 else:
