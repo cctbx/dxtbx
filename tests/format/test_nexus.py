1840224b2373020e8122dbff6000ba518f21104d
Ben Williams <benjaminhwilliams@users.noreply.github.com>
2021-01-29 18:23:31 +0000

    Silence a h5py warning, open file read-only
    Silence this deprecation warning:
    H5pyDeprecationWarning: The default file mode will change to 'r'
    (read-only) in h5py 3.0. To suppress this warning, pass the mode you
    need to h5py.File(), or set the global default
    h5.get_config().default_file_mode, or set the environment variable
    H5PY_DEFAULT_READONLY=1. Available modes are: 'r', 'r+', 'w', 'w-'/'x',
    'a'. See the docs for details.

--- tests/format/test_nexus.py
+++ tests/format/test_nexus.py
@@ -6,7 +6,7 @@ from dxtbx.format import nexus
 
 def test_data_factory_nxs(dials_data):
     nxs_file = dials_data("vmxi_thaumatin") / "image_15799.nxs"
-    with h5py.File(nxs_file) as fh:
+    with h5py.File(nxs_file, "r") as fh:
         data = fh["/entry/data"]
         data_factory = nexus.DataFactory(nexus.NXdata(data))
         for i in (0, data["data"].shape[0] - 1):
