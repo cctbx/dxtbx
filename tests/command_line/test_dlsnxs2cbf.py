3fe313d6926b52b106aff94d9a6eb92d2664571b
Richard Gildea <richard.gildea@diamond.ac.uk>
2021-02-01 09:00:48 +0000

    Add failing test for #294

--- tests/command_line/test_dlsnxs2cbf.py
+++ tests/command_line/test_dlsnxs2cbf.py
@@ -2,6 +2,11 @@ from __future__ import absolute_import, division, print_function
 
 import procrunner
 
+from scitbx.array_family import flex
+
+from dxtbx.format.FormatCBFMiniEigerDLS16MSN160 import FormatCBFMiniEigerDLS16MSN160
+from dxtbx.model.experiment_list import ExperimentListFactory
+
 
 def test_dlsnxs2cbf(dials_data, tmpdir):
     screen = dials_data("thaumatin_eiger_screen")
@@ -19,4 +24,19 @@ def test_dlsnxs2cbf(dials_data, tmpdir):
     # check files on disk
 
     for j in (1, 2, 3):
-        assert tmpdir.join("junk_%04d.cbf" % j).check()
+        assert tmpdir.join(f"junk_{j:04d}.cbf").check()
+
+    expts = ExperimentListFactory.from_filenames(
+        [str(tmpdir.join(f"junk_{j+1:04d}.cbf")) for j in range(3)]
+    )
+    assert all(
+        [
+            imgset.get_format_class() == FormatCBFMiniEigerDLS16MSN160
+            for imgset in expts.imagesets()
+        ]
+    )
+    assert [flex.sum(imgset.get_raw_data(0)[0]) for imgset in expts.imagesets()] == [
+        26990833,
+        27447160,
+        22271214,
+    ]
