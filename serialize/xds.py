cctbx/dxtbx

commit 836fe085b4f570c8aeb9ee8c6f5f2ec93c92e0eb

Bug fix: if eiger then EIGER not PILATUS (#292)

serialize/xds.py
@@ -129,7 +129,7 @@ def xds_detector_name(dxtbx_name):
     if "pilatus" in dxtbx_name:
         return "PILATUS"
     if "eiger" in dxtbx_name:
-        return "PILATUS"
+        return "EIGER"
     if "rayonix" in dxtbx_name:
         return "CCDCHESS"
     if "adsc" in dxtbx_name:

tests/serialize/test_xds.py
@@ -7,6 +7,7 @@ from libtbx.test_utils import approx_equal
 from rstbx.cftbx import coordinate_frame_helpers
 
 from dxtbx.imageset import ImageSetFactory
+from dxtbx.model.experiment_list import ExperimentListFactory
 from dxtbx.serialize import xds
 
 
@@ -127,3 +128,11 @@ SEGMENT_ORGX= 1075.00 SEGMENT_ORGY= 4973.67""",
     s = to_xds.XDS_INP()
     assert "UNTRUSTED_RECTANGLE= 0 488 3604 3800" in s
     assert "UNTRUSTED_RECTANGLE= 1976 2464 3604 3800" in s
+
+
+def test_vmxi_thaumatin(dials_data):
+    master_h5 = dials_data("vmxi_thaumatin") / "image_15799_master.h5"
+    expts = ExperimentListFactory.from_filenames([master_h5.strpath])
+    to_xds = xds.to_xds(expts[0].imageset)
+    s = to_xds.XDS_INP()
+    assert "DETECTOR=EIGER" in s

commit 5296e0224c592e3227c20f049e7b7628695bbb00

Fix missing SENSOR_THICKNESS= for EIGER
Bug introduced by #292

serialize/xds.py
@@ -307,7 +307,7 @@ class to_xds(object):
             % (detector, trusted[0] + 1, trusted[1])
         )
 
-        if detector == "PILATUS":
+        if detector in ("PILATUS", "EIGER"):
             result.append(
                 "SENSOR_THICKNESS= %.3f" % self.get_detector()[0].get_thickness()
             )

tests/serialize/test_xds.py
@@ -136,3 +136,4 @@ def test_vmxi_thaumatin(dials_data):
     to_xds = xds.to_xds(expts[0].imageset)
     s = to_xds.XDS_INP()
     assert "DETECTOR=EIGER" in s
+    assert "SENSOR_THICKNESS= 0.450" in s