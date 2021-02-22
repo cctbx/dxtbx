cctbx/dxtbx

commit bffa28ea73c9468d0c93c33a058822c96304ff5a

Move compression and decompression routines into dxtbx (#238)
Move CBF compression and decompression routines back into dxtbx once more
rehash of #228

boost_python/compression.cc
@@ -1,0 +1,158 @@
+#include "compression.h"
+
+typedef union {
+  char b[2];
+  short s;
+} union_short;
+
+typedef union {
+  char b[4];
+  int i;
+} union_int;
+
+void byte_swap_short(char *b) {
+  char c;
+  c = b[0];
+  b[0] = b[1];
+  b[1] = c;
+  return;
+}
+
+void byte_swap_int(char *b) {
+  char c;
+  c = b[0];
+  b[0] = b[3];
+  b[3] = c;
+  c = b[1];
+  b[1] = b[2];
+  b[2] = c;
+  return;
+}
+
+bool little_endian() {
+  int i = 0x1;
+  char b = ((union_int *) & i)[0].b[0];
+  if (b == 0) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+std::vector<char> dxtbx::boost_python::cbf_compress(const int *values,
+                                                    const std::size_t &sz) {
+  std::vector<char> packed(0);
+  int current = 0;
+  int delta, i;
+  unsigned int j;
+  bool le = little_endian();
+  short s;
+  char c;
+  char *b;
+
+  for (j = 0; j < sz; j++) {
+    delta = values[j] - current;
+
+    if ((-0x7f <= delta) && (delta < 0x80)) {
+      c = (char)delta;
+      packed.push_back(c);
+      current += delta;
+      continue;
+    }
+
+    packed.push_back(-0x80);
+
+    if ((-0xf777 <= delta) && (delta < 0x8000)) {
+      s = (short) delta;
+      b = ((union_short *) & s)[0].b;
+
+      if (!le) {
+        byte_swap_short(b);
+      }
+
+      packed.push_back(b[0]);
+      packed.push_back(b[1]);
+      current += delta;
+      continue;
+    }
+    s = - 0x8000;
+    b = ((union_short *) & s)[0].b;
+
+    if (!le) {
+      byte_swap_short(b);
+    }
+
+    packed.push_back(b[0]);
+    packed.push_back(b[1]);
+
+    if ((-0x7fffffff <= delta) && (delta < 0x80000000)) {
+      i = delta;
+      b = ((union_int *) & i)[0].b;
+
+      if (!le) {
+        byte_swap_int(b);
+      }
+
+      packed.push_back(b[0]);
+      packed.push_back(b[1]);
+      packed.push_back(b[2]);
+      packed.push_back(b[3]);
+      current += delta;
+      continue;
+    }
+  }
+
+  return packed;
+}
+
+void dxtbx::boost_python::cbf_decompress(const char *packed,
+                                         std::size_t packed_sz,
+                                         int *values) {
+  int current = 0;
+  unsigned int j = 0;
+  short s;
+  char c;
+  int i;
+  bool le = little_endian();
+
+  while (j < packed_sz) {
+    c = packed[j];
+    j += 1;
+
+    if (c != -128) {
+      current += c;
+      *values = current;
+      values++;
+      continue;
+    }
+
+    ((union_short *) & s)[0].b[0] = packed[j];
+    ((union_short *) & s)[0].b[1] = packed[j + 1];
+    j += 2;
+
+    if (!le) {
+      byte_swap_short((char *)&s);
+    }
+
+    if (s != -32768) {
+      current += s;
+      *values = current;
+      values++;
+      continue;
+    }
+
+    ((union_int *) & i)[0].b[0] = packed[j];
+    ((union_int *) & i)[0].b[1] = packed[j + 1];
+    ((union_int *) & i)[0].b[2] = packed[j + 2];
+    ((union_int *) & i)[0].b[3] = packed[j + 3];
+    j += 4;
+
+    if (!le) {
+      byte_swap_int((char *) & i);
+    }
+
+    current += i;
+    *values = current;
+    values++;
+  }
+}

SConscript
@@ -16,7 +16,19 @@ env_etc.dxtbx_common_includes = [
     env_etc.boost_include,
     env_etc.dxtbx_include,
 ] + env_etc.cbflib_common_includes
-env_etc.dxtbx_libs = ["tiff", "cbf"]
+
+Import("env_no_includes_boost_python_ext")
+env = env_no_includes_boost_python_ext.Clone()
+if libtbx.env.build_options.use_conda:
+    boost_python = get_boost_library_with_python_version(
+        "boost_python", env_etc.conda_libpath
+    )
+    env.Append(LIBPATH=env_etc.conda_libpath)
+    env_etc.dxtbx_common_includes.extend(env_etc.conda_cpppath)
+else:
+    boost_python = "boost_python"
+
+env_etc.dxtbx_libs = ["tiff", "cbf", boost_python]
 env_etc.dxtbx_hdf5_libs = ["hdf5"]
 env_etc.dxtbx_lib_paths = [
     env_etc.base_lib,
@@ -111,7 +123,11 @@ if not env_etc.no_boost_python and hasattr(env_etc, "boost_adaptbx_include"):
 
     env.SharedLibrary(
         target="#lib/dxtbx_ext",
-        source=["boost_python/to_ewald_sphere_helpers.cc", "boost_python/ext.cpp"],
+        source=[
+            "boost_python/to_ewald_sphere_helpers.cc",
+            "boost_python/ext.cpp",
+            "boost_python/compression.cc",
+        ],
         LIBS=env_etc.libs_python + env_etc.libm + env_etc.dxtbx_libs,
     )
 

boost_python/compression.h
@@ -1,0 +1,11 @@
+#ifndef DXTBX_COMPRESSION
+#define DXTBX_COMPRESSION
+#include <cstddef>
+#include <vector>
+
+namespace dxtbx { namespace boost_python {
+  void cbf_decompress(const char*, std::size_t, int*);
+  std::vector<char> cbf_compress(const int*, const std::size_t&);
+}}  // namespace dxtbx::boost_python
+
+#endif

boost_python/ext.cpp
@@ -9,6 +9,7 @@
 #include <vector>
 #include <limits>
 #include <dxtbx/error.h>
+#include "compression.h"
 
 namespace dxtbx { namespace boost_python {
 
@@ -165,6 +166,30 @@ namespace dxtbx { namespace boost_python {
     return result;
   }
 
+  scitbx::af::flex_int uncompress(const boost::python::object &packed,
+                                  const int &slow,
+                                  const int &fast) {
+    std::string strpacked = boost::python::extract<std::string>(packed);
+    std::size_t sz_buffer = strpacked.size();
+
+    scitbx::af::flex_int z((scitbx::af::flex_grid<>(slow, fast)),
+                           scitbx::af::init_functor_null<int>());
+    int *begin = z.begin();
+
+    dxtbx::boost_python::cbf_decompress(strpacked.c_str(), sz_buffer, begin);
+
+    return z;
+  }
+
+  PyObject *compress(const scitbx::af::flex_int z) {
+    const int *begin = z.begin();
+    std::size_t sz = z.size();
+
+    std::vector<char> packed = dxtbx::boost_python::cbf_compress(begin, sz);
+
+    return PyBytes_FromStringAndSize(&*packed.begin(), packed.size());
+  }
+
   void init_module() {
     using namespace boost::python;
     def("read_uint8", read_uint8, (arg("file"), arg("count")));
@@ -176,6 +201,8 @@ namespace dxtbx { namespace boost_python {
     def("read_int32", read_int32, (arg("file"), arg("count")));
     def("read_float32", read_float32, (arg("file"), arg("count")));
     def("is_big_endian", is_big_endian);
+    def("uncompress", &uncompress, (arg_("packed"), arg_("slow"), arg_("fast")));
+    def("compress", &compress);
   }
 
   void export_to_ewald_sphere_helpers();

format/FormatCBFFullPilatus.py
@@ -6,9 +6,9 @@ import binascii
 import re
 import sys
 
-from cbflib_adaptbx import uncompress
 from cctbx.eltbx import attenuation_coefficient
 
+from dxtbx.ext import uncompress
 from dxtbx.format.FormatCBFFull import FormatCBFFull
 from dxtbx.format.FormatPilatusHelpers import determine_pilatus_mask
 from dxtbx.format.FormatPilatusHelpers import get_vendortype as gv

format/FormatCBFMini.py
@@ -13,13 +13,12 @@ import sys
 import pycbf
 
 from boost_adaptbx.boost.python import streambuf
-from cbflib_adaptbx import uncompress
 from cctbx import factor_ev_angstrom
 from cctbx.eltbx import attenuation_coefficient
 from iotbx.detectors.pilatus_minicbf import PilatusImage
 from scitbx.array_family import flex
 
-from dxtbx.ext import read_int32
+from dxtbx.ext import read_int32, uncompress
 from dxtbx.format.FormatCBF import FormatCBF
 from dxtbx.format.FormatCBFMiniPilatusHelpers import get_pilatus_timestamp
 from dxtbx.format.FormatCBFMultiTile import cbf_wrapper

format/FormatCBFMiniADSCHF4M.py
@@ -12,10 +12,10 @@ import re
 import sys
 import time
 
-from cbflib_adaptbx import uncompress
 from cctbx.eltbx import attenuation_coefficient
 from iotbx.detectors.adsc_minicbf import ADSCHF4MImage
 
+from dxtbx.ext import uncompress
 from dxtbx.format.FormatCBFMini import FormatCBFMini
 from dxtbx.model import ParallaxCorrectedPxMmStrategy
 

format/FormatCBFMiniEiger.py
@@ -6,10 +6,10 @@ import binascii
 import os
 import sys
 
-from cbflib_adaptbx import uncompress
 from cctbx.eltbx import attenuation_coefficient
 from iotbx.detectors.eiger_minicbf import EigerCBFImage
 
+from dxtbx.ext import uncompress
 from dxtbx.format.FormatCBFMini import FormatCBFMini
 from dxtbx.format.FormatCBFMiniPilatusHelpers import get_pilatus_timestamp
 from dxtbx.format.FormatPilatusHelpers import determine_eiger_mask

commit d1f9583f8a683fea651ecb6ce173be3d042d0fdd

Fix data-corruption error in CBF compression (#314)
The byte-offset compression introduced by #238, #228 was incorrect, causing
problems for anything _writing_ cbf files... so dlsnsx2cbf (and probably
dials.merge_cbf).

- 0xf777 -> 0x7fff as one greater than -0x8000 - values with too large a
  negative value were being incorrectly encoded
- treatment of the fall through / last case in compression for similarity with
  decompression layout
- more thorough testing in particular tests with negative delta of magnitude
  greater than 0x8000
- test added which verifies that the HDF5 and CBF representation of the data
  from dlsnxs2cbf are the same

boost_python/compression.cc
@@ -1,4 +1,5 @@
 #include "compression.h"
+#include <assert.h>
 
 typedef union {
   char b[2];
@@ -62,7 +63,7 @@ std::vector<char> dxtbx::boost_python::cbf_compress(const int *values,
 
     packed.push_back(-0x80);
 
-    if ((-0xf777 <= delta) && (delta < 0x8000)) {
+    if ((-0x7fff <= delta) && (delta < 0x8000)) {
       s = (short)delta;
       b = ((union_short *)&s)[0].b;
 
@@ -75,6 +76,7 @@ std::vector<char> dxtbx::boost_python::cbf_compress(const int *values,
       current += delta;
       continue;
     }
+
     s = -0x8000;
     b = ((union_short *)&s)[0].b;
 
@@ -85,21 +87,20 @@ std::vector<char> dxtbx::boost_python::cbf_compress(const int *values,
     packed.push_back(b[0]);
     packed.push_back(b[1]);
 
-    if ((-0x7fffffff <= delta) && (delta < 0x80000000)) {
-      i = delta;
-      b = ((union_int *)&i)[0].b;
+    assert((-0x7fffffff <= delta) && (delta < 0x80000000));
 
-      if (!le) {
-        byte_swap_int(b);
-      }
+    i = delta;
+    b = ((union_int *)&i)[0].b;
 
-      packed.push_back(b[0]);
-      packed.push_back(b[1]);
-      packed.push_back(b[2]);
-      packed.push_back(b[3]);
-      current += delta;
-      continue;
+    if (!le) {
+      byte_swap_int(b);
     }
+
+    packed.push_back(b[0]);
+    packed.push_back(b[1]);
+    packed.push_back(b[2]);
+    packed.push_back(b[3]);
+    current += delta;
   }
 
   return packed;
@@ -119,7 +120,7 @@ void dxtbx::boost_python::cbf_decompress(const char *packed,
     c = packed[j];
     j += 1;
 
-    if (c != -128) {
+    if (c != -0x80) {
       current += c;
       *values = current;
       values++;
@@ -134,7 +135,7 @@ void dxtbx::boost_python::cbf_decompress(const char *packed,
       byte_swap_short((char *)&s);
     }
 
-    if (s != -32768) {
+    if (s != -0x8000) {
       current += s;
       *values = current;
       values++;

tests/command_line/test_dlsnxs2cbf.py
@@ -1,24 +1,41 @@
+import h5py
+import numpy as np
 import procrunner
 import pytest
+
+from dxtbx.format.FormatCBFMiniEigerDLS16MSN160 import FormatCBFMiniEigerDLS16MSN160
+from dxtbx.model.experiment_list import ExperimentListFactory
 
 
 @pytest.mark.xfail(
     "os.name == 'nt'", reason="crashes python process on Windows", run=False
 )
-def test_dlsnxs2cbf(dials_data, tmpdir):
+def test_dlsnxs2cbf(dials_data, tmp_path):
     screen = dials_data("thaumatin_eiger_screen")
     master = screen.join("Therm_6_1_master.h5")
     result = procrunner.run(
-        ["dxtbx.dlsnxs2cbf", master, "junk_%04d.cbf"], working_directory=tmpdir
+        ["dxtbx.dlsnxs2cbf", master, "junk_%04d.cbf"], working_directory=tmp_path
     )
     assert not result.returncode and not result.stderr
 
-    expected_output = "\n".join("junk_%04d.cbf" % j for j in (1, 2, 3))
+    output_files = ["junk_%04d.cbf" % j for j in (1, 2, 3)]
+    for file in output_files:
+        assert file.encode("latin-1") in result.stdout
+        assert tmp_path.joinpath(file).is_file()
 
-    for record in expected_output.split("\n"):
-        assert record.strip().encode("latin-1") in result.stdout, record
+    expts = ExperimentListFactory.from_filenames(
+        str(tmp_path / file) for file in output_files
+    )
+    assert all(
+        imgset.get_format_class() == FormatCBFMiniEigerDLS16MSN160
+        for imgset in expts.imagesets()
+    )
 
-    # check files on disk
-
-    for j in (1, 2, 3):
-        assert tmpdir.join("junk_%04d.cbf" % j).check()
+    with h5py.File(master) as fh:
+        for i, imgset in enumerate(expts.imagesets()):
+            original = fh["/entry/data/data_000001"][i][()]
+            sel = np.where(original < original.max())
+            np.testing.assert_equal(
+                fh["/entry/data/data_000001"][i][sel],
+                imgset.get_raw_data(0)[0].as_numpy_array()[sel],
+            )

tests/test_compression.py
@@ -1,0 +1,15 @@
+from scitbx.array_family import flex
+
+from dxtbx.ext import compress, uncompress
+
+
+def test_compress_decompress():
+    x, y = 10, 10
+
+    data = flex.int(x * y, 1)
+    data[10] = 44369
+    data[11] = 214
+    compressed = compress(data)
+    uncompressed = uncompress(compressed, x, y)
+
+    assert list(data) == list(uncompressed)