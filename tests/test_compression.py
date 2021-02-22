b45faa79daed8eebae015275af410cd9308c4a96
Graeme Winter <graeme.winter@gmail.com>
2021-02-22 15:09:08 +0000

    Fix (de)compression
    Adds tests as well
    
    And fixes #294
    
    Previously the code was working so pretty bad it got this far

---
+++ tests/test_compression.py
@@ -0,0 +1,19 @@
+from scitbx.array_family import flex
+
+from dxtbx.ext import compress, uncompress
+
+
+def test_compress_decompress():
+    x, y = 10, 10
+
+    original = [1 for n in range(x * y)]
+
+    original[10] = 44369
+    original[11] = 214
+
+    data = flex.int(original)
+    compressed = compress(data)
+    uncompressed = uncompress(compressed, x, y)
+
+    for d, u in zip(data, uncompressed):
+        assert d == u
