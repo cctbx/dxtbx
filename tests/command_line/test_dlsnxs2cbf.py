a3c3231bbf9ee52b17d99db24c13122eb8ca1ae4
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2021-02-22 15:55:01 +0000

    tmpdir -> tmp_path, lists -> iterators

--- tests/command_line/test_dlsnxs2cbf.py
+++ tests/command_line/test_dlsnxs2cbf.py
@@ -10,32 +10,25 @@ from dxtbx.model.experiment_list import ExperimentListFactory
 @pytest.mark.xfail(
     "os.name == 'nt'", reason="crashes python process on Windows", run=False
 )
-def test_dlsnxs2cbf(dials_data, tmpdir):
+def test_dlsnxs2cbf(dials_data, tmp_path):
     screen = dials_data("thaumatin_eiger_screen")
     master = screen.join("Therm_6_1_master.h5")
     result = procrunner.run(
-        ["dxtbx.dlsnxs2cbf", master, "junk_%04d.cbf"], working_directory=tmpdir
+        ["dxtbx.dlsnxs2cbf", master, "junk_%04d.cbf"], working_directory=tmp_path
     )
     assert not result.returncode and not result.stderr
 
-    expected_output = "\n".join("junk_%04d.cbf" % j for j in (1, 2, 3))
-
-    for record in expected_output.split("\n"):
-        assert record.strip().encode("latin-1") in result.stdout, record
-
-    # check files on disk
-
-    for j in (1, 2, 3):
-        assert tmpdir.join(f"junk_{j:04d}.cbf").check()
+    output_files = ["junk_%04d.cbf" % j for j in (1, 2, 3)]
+    for file in output_files:
+        assert file.encode("latin-1") in result.stdout
+        assert tmp_path.joinpath(file).is_file()
 
     expts = ExperimentListFactory.from_filenames(
-        [str(tmpdir.join(f"junk_{j+1:04d}.cbf")) for j in range(3)]
+        str(tmp_path / file) for file in output_files
     )
     assert all(
-        [
-            imgset.get_format_class() == FormatCBFMiniEigerDLS16MSN160
-            for imgset in expts.imagesets()
-        ]
+        imgset.get_format_class() == FormatCBFMiniEigerDLS16MSN160
+        for imgset in expts.imagesets()
     )
 
     with h5py.File(master) as fh:
