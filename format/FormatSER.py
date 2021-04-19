4f478010f6b345f5844930a657ca68de6da265c6
David Waterman <dgwaterman@gmail.com>
2021-04-19 14:36:15 +0100

    Get extra metadata from .emi sidecar file, if present.
    This uses read_emi from openNCEM (https://github.com/ercius/openNCEM)

--- format/FormatSER.py
+++ format/FormatSER.py
@@ -8,6 +8,11 @@ from __future__ import absolute_import, division, print_function
 import os
 import re
 import struct
+import xml.etree.ElementTree as ET
+from math import sqrt
+from pathlib import Path
+
+import numpy as np
 
 from boost_adaptbx.boost.python import streambuf
 from scitbx.array_family import flex
@@ -16,6 +21,156 @@ import dxtbx.ext
 from dxtbx import IncorrectFormatError
 from dxtbx.format.Format import Format
 from dxtbx.format.FormatMultiImage import FormatMultiImage
+
+
+# The read_emi and _parseEntry_emi functions are taken from openNCEM project
+# under the terms of the MIT license.
+#
+# Copyright 2019 the Ncempy developers
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+def read_emi(filename):
+    """Read the meta data from an emi file.
+    Parameters
+    ----------
+        filename: str or pathlib.Path
+            Path to the emi file.
+    Returns
+    -------
+        : dict
+            Dictionary of experimental metadata stored in the EMI file.
+    """
+
+    # check filename type
+    if isinstance(filename, str):
+        pass
+    elif isinstance(filename, Path):
+        filename = str(filename)
+    else:
+        raise TypeError("Filename is supposed to be a string or pathlib.Path")
+
+    # try opening the file
+    try:
+        # open file for reading bytes, as binary and text are intermixed
+        with open(filename, "rb") as f_emi:
+            emi_data = f_emi.read()
+    except IOError:
+        print('Error reading file: "{}"'.format(filename))
+        raise
+    except Exception:
+        raise
+
+    # dict to store _emi stuff
+    _emi = {}
+
+    # need anything readable from <ObjectInfo> to </ObjectInfo>
+    # collect = False
+    # data = b''
+    # for line in f_emi:
+    #    if b'<ObjectInfo>' in line:
+    #        collect = True
+    #    if collect:
+    #        data += line.strip()
+    #    if b'</ObjectInfo>' in line:
+    #        collect = False
+
+    # close the file
+    # f_emi.close()
+
+    metaStart = emi_data.find(b"<ObjectInfo>")
+    metaEnd = emi_data.find(
+        b"</ObjectInfo>"
+    )  # need to add len('</ObjectInfo>') = 13 to encompass this final tag
+
+    root = ET.fromstring(emi_data[metaStart : metaEnd + 13])
+
+    # strip of binary stuff still around
+    # data = data.decode('ascii', errors='ignore')
+    # matchObj = re.search('<ObjectInfo>(.+?)</ObjectInfo', data)
+    # try:
+    #    data = matchObj.group(1)
+    # except:
+    #    raise RuntimeError('Could not find _emi metadata in specified file.')
+
+    # parse metadata as xml
+    # root = ET.fromstring('<_emi>' + data + '</_emi>')
+
+    # single items
+    _emi["Uuid"] = root.findtext("Uuid")
+    _emi["AcquireDate"] = root.findtext("AcquireDate")
+    _emi["Manufacturer"] = root.findtext("Manufacturer")
+    _emi["DetectorPixelHeight"] = root.findtext("DetectorPixelHeight")
+    _emi["DetectorPixelWidth"] = root.findtext("DetectorPixelWidth")
+
+    # Microscope Conditions
+    grp = root.find("ExperimentalConditions/MicroscopeConditions")
+
+    for elem in grp:
+        _emi[elem.tag] = _parseEntry_emi(elem.text)
+
+    # Experimental Description
+    grp = root.find("ExperimentalDescription/Root")
+
+    for elem in grp:
+        _emi[
+            "{} [{}]".format(elem.findtext("Label"), elem.findtext("Unit"))
+        ] = _parseEntry_emi(elem.findtext("Value"))
+
+    # AcquireInfo
+    grp = root.find("AcquireInfo")
+
+    for elem in grp:
+        _emi[elem.tag] = _parseEntry_emi(elem.text)
+
+    # DetectorRange
+    grp = root.find("DetectorRange")
+
+    for elem in grp:
+        _emi["DetectorRange_" + elem.tag] = _parseEntry_emi(elem.text)
+
+    return _emi
+
+
+def _parseEntry_emi(value):
+    """Auxiliary function to parse string entry to int, float or np.string_().
+    Parameters
+    ----------
+        value : str
+            String containing an int, float or string.
+    Returns
+    -------
+        : int or float or str
+            Entry value as int, float or string.
+    """
+
+    # try to parse as int
+    try:
+        p = int(value)
+    except ValueError:
+        # if not int, then try float
+        try:
+            p = float(value)
+        except ValueError:
+            # if neither int nor float, stay with string
+            p = np.string_(str(value))
+
+    return p
 
 
 class FormatSER(Format):
@@ -99,6 +254,12 @@ class FormatSER(Format):
             hd["ArraySizeX"] = struct.unpack("<I", f.read(4))[0]
             hd["ArraySizeY"] = struct.unpack("<I", f.read(4))[0]
 
+        try:
+            emi = read_emi(os.path.splitext(image_file)[0] + ".emi")
+            hd.update(emi)
+        except FileNotFoundError:
+            pass
+
         return hd
 
     def _start(self):
@@ -112,24 +273,26 @@ class FormatSER(Format):
         return self._goniometer_factory.known_axis((0, -1, 0))
 
     def _detector(self):
-        """Dummy Ceta-D detector"""
+        """Dummy detector"""
 
         image_size = (
             self._header_dictionary["ArraySizeX"],
             self._header_dictionary["ArraySizeY"],
         )
-        if image_size == (4096, 4096):
-            pixel_size = 0.014, 0.014
-            binning = 1
+
+        # Assumptions relevant for either Ceta or Falcon
+        pixel_size = 0.014, 0.014
+        binning = 1
         if image_size == (2048, 2048):
             pixel_size = 0.028, 0.028
             binning = 2
+        binning = self._header_dictionary.get("Binning", binning)
+        saturation = 8000 * binning ** 2
+        trusted_range = (-1000, saturation)
 
-        distance = 2000
-        # For Ceta binning=1, complete saturation occurs ~8000 counts. Negative values
-        # are common and may require setting a negative pedestal to make most
-        # pixels positive.
-        trusted_range = (-1000, 8000 * binning ** 2)
+        distance = float(self._header_dictionary.get("Camera length [m]", 2)) * 1000
+
+        # Set beam to the centre of the image
         beam_centre = [(p * i) / 2 for p, i in zip(pixel_size, image_size)]
         d = self._detector_factory.simple(
             "PAD",
@@ -141,16 +304,37 @@ class FormatSER(Format):
             image_size,
             trusted_range,
         )
-        # The gain of the CetaD is thought to be > 26.0 at 200 keV. That of
-        # the standard Ceta is about 7.
+
+        # Assume Ceta-D gain unless known to be a Falcon (this neglects standard
+        # Ceta gain, which is said to be about 7.0).
+        if b"falcon" in self._header_dictionary["CameraNamePath"].lower():
+            gain = 1.0
+        else:
+            gain = 26.0
+
         for p in d:
-            p.set_gain(26)
+            p.set_gain(gain)
         return d
 
     def _beam(self):
-        """Dummy unpolarized beam, energy 200 keV"""
+        """Unpolarized beam, default energy 200 keV unless in metadata"""
 
-        wavelength = 0.02508
+        def cal_wavelength(V0):
+            h = 6.626e-34  # Js, Planck's constant
+            m = 9.109e-31  # kg, electron mass
+            e = 1.6021766208e-19  # C, electron charge
+            c = 3e8  # m/s^2, speed
+
+            # Default to e- wavelength at 200 keV if voltage set to zero
+            if V0 == 0:
+                V0 = 200000
+            return (
+                h / sqrt(2 * m * e * V0 * (1 + e * V0 / (2 * m * c * c))) * 1e10
+            )  # return wavelength in Angstrom
+
+        wavelength = cal_wavelength(
+            self._header_dictionary.get("AcceleratingVoltage", 200000)
+        )
         return self._beam_factory.make_polarized_beam(
             sample_to_source=(0.0, 0.0, 1.0),
             wavelength=wavelength,
