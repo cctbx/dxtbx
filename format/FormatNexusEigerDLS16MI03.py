0a3afdbf77f0eb211616a67ca77b5694c78da9c1
Richard Gildea <rjgildea@users.noreply.github.com>
2021-04-23 16:13:29 +0100

    Temporarily mask bad modules on DLS I03 Eiger (#348)
    Add new format class for the Eiger 2XE 16M on DLS I03
    
    Only active for data collected between 20210422 and 20210503 (inclusive).
    Mask out two known bad modules in full detector mode, and one module in
    ROI mode.

---
+++ format/FormatNexusEigerDLS16MI03.py
@@ -0,0 +1,49 @@
+import calendar
+
+import h5py
+
+from dxtbx.format.FormatCBFMiniPilatusHelpers import get_pilatus_timestamp
+from dxtbx.format.FormatNexusEigerDLS16M import FormatNexusEigerDLS16M
+from dxtbx.format.nexus import h5str
+
+
+class FormatNexusEigerDLS16MI03(FormatNexusEigerDLS16M):
+    """Format class for the Eiger 2XE 16M on DLS I03
+
+    Only active for data collected between 20210422 and 20210503 (inclusive).
+    Mask out two known bad modules in full detector mode, and one module in
+    ROI mode.
+    """
+
+    @staticmethod
+    def understand(image_file):
+        # Get the file handle
+        with h5py.File(image_file, "r") as fh:
+            name = h5str(FormatNexusEigerDLS16M.get_instrument_name(fh))
+            if name and name.upper() in {"DIAMOND BEAMLINE I03", "DLS I03"}:
+                timestamp = get_pilatus_timestamp(
+                    h5str(fh["/entry/start_time"][()]).rstrip("Z")
+                )
+                return timestamp > calendar.timegm(
+                    (2021, 4, 22, 0, 0, 0)
+                ) and timestamp < calendar.timegm((2021, 5, 4, 0, 0, 0))
+
+    def _start(self):
+        super()._start()
+        nx = 1028  # module pixels x
+        ny = 512  # module pixels y
+        dx = 12  # module gap size
+        dy = 38  # module gap size
+        if self._detector_model[0].get_image_size() == (4148, 4362):
+            # Full detector mode
+            bad_modules = {(2, 5), (3, 5)}
+        else:
+            # ROI detector mode
+            bad_modules = {(1, 3)}
+        for i_col, i_row in bad_modules:
+            self._detector_model[0].add_mask(
+                (nx + dx) * i_col,
+                (ny + dy) * i_row,
+                (nx + dx) * i_col + nx,
+                (ny + dy) * i_row + ny,
+            )
---
+++ tests/format/test_FormatNexusEigerDLS16MI03.py
@@ -0,0 +1,34 @@
+import os
+
+import pytest
+
+from dxtbx.format.FormatNexusEigerDLS16MI03 import FormatNexusEigerDLS16MI03
+from dxtbx.model.experiment_list import ExperimentListFactory
+
+dials = pytest.importorskip("dials")
+pytest.importorskip("h5py")
+
+
+@pytest.mark.parametrize(
+    "master_h5,masked_count",
+    [
+        (
+            "/dls/i03/data/2021/cm28170-2/TestProteinaseK/protk_4/protk_4_5.nxs",
+            1052672,
+        ),  # full detector mode
+        (
+            "/dls/i03/data/2021/cm28170-2/xraycentring/TestProteinaseK/protk_5/protk_5_15.nxs",
+            526336,
+        ),  # ROI mode
+    ],
+)
+@pytest.mark.skipif(
+    not os.access("/dls/i03/data/2021/cm28170-2/TestProteinaseK/", os.R_OK),
+    reason="Test images not available",
+)
+def test_masked_i03(master_h5, masked_count):
+    assert FormatNexusEigerDLS16MI03.understand(master_h5)
+    expts = ExperimentListFactory.from_filenames([master_h5])
+    assert (
+        expts[0].detector[0].get_untrusted_rectangle_mask().count(False) == masked_count
+    )
