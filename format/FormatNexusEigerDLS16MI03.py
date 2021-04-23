777610ae64714c1ecb3b9699d400cadf73f2524f
Richard Gildea <richard.gildea@diamond.ac.uk>
2021-04-23 14:01:25 +0100

    Timestamp check in understand()
    * Only mask out in full detector mode
    * More explicit variable names

--- format/FormatNexusEigerDLS16MI03.py
+++ format/FormatNexusEigerDLS16MI03.py
@@ -7,25 +7,19 @@ from dxtbx.format.FormatNexusEigerDLS16M import FormatNexusEigerDLS16M
 from dxtbx.format.nexus import h5str
 
 
-def get_start_time(filename):
-    with h5py.File(filename, mode="r") as f:
-        timestamp = h5str(f["/entry/start_time"][()]).rstrip("Z")
-    return get_pilatus_timestamp(timestamp)
-
-
 class FormatNexusEigerDLS16MI03(FormatNexusEigerDLS16M):
     @staticmethod
     def understand(image_file):
         # Get the file handle
-        with h5py.File(image_file, "r") as handle:
-            name = h5str(FormatNexusEigerDLS16M.get_instrument_name(handle))
-            return name and name.upper() in {"DIAMOND BEAMLINE I03", "DLS I03"}
-
-    def __init__(self, image_file, **kwargs):
-        """Initialise the image structure from the given file."""
-
-        super().__init__(image_file, **kwargs)
-        self._image_file = image_file
+        with h5py.File(image_file, "r") as fh:
+            name = h5str(FormatNexusEigerDLS16M.get_instrument_name(fh))
+            if name and name.upper() in {"DIAMOND BEAMLINE I03", "DLS I03"}:
+                timestamp = get_pilatus_timestamp(
+                    h5str(fh["/entry/start_time"][()]).rstrip("Z")
+                )
+                return timestamp > calendar.timegm(
+                    (2021, 4, 22, 0, 0, 0)
+                ) and timestamp < calendar.timegm((2021, 5, 4, 0, 0, 0))
 
     def _start(self):
         super()._start()
@@ -33,14 +27,14 @@ class FormatNexusEigerDLS16MI03(FormatNexusEigerDLS16M):
         ny = 512  # module pixels y
         dx = 12  # module gap size
         dy = 38  # module gap size
-        timestamp = get_start_time(self._image_file)
-        if timestamp > calendar.timegm(
-            (2021, 4, 22, 0, 0, 0)
-        ) and timestamp < calendar.timegm((2021, 5, 4, 0, 0, 0)):
+        if self._detector_model[0].get_image_size() == (4148, 4362):
             # 2021 run 2
             # Full module
             bad_modules = {(2, 5), (3, 5)}
-            for i, j in bad_modules:
+            for i_col, i_row in bad_modules:
                 self._detector_model[0].add_mask(
-                    (nx + dx) * i, (ny + dy) * j, (nx + dx) * i + nx, (ny + dy) * j + ny
+                    (nx + dx) * i_col,
+                    (ny + dy) * i_row,
+                    (nx + dx) * i_col + nx,
+                    (ny + dy) * i_row + ny,
                 )
