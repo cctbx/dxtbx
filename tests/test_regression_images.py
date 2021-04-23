107b62c5c392acb4740856868dd17b63b137351d
Markus Gerstel <2102431+Anthchirp@users.noreply.github.com>
2021-04-23 08:57:06 +0100

    Explicitly enumerate regression images (#346)
    Major overhaul of the test_regression_images test.
    
    * Fix the failing test with the eBIC sample file
    * Re-enable the SACLA MPCCD test (skipped since 2016), as
      its error condition is also covered by the fix for the eBIC file.
    * Explicitly enumerate all files under test, leading to a correct
      skip count when dials_regression is not present, and expose
      opportunities for future test count reduction.
    * Hugely simplify the pytest fixture setup to make it understandable
    * Modernize code
    * Remove tests that have been skipped since 2013
      These tests have not been run for a very long time.
      While the files were used in the test_format_class_API_assumptions
      test, they do not add anything there. The code paths visited are
      already covered by a number of other files.

--- tests/test_regression_images.py
+++ tests/test_regression_images.py
@@ -1,20 +1,15 @@
 """
 Image reading tests against the dials_regression suite
 """
-from __future__ import absolute_import, division, print_function
 
 import bz2
 import gzip
 import os
-import re
 import shutil
-import sys
+from pathlib import Path
 
-import py.path
 import pytest
-import six
 
-import libtbx.load_env
 import scitbx.matrix
 from rstbx.slip_viewer.slip_viewer_image_factory import SlipViewerImageFactory
 from scitbx.array_family import flex
@@ -23,153 +18,144 @@ import dxtbx.conftest
 import dxtbx.format.Registry
 from dxtbx.util import get_url_scheme
 
-if sys.version_info[:2] >= (3, 6):
-    from pathlib import Path
-else:
-    from pathlib2 import Path
-
 try:
     import h5py
-except ImportError:
+except ModuleNotFoundError:
     h5py = None
 
+try:
+    import xfel
+except ImportError:
+    # Although ModuleNotFoundError is correct, cctbx may raise an ImportError
+    # when the xfel extension module is not found.
+    xfel = None
 
-def _generate_all_test_images():
-    """Internal convenience function to generates a list of test images.
+_files = (
+    "ALS_1231/q315r_lyso_1_001.img",
+    "ALS_422/lyso_041013a_1_001.img",
+    "ALS_501/als501_q4_1_001.img",
+    "ALS_733/200mMNaCl5pcGlyc_400.edf",
+    "ALS_821/q210_lyso_1_101.img",
+    "ALS_831/q315r_lyso_001.img",
+    "APS_14BMC/q315_1_001.img",
+    "APS_17ID/q210_1_001.img",
+    "APS_19ID/q315_unbinned_a.0001.img",
+    "APS_19ID/t1.0001.img.bz2",
+    "APS_22ID/mar300.0001",
+    "APS_23IDD/mar300_1_E1.0001",
+    "APS_24IDC/pilatus_1_0001.cbf",
+    "APS_24IDC/q315_1_001.img",
+    "APS_24IDE_test/thaum-12_1_0001.cbf",
+    "APS_24IDE_test/thaum-12_1_0002.cbf",
+    "APS_24IDE_test/thaum-12_1_0003.cbf",
+    "APS_24IDE_test/thaum-12_1_0004.cbf",
+    "APS_24IDE_test/thaum-12_1_0005.cbf",
+    "APS_24IDE_test/thaum-12_1_0006.cbf",
+    "APS_24IDE_test/thaum-12_1_0007.cbf",
+    "APS_24IDE_test/thaum-12_1_0008.cbf",
+    "APS_24IDE_test/thaum-12_1_0009.cbf",
+    "APS_24IDE_test/thaum-12_1_0010.cbf",
+    "Bruker_PHOTON_II/dan_01_0001.sfrm",
+    "CLS1_08ID1/mar225_2_E0_0001.img",
+    "DESY_BW7B/mar345_01_001.mar2300",
+    "DESY_ID141/q210_2_001.img",
+    "DLS_I02/X4_wide_M1S4_1_0001.cbf",
+    "DLS_I04/grid_full_cbf_0005.cbf",
+    "DLS_I19/I19_P300k_00001.cbf",
+    "DLS_I23/I23_P12M_alpha_0001.cbf",
+    "DLS_I23/germ_13KeV_0001.cbf",
+    "DLS_I24_stills/still_0001.cbf",
+    "DLS_eBIC/nanB_4_singleframe_1.ser.bz2",
+    "DTREK_home_lab/s01f0001.osc",
+    "ESRF_BM14/mar165_001.mccd",
+    "ESRF_BM14/mar225_1_001.mccd",
+    "ESRF_ID231/q315r_7_001.img",
+    "ESRF_ID29/trypsin_1_0001.cbf",
+    "LCLS_cspad_nexus/idx-20130301060858401.cbf",
+    "LCLS_cspad_nexus/idx-20130301060858601.cbf",
+    "LCLS_cspad_nexus/idx-20130301060858701.cbf",
+    "LCLS_cspad_nexus/idx-20130301060858801.cbf",
+    "LCLS_jungfrau/jungfrau_multipanel.cbf",
+    "MLFSOM_simulation/fake_00001.img",
+    "MacScience/reallysurprise_001.ipf",
+    "RAXIS-HTC/test1_lysozyme_0111060001.osc",
+    "RigakuA200/XV10001.img",
+    "SACLA_MPCCD_Cheetah/run266702-0-subset.h5",
+    "SLS_Eiger_16M_as_CBF/insu_with_bs_labelit_0001.cbf",
+    "SLS_Eiger_16M_as_CBF/insu_with_bs_labelit_0901.cbf",
+    "SLS_X06SA/mar225_2_001.img",
+    "SLS_X06SA/pilatus6m_1_00001.cbf",
+    "SPring8_ADSC_SN916/Xtal17-2phi_3_015.cbf",
+    "SPring8_BL12B2_MX225HE/lys001_000001.img",
+    "SPring8_BL12B2_MX225HE/lys001_000091.img",
+    "SPring8_BL26B1_Raxis5/raxis5_000001.img",
+    "SPring8_BL26B1_Raxis5/raxis5_000091.img",
+    "SPring8_BL26B1_SaturnA200/A200_000001.img",
+    "SPring8_BL26B1_SaturnA200/A200_000002.img",
+    "SPring8_BL26B2_MX225/2sec_Al200um_000001.img",
+    "SPring8_BL26B2_MX225/2sec_Al200um_000090.img",
+    "SPring8_BL32XU/lys_00001.img",
+    "SPring8_BL32XU/rayonix225_0001.img",
+    "SPring8_BL32XU/rayonix225hs_0001.img",
+    "SPring8_BL32XU_MX225HS/ds_000001.img",
+    "SPring8_BL32XU_MX225HS/ds_000045.img",
+    "SPring8_BL38B1_MX225HE/bl38b1_001.img",
+    "SPring8_BL38B1_MX225HE/bl38b1_090.img",
+    "SPring8_BL41XU_PILATUS3_6M/data1_000001.cbf",
+    "SPring8_BL41XU_PILATUS3_6M/data1_000901.cbf",
+    "SPring8_BL44XU_MX300HE/bl44xu_lys_000001.img",
+    "SPring8_BL44XU_MX300HE/bl44xu_lys_000002.img",
+    "SRS_101/mar225_001.img",
+    "SRS_142/q4_1_001.img",
+    "SSRL_bl111/mar325_1_001.mccd",
+    "SSRL_bl91/q315_1_001.img",
+    "Texas_A_and_M_University/lyziph6p5_01_0001.sfrm",
+    "XDS/INTEGRATE.HKL",
+    "XDS/XDS_ASCII.HKL",
+    "XDS/XPARM.XDS",
+    "dials-190/whatev1_01_00001.cbf",
+    "dials-190/whatev1_01_00002.cbf",
+    "dials-190/whatev1_02_00001.cbf",
+    "dials-190/whatev1_02_00002.cbf",
+    "dials-190/whatev1_03_00001.cbf",
+    "dials-190/whatev1_03_00002.cbf",
+    "johns_hopkins_raxisII/lys_001.osc",
+    "saturn/lyso_00001.img",
+    "xia2/merge2cbf_averaged_0001.cbf",
+)
 
-    Along with a custom LBL-only file, iterates over all contents of
-    dials_regression/image_examples and yields data for each file, for
-    converting to a fixture.
 
-    Do not use this function directly for tests - instead use the
-    test_image fixture.
-
-    Generates (path, name) where:
-      path could be a pytest-marked value - and could be a skipped dummy
-      name is intended as a short test-parameter name
-    """
-
+def test_berkeley_special_h5():
     # Handle the special berkeley-only h5 file
     special_h5 = "/net/viper/raid1/dectris/eiger16MNov2015/2015_11_10/insu6_1_master.h5"
-    if h5py is None:
-        yield pytest.param(
-            special_h5, marks=pytest.mark.skip(reason="could not import 'h5py'")
-        ), special_h5
-    else:
-        if os.path.isfile(special_h5):
-            yield special_h5, special_h5
-        else:
-            yield pytest.param(
-                special_h5, marks=pytest.mark.skip(reason="LBL-only file not present")
-            ), special_h5
+    if not os.path.isfile(special_h5):
+        pytest.skip("LBL-only file not present")
 
-    # Filename patterns to ignore completely
-    ignore_files = [
-        r"\..*",  # Hidden files
-        r"\.svn",  # SVN directories
-        r"ED_From_TIFF",  # removed [2017:jmp@r1618] for unknown reason
-        r"LCLS_cspad_nexus/cxi78513_bslz4_r0014_subset4_\d+\.h5$",  # Non-master files
-        r"LCLS_cspad_nexus/[^/]*\.h5$",  # Incompatible with current nexus definition [2018:asmit@r1902]
-        r"DLS_eBIC/image_0001\.tif$",  # The format is in testing_dxtbx_format_classes/, not dxtbx, so skip the example image [2017:upintheair@r1693]
-        r"README$",
-        r".*\.(?:pyc?|log|json)$",  # Extensions to ignore
-        r"putative_imgCIF_HDF5_mapping/X4_wide_M1S4_1.nxs",  # See https://github.com/cctbx/dxtbx/issues/321#issuecomment-787731768
-    ]
-    # Pattern to match item at start of string or after directory indicator
-    DIRSTART = r"^(?:.*/)?"
-    # Handle windows paths, and ignore case when matching
-    ignore_files = [
-        re.compile((DIRSTART + x).replace(r"/", re.escape(os.sep)), re.IGNORECASE)
-        for x in ignore_files
-    ]
-
-    # Try to find dials_regression
-    dials_regression = dxtbx.conftest.dials_regression_path()
-    if not dials_regression:
-        # Have one, skipped placeholder test, if there is no dials_regression
-        yield (
-            pytest.param(
-                "image_examples",
-                marks=pytest.mark.skip(
-                    reason="dials_regression required for image format tests"
-                ),
-            ),
-            "image_examples",
-        )
-        return
-
-    image_dir = os.path.join(dials_regression, "image_examples")
-    # Now look at everything in image_examples
-    for root, dirs, files in os.walk(image_dir):
-        for file in files:
-            full_path = os.path.join(root, file)
-            rel_path = os.path.relpath(full_path, image_dir)
-
-            # Test the filename patterns
-            if any(pattern.search(full_path) for pattern in ignore_files):
-                print("Ignoring {}".format(full_path))
-                continue
-
-            # Skip h5 files if h5py is not present
-            if (full_path.endswith(".h5") or full_path.endswith(".nxs")) and not h5py:
-                yield pytest.param(
-                    full_path, marks=pytest.mark.skip(reason="could not import 'h5py'")
-                ), rel_path
-            else:
-                # Give this file back
-                yield (full_path, rel_path)
-
-
-# Generate this list once to use as a fixture
-_all_images = list(_generate_all_test_images())
-
-
-@pytest.fixture(params=[x[0] for x in _all_images], ids=[x[1] for x in _all_images])
-def test_image(request, dials_regression):
-    """Fixture to allow tests to be parametrized for every test image"""
-
-    return request.param
-
-
-@pytest.fixture
-def test_image_for_reading(test_image):
-    """Test images, plus xfail marking for things we expect to not read"""
-
-    # Explicit list of directories that we expect to fail.
-    # References are to original dials_regression commit that added the exclusion
-    skipped_tests = {
-        "ADSC_CBF": "something wrong with ADSC CBF format reader         [2013:sauter@r12]",
-        "APS_19BM": "APS beamline 19; appears to be an uncorrected image [2013:sauter@r16]",
-        "SACLA_MPCCD_Cheetah": "MPCCD not supported by iotbx             [2016:nakane@r1540]",
-        "putative_imgCIF_HDF5_mapping": "test hdf5 image set not yet supported [2013:aaron@r66]",
-    }
-    # Check that the name is an actual folder in the file path
-    path_parts = {x.basename for x in py.path.local(test_image).parts()}
-    skip_this_test = set(skipped_tests) & path_parts
-    if skip_this_test:
-        reason = skipped_tests[skip_this_test.pop()]
-        pytest.skip(reason)
-
-    if six.PY3 and test_image.endswith(".pickle"):
-        pytest.skip("Importing .pickle format images is not supported in Python 3")
-
-    return test_image
+    # Run the tests, but without dials_regression fixture, circumventing the path resolution step
+    test_read_image(special_h5, dials_regression=None)
+    test_format_class_API_assumptions(special_h5, dials_regression=None)
 
 
 @pytest.mark.regression
-def test_read_image(test_image_for_reading):
+@pytest.mark.parametrize("test_image", _files)
+def test_read_image(test_image, dials_regression):
     """Test that all the regression images can be read"""
-    if "LCLS" in test_image_for_reading and not libtbx.env.has_module("xfel"):
-        pytest.skip("Ignoring LCLS because xfel missing")
-        return
+    if not xfel and test_image.startswith("LCLS"):
+        pytest.skip("could not import 'xfel'")
 
-    format_instance = dxtbx.format.Registry.get_format_class_for_file(
-        test_image_for_reading
-    )
-    print("Reading", test_image_for_reading)
+    if not h5py and test_image.endswith((".h5", ".nxs")):
+        pytest.skip("could not import 'h5py'")
+
+    if dials_regression:
+        test_image = os.path.join(
+            dials_regression, "image_examples", *test_image.split("/")
+        )
+
+    format_instance = dxtbx.format.Registry.get_format_class_for_file(test_image)
+    print("Reading", test_image)
     print("Format:", format_instance)
     assert format_instance, "no matching format class found"
-    instance = format_instance(test_image_for_reading)
+    instance = format_instance(test_image)
 
     # Test metadata reading
     instance.get_goniometer()
@@ -182,14 +168,12 @@ def test_read_image(test_image_for_reading):
 
     for panel in detector:
         d_mat = scitbx.matrix.sqr(panel.get_d_matrix())
-        if d_mat.determinant() == 0:
-            print("  d matrix with zero determinant")
-        if d_mat.determinant() < 0:
-            print("  d matrix with negative determinant")
+        if d_mat.determinant() <= 0:
+            print("  d matrix with non-positive determinant")
 
     # Test reading of the raw data
     # XDS, HKL we expect to fail for this  - so skip this part for those
-    if not test_image_for_reading[-3:].upper() in {"XDS", "HKL"}:
+    if not test_image.endswith(("XDS", "HKL")):
         try:
             R_raw_data = instance.get_raw_data()
         except TypeError:
@@ -203,14 +187,14 @@ def test_read_image(test_image_for_reading):
         # Set instance.detectorbase, if available. There used to be a blacklist
         # of files not to call this with, but relying on the attribute test
         # seems to be just as effective
-        instance.get_detectorbase()
+        try:
+            instance.get_detectorbase()
+        except NotImplementedError:
+            pass
         print("  Have detectorbase? ", hasattr(instance, "detectorbase"))
 
         # Specific test for cctbx/dxtbx#163. This test will fail if char is unsigned.
-        if (
-            "APS_24IDC" in test_image_for_reading
-            and "pilatus_1_0001.cbf" in test_image_for_reading
-        ):
+        if "APS_24IDC" in test_image and "pilatus_1_0001.cbf" in test_image:
             d = R_raw_data[0]
             assert (
                 flex.sum(d.as_1d().select(d.as_1d() >= 0)) == 20108255
@@ -218,7 +202,7 @@ def test_read_image(test_image_for_reading):
 
         # test the older detectorbase interface if available
         if hasattr(instance, "detectorbase"):
-            imgfactory = SlipViewerImageFactory(test_image_for_reading)
+            imgfactory = SlipViewerImageFactory(test_image)
             imgfactory.read()
             print("  Detectorbase:", instance.detectorbase.__class__.__name__)
 
@@ -237,15 +221,21 @@ def test_read_image(test_image_for_reading):
                 assert (Ip == Rp).all_eq(True)
 
 
-def test_format_class_API_assumptions(test_image):
+@pytest.mark.parametrize("test_image", _files)
+def test_format_class_API_assumptions(test_image, dials_regression):
     """For a given image file, walk the whole DAG of Format objects, and
     verify the following basic assumptions for format classes:
     * Any file must only be understood by a single leaf node format class.
     * No .understand() call on any top level format class or a child class
       of another understanding format is allowed to throw an exception.
     """
-    if six.PY3 and test_image.endswith(".pickle"):
-        pytest.skip("Importing .pickle format images is not supported in Python 3")
+    if not h5py and test_image.endswith((".h5", ".nxs")):
+        pytest.skip("could not import 'h5py'")
+
+    if dials_regression:
+        test_image = os.path.join(
+            dials_regression, "image_examples", *test_image.split("/")
+        )
 
     dag = dxtbx.format.Registry.get_format_class_dag()
 
@@ -266,8 +256,7 @@ def test_format_class_API_assumptions(test_image):
                 understood_format_class = recursive_format_class or subformat
                 if known_format_class and known_format_class != understood_format_class:
                     print(
-                        "File can be understood as %s and %s"
-                        % (known_format_class, understood_format_class)
+                        f"File can be understood as {known_format_class} and {understood_format_class}"
                     )
                     multiple_formats = True
                 known_format_class = understood_format_class
