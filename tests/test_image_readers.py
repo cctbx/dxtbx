123837e147f7f643a62e9ac2adb081fbf2680d04
Nicholas Devenish <ndevenish@gmail.com>
2021-05-20 13:10:03 +0100

    Don't use CBFLib binary bindings if using new pycbf

--- tests/test_image_readers.py
+++ tests/test_image_readers.py
@@ -4,8 +4,16 @@ import os
 
 import pycbf
 
-from dxtbx.format.image import cbf_read_buffer
 from dxtbx.model.detector import DetectorFactory
+
+try:
+    from dxtbx.format.image import cbf_read_buffer
+except ImportError:
+    # If we didn't compile cbf_read_buffer, use the pycbf version
+    def cbf_read_buffer(handle, contents, flags):
+        """Use cbf_read_buffer via pycbf"""
+        assert hasattr(pycbf.cbf_handle_struct, "read_buffer")
+        return handle.read_buffer(contents, flags)
 
 
 def test_cbf_buffer(dials_regression):
