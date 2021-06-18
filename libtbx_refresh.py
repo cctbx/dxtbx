2baad10947067e2fdb48b7a0027c962e986e44f8
Nicholas Devenish <ndevenish@gmail.com>
2021-06-18 15:22:52 +0100

    Typo while there

--- libtbx_refresh.py
+++ libtbx_refresh.py
@@ -31,7 +31,7 @@ def _install_dxtbx_setup():
     )
 
 
-# Retain until after DIALS 3.6 is release to unregister the previous dispatcher handlers
+# Retain until after DIALS 3.6 is released to unregister the previous dispatcher handlers
 if not pkg_resources or any(x.key == "libtbx.dxtbx" for x in pkg_resources.working_set):
     libtbx.pkg_utils.define_entry_points({})
 
