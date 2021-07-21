9b59cc26688a77e6f7490c99dca0aa8aca6741e7
Nicholas Devenish <ndevenish@gmail.com>
2021-07-21 11:26:01 +0100

    Update pre-commit hooks (#403)
    Updated:
    - Isort: Mainly stability and configuration changes
    - Black: Mainly internal restructuring, string and docstring edge-case
      handling
    - flake8: Updated backend checkers
    - pre-commit-hooks (things that affect us):
        - `no-commit-to-branch`: added "main" to default blocked branches
        - `check-added-large-files`: added an `--enforce-all` option to
          check files already existing
    
    Additionally:
    - `check-yaml` fell over on `.clang-format`. We never ran this before
      because it hasn't been changed since we added the validation check.
      This needs you to explicitly request multi-document yamls, which has
      been added
    - Single-source flake8 configuration
    - Enabled 'F541 f-string is missing placeholders' check, and fix in
      3places
    
    Co-authored-by: Markus Gerstel <markus.gerstel@diamond.ac.uk>

--- tests/format/test_FormatNexusEigerDLS16M.py
+++ tests/format/test_FormatNexusEigerDLS16M.py
@@ -235,9 +235,9 @@ def test_do_not_understand_i24(tmp_path):
     with h5py.File(nxs, mode="w") as fh:
         entry = fh.create_group("entry")
         instrument = entry.create_group("instrument")
-        instrument.attrs["short_name"] = np.string_(f"DLS I24")
+        instrument.attrs["short_name"] = np.string_("DLS I24")
         name = instrument.create_dataset(
-            "name", data=np.string_(f"DIAMOND BEAMLINE I24")
+            "name", data=np.string_("DIAMOND BEAMLINE I24")
         )
-        name.attrs["short_name"] = np.string_(f"DLS I24")
+        name.attrs["short_name"] = np.string_("DLS I24")
     assert not FormatNexusEigerDLS16M.understand(nxs)
