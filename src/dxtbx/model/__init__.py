33f5b063e6cf3b71e579d35cfe87df230da01603
davidmcdonagh <davidnmcdonagh@gmail.com>
2021-07-30 09:45:57 +0100

    Added BeamFactory expot to model/__init__.py

--- src/dxtbx/model/__init__.py
+++ src/dxtbx/model/__init__.py
@@ -12,7 +12,7 @@ from scitbx import matrix
 from scitbx.array_family import flex
 
 from dxtbx.imageset import ImageGrid, ImageSet, RotImageSequence, TOFImageSequence
-from dxtbx.model.beam import MonochromaticBeamFactory, TOFBeamFactory
+from dxtbx.model.beam import BeamFactory, MonochromaticBeamFactory, TOFBeamFactory
 from dxtbx.model.crystal import CrystalFactory
 from dxtbx.model.detector import DetectorFactory
 from dxtbx.model.goniometer import GoniometerFactory
@@ -60,6 +60,7 @@ __all__ = (
     "MonochromaticBeam",
     "TOFBeam",
     "Beam",
+    "BeamFactory",
     "MonochromaticBeamFactory",
     "TOFBeamFactory",
     "Crystal",
