8228a03695ff23f605f22ddf62fef4750ef379d9
Nicholas Devenish <ndevenish@gmail.com>
2021-08-03 20:07:26 +0100

    Cleanup

--- libtbx_refresh.py
+++ libtbx_refresh.py
@@ -53,8 +53,7 @@ def _install_dxtbx_setup_readonly_fallback():
     and other entrypoints should be enumerable.
     """
     dxtbx_root_path = Path(libtbx.env.dist_path("dxtbx"))
-    # Find a path to "faux-install" this package to. We'll then move
-    # out the .egg-link
+    # Install this into a build/dxtbx subfolder
     build_path = Path(abs(libtbx.env.build_path))
     dxtbx_build_path = build_path / "dxtbx"
     subprocess.run(
@@ -72,22 +71,8 @@ def _install_dxtbx_setup_readonly_fallback():
         ],
         check=True,
     )
-    # Check that we can find the egg-link
-    # links = list(build_path.glob("**/dxtbx.egg-link"))
-    # if len(links) == 0:
-    #    raise RuntimeError(
-    #        "Could not configure dxtbx with read-only base - no .egg-link found after install"
-    #    )
-    # elif len(links) > 1:
-    #    raise RuntimeError(
-    #        "Could not configure dxtbx with read-only base - too many .egg-link found after install"
-    #    )
 
-    # link = links[0]
-    # Move this .egg-link to the build/lib path (libtbx puts this as a root PYTHONPATH)
-    # dest = build_path / "lib" / link.name
-    # print(f"Renaming {link} → {dest}")
-    # link.rename(dest)
+    # Update the libtbx environment to point to the source location
     libtbx.env.pythonpath.insert(0, str(dxtbx_root_path / "src"))
     libtbx.env.pickle()
 
@@ -114,15 +99,12 @@ def _test_writable_dir(path: Path) -> bool:
             return True
 
 
-# Retain until after DIALS 3.6 is released to unregister the previous dispatcher handlers
-if not pkg_resources or any(x.key == "libtbx.dxtbx" for x in pkg_resources.working_set):
-    libtbx.pkg_utils.define_entry_points({})
-
+site_packages = Path(site.getsitepackages()[0])
 
 # Detect case where base python environment is read-only
 # e.g. on an LCLS session on a custom cctbx installation where the
 # source is editable but the conda_base is read-only
-site_packages = Path(site.getsitepackages()[0])
+#
 # We need to check before trying to install as pip does os.access-based
 # checks then installs with --user if it fails. We don't want that.
 if _test_writable_dir(site_packages):
@@ -130,3 +112,7 @@ if _test_writable_dir(site_packages):
 else:
     print("Python site directory not writable - falling back to tbx install")
     _install_dxtbx_setup_readonly_fallback()
+
+# Retain until after DIALS 3.6 is released to unregister the previous dispatcher handlers
+if not pkg_resources or any(x.key == "libtbx.dxtbx" for x in pkg_resources.working_set):
+    libtbx.pkg_utils.define_entry_points({})
