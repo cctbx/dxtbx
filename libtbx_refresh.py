37011a27846fd84f1e230ecf00b2594e200f7321
Richard Gildea <richard.gildea@diamond.ac.uk>
2021-08-31 11:06:40 +0100

    Revert "Add pint requirement to libtbx_refresh.py"
    The later commit 7ea8854a78e8753831a2a36fd5c7a7bb3faecb93 was the
    necessary one.
    
    This reverts commit 11e1645184c447e4012a445952fac48959fca004.

--- libtbx_refresh.py
+++ libtbx_refresh.py
@@ -22,9 +22,6 @@ except ModuleNotFoundError:
     pkg_resources = None
 
 
-libtbx.pkg_utils.require("pint")
-
-
 def _install_dxtbx_setup():
     """Install xia2 as a regular/editable python package"""
 
