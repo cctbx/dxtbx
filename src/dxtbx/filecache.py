843bc40c24ec5de9ba3d14f904d0c0c6f5ea719d
Nicholas Devenish <ndevenish@gmail.com>
2021-10-30 17:29:43 +0100

    Add more IOBase methods to pseudo_file
    This allows it to be read by TextIOWrapper

--- src/dxtbx/filecache.py
+++ src/dxtbx/filecache.py
@@ -339,7 +339,7 @@ class lazy_file_cache:
                 )
 
 
-class pseudo_file:
+class pseudo_file(io.IOBase):
     """A file-like object that serves as frontend to a dxtbx lazy file cache."""
 
     def __init__(self, lazy_cache_object):
@@ -374,13 +374,24 @@ class pseudo_file:
     def flush(self):
         self._check_not_closed()
 
-    def next(self):
+    def __next__(self):
         data = self.readline()
         if data == b"":
             raise StopIteration()
         return data
 
-    __next__ = next
+    def readable(self):
+        return not self._closed
+
+    def writable(self):
+        return False
+
+    def seekable(self):
+        return self.readable()
+
+    @property
+    def closed(self):
+        return self._closed
 
     def read(self, size=-1):
         self._check_not_closed()
