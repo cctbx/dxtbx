4488e5994883d784728eb9dd9c19494c9e48cfb7
Nicholas Devenish <ndevenish@gmail.com>
2021-10-31 12:09:17 +0000

    Add basic typing for dxtbx.ext

--- src/dxtbx/ext.py
+++ src/dxtbx/ext.py
@@ -1,4 +1,8 @@
 import boost_adaptbx.boost.python
 
 ext = boost_adaptbx.boost.python.import_ext("dxtbx_ext")
-from dxtbx_ext import *  # isort:skip # noqa: F401,F403,E402
+
+try:
+    from .dxtbx_ext import *  # noqa: F401,F403,E402
+except ModuleNotFoundError:
+    from dxtbx_ext import *  # type: ignore # noqa: F401,F403,E402
