b74d667a6a50c3593bc6c475d74214340d070f86
Ben Williams <benjaminhwilliams@users.noreply.github.com>
2021-11-11 13:08:25 +0000

    Address review

--- src/dxtbx/model/goniometer.h
+++ src/dxtbx/model/goniometer.h
@@ -33,35 +33,36 @@ namespace dxtbx { namespace model {
   class GoniometerBase {};
 
   /**
-   * A class to represent the rotation axis for a standard rotation
+   * A class to represent the rotation operator for a standard rotation
    * geometry diffraction data set.
    *
-   * The rotation axis is assumed to have its origin at the origin of the
+   * The rotation is assumed to have its origin at the origin of the
    * laboratory coordinate system. The rotation axis vector is normalized
    * when set using either the constructor or the rotation axis 'setter'.
    *
    * In the conventional crystallographic formalism, the goniostat rotation
    * operator R represents the rotation of the sample mount and relates the
    * sample reference frame to the lab frame.  R is calculated from the
-   * recorded angular positions of each axis, combined with prior knowledge
-   * of the orientation in the lab frame of each axis when the goniometer is
-   * at its zero datum.
+   * recorded angular positions of each motor, combined with prior knowledge
+   * of the orientation in the lab frame of each motor's axis when the
+   * goniometer is at its zero datum.
    *
    * In the DXTBX goniometer model, we define a finer-grained decomposition
-   * of R to represent the different axes of a general goniometer, which may
-   * have one single axis or multiple axes.  This model is intended to
-   * describe a typical rotation experiment in which only one axis of the
-   * goniometer is allowed to rotate during the scan, any other axes being
-   * static throughout the measurement.  R is expressed as three components:
+   * of R to represent the different motors of a general goniometer, which
+   * may have one single motor or multiple motors.  This model is intended
+   * to describe a typical rotation experiment in which only one motor of
+   * the goniometer is allowed to rotate during the scan, any other motors
+   * being static throughout the measurement.  R is expressed as three
+   * components:
    *
    *  R = S ∘ R' ∘ F
    *
-   * Here, R' denotes the scan axis rotation operator, S denotes a static
-   * 'setting rotation' operator (not to be confused with the setting
+   * Here, R' denotes the scanning motor rotation operator, S denotes a
+   * static 'setting rotation' operator (not to be confused with the setting
    * operator, A = U ∘ B), and F denotes another static 'fixed rotation'
-   * operator.  S represents the combined rotation of any 'parent' axes on
-   * which the scan axis is mounted, and F represents the combined rotation
-   * of any 'child' axes mounted on the scan axis.
+   * operator.  S represents the combined rotation of any 'parent' motors on
+   * which the scanning motor is mounted, and F represents the combined
+   * rotation of any 'child' motors mounted on the scanning motor.
    *
    * For more details, see
    *  https://dials.github.io/documentation/conventions.html
@@ -74,7 +75,9 @@ namespace dxtbx { namespace model {
     /**
      * Initialise the goniometer. The fixed rotation matrix is set to the
      * identity matrix and the rotation axis is set to the x axis
-     * @param rotation_axis The goniometer rotation (R') axis
+     * @param rotation_axis The axis in the lab frame of the rotation R'
+     *                      corresponding to the scanning motor of the
+     *                      goniometer.
      */
     Goniometer()
         : rotation_axis_(1.0, 0.0, 0.0),
@@ -84,7 +87,9 @@ namespace dxtbx { namespace model {
     /**
      * Initialise the goniometer. The fixed rotation matrix is set to the
      * identity matrix.
-     * @param rotation_axis The goniometer rotation (R') axis
+     * @param rotation_axis The axis in the lab frame of the rotation R'
+     *                      corresponding to the scanning motor of the
+     *                      goniometer.
      */
     Goniometer(vec3<double> rotation_axis)
         : fixed_rotation_(1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0),
@@ -95,8 +100,11 @@ namespace dxtbx { namespace model {
 
     /**
      * Initialise the goniometer.
-     * @param rotation_axis The goniometer rotation (R') axis
-     * @param fixed_rotation The additional fixed (F) rotation of the sample
+     * @param rotation_axis The axis in the lab frame of the rotation R'
+     *                      corresponding to the scanning motor of the
+     *                      goniometer.
+     * @param fixed_rotation The additional 'fixed' (F) rotation of the
+     *                       sample.
      */
     Goniometer(vec3<double> rotation_axis, mat3<double> fixed_rotation)
         : fixed_rotation_(fixed_rotation),
@@ -107,9 +115,13 @@ namespace dxtbx { namespace model {
 
     /**
      * Initialise the goniometer.
-     * @param rotation_axis The goniometer rotation (R') axis at datum
-     * @param fixed_rotation The additional fixed rotation (F) of the sample
-     * @param setting_rotation The additional setting rotation (S) of the axis
+     * @param rotation_axis The axis in the lab frame, at zero datum, of the
+     *                      rotation R' corresponding to the scanning motor
+     *                      of the goniometer.
+     * @param fixed_rotation The additional 'fixed' (F) rotation of the
+     *                       sample.
+     * @param setting_rotation The additional 'setting' rotation (S) of the
+     *                         scanning motor.
      */
     Goniometer(vec3<double> rotation_axis,
                mat3<double> fixed_rotation,
@@ -135,12 +147,12 @@ namespace dxtbx { namespace model {
       return rotation_axis_;
     }
 
-    /** Get the fixed rotation matrix, F */
+    /** Get the 'fixed' rotation matrix, F */
     mat3<double> get_fixed_rotation() const {
       return fixed_rotation_;
     }
 
-    /** Get the setting rotation matrix, S */
+    /** Get the 'setting' rotation matrix, S */
     mat3<double> get_setting_rotation() const {
       return setting_rotation_;
     }
@@ -150,40 +162,40 @@ namespace dxtbx { namespace model {
       return setting_rotation_at_scan_points_.size();
     }
 
-    /** Get the setting rotation at scan points */
+    /** Get the 'setting' rotation at scan points */
     scitbx::af::shared<mat3<double> > get_setting_rotation_at_scan_points() const {
       return setting_rotation_at_scan_points_;
     }
 
-    /** Get the setting rotation at the scan point */
+    /** Get the 'setting' rotation at the scan point */
     mat3<double> get_setting_rotation_at_scan_point(std::size_t index) const {
       DXTBX_ASSERT(index < setting_rotation_at_scan_points_.size());
       return setting_rotation_at_scan_points_[index];
     }
 
-    /** Set the orientation of the axis of the R' rotation */
+    /** Set the orientation of the axis of the scanning rotation, R' */
     void set_rotation_axis(vec3<double> rotation_axis) {
       DXTBX_ASSERT(rotation_axis.length() > 0);
       rotation_axis_ = setting_rotation_.inverse() * rotation_axis.normalize();
     }
 
-    /** Set the R' rotation axis */
+    /** Set the axis of the scanning rotation, R' */
     void set_rotation_axis_datum(vec3<double> rotation_axis) {
       DXTBX_ASSERT(rotation_axis.length() > 0);
       rotation_axis_ = rotation_axis.normalize();
     }
 
-    /** Set the fixed rotation matrix, F */
+    /** Set the 'fixed' rotation matrix, F */
     void set_fixed_rotation(mat3<double> fixed_rotation) {
       fixed_rotation_ = fixed_rotation;
     }
 
-    /** Set the setting rotation matrix, S */
+    /** Set the 'setting' rotation matrix, S */
     void set_setting_rotation(mat3<double> setting_rotation) {
       setting_rotation_ = setting_rotation;
     }
 
-    /** Set the setting rotation matrix at scan-points */
+    /** Set the 'setting' rotation matrix at scan-points */
     void set_setting_rotation_at_scan_points(
       const scitbx::af::const_ref<mat3<double> > &S) {
       setting_rotation_at_scan_points_ =
