fdd1686c2b7767ef6754973500d4f67be75a51ba
Richard Gildea <richard.gildea@diamond.ac.uk>
2021-11-24 15:58:11 +0000

    Fix print_header test

--- tests/command_line/test_print_header.py
+++ tests/command_line/test_print_header.py
@@ -10,8 +10,8 @@ def test_print_header(dials_data):
     assert not result.returncode and not result.stderr
 
     expected_output = [
-        "=== %s ===" % master,
-        "Using header reader: FormatNexusEigerDLS16M",
+        f"=== {master} ===",
+        "Using header reader: FormatNXmxDLS16M",
     ]
 
     for record in expected_output:
