326e9eec9919067e1237d49c885bfa52a276528e
Nicholas Devenish <ndevenish@gmail.com>
2022-01-05 11:39:39 +0000

    Now I know why this was ImportError
    Module exists but not this symbol.

--- src/dxtbx/version.py
+++ src/dxtbx/version.py
@@ -2,13 +2,13 @@ try:
     from importlib import metadata
 
     version = metadata.version("dxtbx")
-except ModuleNotFoundError:
+except ImportError:
     try:
         # Running on pre-3.8 Python; use importlib-metadata backport package
         import importlib_metadata as metadata
 
         version = metadata.version("dxtbx")
-    except ModuleNotFoundError:
+    except ImportError:
         # If we don't have importlib or the backport assume 3.8 series.
         # In this scenario we probably aren't going to be checking this anyway.
         version = "3.8.dev"
