7047afdbdef5b30b2d072eeea863d3ae5b80678c
Aaron Brewster <asbrewster@lbl.gov>
2022-01-18 13:33:00 -0800

    Make FormatPY abstract

--- src/dxtbx/format/FormatPY.py
+++ src/dxtbx/format/FormatPY.py
@@ -5,9 +5,10 @@ import pickle
 import sys
 
 from dxtbx import IncorrectFormatError
-from dxtbx.format.Format import Format
+from dxtbx.format.Format import Format, abstract
 
 
+@abstract
 class FormatPY(Format):
     """Let's take an educated guess as to how to recognize a Python
     pickle file containing a dictionary.  Not easy because there are
