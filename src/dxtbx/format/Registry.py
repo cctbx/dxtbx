ac832b276d732f77bc05530481e9decb7b2bd6d1
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2022-01-19 10:49:08 +0000

    allow pathlike objects in get_format_class

--- src/dxtbx/format/Registry.py
+++ src/dxtbx/format/Registry.py
@@ -4,6 +4,7 @@ this is useful for i.e. identifying the best tool to read a given range
 of image formats.
 """
 
+import os
 
 import pkg_resources
 
@@ -88,6 +89,8 @@ def get_format_class_for_file(image_file, format_hint=None):
     # Grab the scheme from this URI
     scheme = get_url_scheme(image_file)
 
+    image_file_str = os.fspath(image_file)
+
     # If a format hint was given then find all paths through the DAG leading
     # to this format class. Create a set containing all format class names
     # on those paths (priority_formats), and use the information whether a
@@ -121,5 +124,5 @@ def get_format_class_for_file(image_file, format_hint=None):
     # accepting leaf node
     for format in sorted(_format_dag["Format"], key=format_sort):
         format_class = get_format_class_for(format)
-        if scheme in format_class.schemes and format_class.understand(image_file):
-            return recurse(format, image_file)
+        if scheme in format_class.schemes and format_class.understand(image_file_str):
+            return recurse(format, image_file_str)
