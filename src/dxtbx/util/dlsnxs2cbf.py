8a077069e46de5ad871302878760c33a7e81eef7
Richard Gildea <rjgildea@users.noreply.github.com>
2022-01-25 16:40:05 +0000

    dlsnxs2cbf: support more general NeXus data (#453)
    * Support NXmx-flavoured NeXus data, rather than simply a narrow subset of Diamond Light Source in-house nearly-NeXus data.
    * Improve the UI.
    
    Co-authored-by: DiamondLightSource-build-server <DiamondLightSource-build-server@users.noreply.github.com>
    Co-authored-by: Ben Williams <benjaminhwilliams@users.noreply.github.com>
    Co-authored-by: Nicholas Devenish <ndevenish@gmail.com>

--- src/dxtbx/util/dlsnxs2cbf.py
+++ src/dxtbx/util/dlsnxs2cbf.py
@@ -1,118 +1,110 @@
 import binascii
+import math
+from pathlib import Path
 
 import h5py
 import hdf5plugin  # noqa; F401
 import numpy as np
+from tqdm import tqdm
 
-from scitbx.array_family import flex
-
+import dxtbx.model
+import dxtbx.nexus.nxmx
 from dxtbx.ext import compress
 
 
-def get_mask(nfast, nslow):
-    module_size_fast, module_size_slow = (1028, 512)
-    gap_size_fast, gap_size_slow = (12, 38)
-    n_fast, remainder = divmod(nfast, module_size_fast)
-    assert (n_fast - 1) * gap_size_fast == remainder
+def compute_cbf_header(nxmx: dxtbx.nexus.nxmx.NXmx, nn: int):
+    nxentry = nxmx.entries[0]
+    nxsample = nxentry.samples[0]
+    nxinstrument = nxentry.instruments[0]
+    nxdetector = nxinstrument.detectors[0]
+    nxbeam = nxinstrument.beams[0]
 
-    n_slow, remainder = divmod(nslow, module_size_slow)
-    assert (n_slow - 1) * gap_size_slow == remainder
-
-    mask = flex.bool(flex.grid(nslow, nfast), True)
-    blit = flex.bool(flex.grid(module_size_slow, module_size_fast), False)
-
-    for j in range(n_slow):
-        for i in range(n_fast):
-            o_i = i * (module_size_fast + gap_size_fast)
-            o_j = j * (module_size_slow + gap_size_slow)
-            mask.matrix_paste_block_in_place(blit, i_row=o_j, i_column=o_i)
-
-    return mask
-
-
-def get_distance_in_mm(f):
-    try:
-        D = f["/entry/instrument/detector_distance"]
-    except KeyError:
-        D = f["/entry/instrument/detector/detector_distance"]
-    d = D[()]
-    if D.attrs["units"] == np.string_("m"):
-        d *= 1000
-    elif D.attrs["units"] != np.string_("mm"):
-        raise RuntimeError(
-            "unknown distance unit '%s'" % D.attrs["units"].decode("latin-1")
-        )
-    return d
-
-
-def compute_cbf_header(f, nn=0):
+    distance = nxdetector.distance
+    if distance is None:
+        distance = dxtbx.nexus.get_dxtbx_detector(nxdetector, nxbeam)[0].get_distance()
 
     result = []
 
-    D = get_distance_in_mm(f)
-    instrument = f["/entry/instrument"]
-    name = instrument.attrs.get("short_name", "")
-    T = instrument["detector/count_time"][()]
-    L = instrument["beam/incident_wavelength"][()]
-    A = instrument["attenuator/attenuator_transmission"][()]
+    A = nxinstrument.attenuators[0]["attenuator_transmission"][()]
 
     # this timestamp _should_ be in UTC - so can ignore timezone info - at
     # least for purposes here (causes errors for old versions of dxtbx reading
     # the data) - 19 chars needed
-    timestamp = f["/entry/start_time"][()].decode()[:19]
-    omega = f["/entry/sample/transformations/omega"][()]
-    omega_increment = f["/entry/sample/transformations/omega_increment_set"][()]
-    chi_key = "/entry/sample/transformations/chi"
-    phi_key = "/entry/sample/transformations/phi"
+    timestamp = nxentry.start_time
 
-    if "/entry/instrument/detector/beam_centre_x" in f:
-        Bx = instrument["detector/beam_centre_x"][()]
-        By = instrument["detector/beam_centre_y"][()]
-    else:
-        Bx = instrument["detector/beam_center_x"][()]
-        By = instrument["detector/beam_center_y"][()]
+    dependency_chain = dxtbx.nexus.nxmx.get_dependency_chain(nxsample.depends_on)
 
     result.append("###CBF: VERSION 1.5, CBFlib v0.7.8 - Eiger detectors")
     result.append("")
-    result.append("data_%06d" % (nn + 1))
+    result.append(f"data_{nn + 1:06d}")
     result.append("")
     result.append(
         """_array_data.header_convention "PILATUS_1.2"
 _array_data.header_contents
 ;"""
     )
-    result.append("# Detector: EIGER 2XE 16M S/N 160-0001 Diamond %s" % name)
-    result.append("# %s" % timestamp)
-    result.append("# Pixel_size 75e-6 m x 75e-6 m")
-    result.append("# Silicon sensor, thickness 0.000450 m")
-    result.append("# Exposure_time %.5f s" % T)
-    result.append("# Exposure_period %.5f s" % T)
+    result.append(
+        f"# Detector: {nxdetector.description} S/N {nxdetector.serial_number} "
+        f"{nxinstrument.short_name}"
+    )
+    result.append(f"# {timestamp}")
+    result.append(
+        f"# Pixel_size {nxdetector.modules[0].fast_pixel_direction[()]:~} "
+        f"x {nxdetector.modules[0].slow_pixel_direction[()]:~}"
+    )
+    result.append(
+        f"# {nxdetector.sensor_material} sensor, "
+        f"thickness {nxdetector.sensor_thickness:~}"
+    )
+    result.append(f"# Exposure_time {nxdetector.count_time:.5f} s")
+    result.append(f"# Exposure_period {nxdetector.count_time:.5f} s")
     result.append("# Tau = 1e-9 s")
-    result.append("# Count_cutoff 65535 counts")
+    result.append(f"# Count_cutoff {nxdetector.saturation_value} counts")
     result.append("# Threshold_setting: 0 eV")
     result.append("# Gain_setting: mid gain (vrf = -0.200)")
     result.append("# N_excluded_pixels = 0")
-    result.append("# Excluded_pixels: badpix_mask.tif")
-    result.append("# Flat_field: (nil)")
-    result.append("# Wavelength %.5f A" % L)
-    result.append("# Detector_distance %.5f m" % (D / 1000.0))
-    result.append(f"# Beam_xy ({Bx:.2f}, {By:.2f}) pixels")
+    result.append(f"# Wavelength {nxbeam.incident_wavelength:.5f} A")
+    result.append(f"# Detector_distance {distance / 1000.0:.5f} m")
+    result.append(
+        f"# Beam_xy ({nxdetector.beam_center_x:.2f}, {nxdetector.beam_center_y:.2f}) "
+        "pixels"
+    )
     result.append("# Flux 0.000000")
-    result.append("# Filter_transmission %.3f" % A)
-    result.append("# Start_angle %.4f deg." % omega[nn])
-    result.append("# Angle_increment %.4f deg." % omega_increment[nn])
-    result.append("# Detector_2theta 0.0000 deg.")
+    result.append(f"# Filter_transmission {A:.3f}")
+
+    transformations = {
+        t.path.split("/")[-1].capitalize(): t for t in nxinstrument.transformations
+    }
+    two_theta = transformations.get("TWO_THETA", np.array(0))[()]
+    result.append(f"# Detector_2theta {two_theta:.3f} deg.")
+
     result.append("# Polarization 0.990")
-    result.append("# Alpha 0.0000 deg.")
-    result.append("# Kappa 0.0000 deg.")
-    if phi_key in f:
-        result.append(f"# Phi {np.squeeze(f[phi_key][()]):.4f} deg.")
-        result.append("# Phi_increment 0.0000 deg.")
-    result.append("# Omega %.4f deg." % omega[nn])
-    result.append("# Omega_increment %.4f deg." % omega_increment[nn])
-    if chi_key in f:
-        result.append(f"# Chi {np.squeeze(f[chi_key][()]):.4f} deg.")
-        result.append("# Chi_increment 0.0000 deg.")
+
+    rotations = {
+        t.path.split("/")[-1].capitalize(): t
+        for t in dependency_chain
+        if t.transformation_type == "rotation"
+    }
+
+    if "PHI" in rotations and "KAPPA" in rotations:
+        kappa_axis = rotations["KAPPA"].attrs["vector"]
+        phi_axis = rotations["PHI"].attrs["vector"]
+        alpha = np.degrees(np.arccos(np.dot(phi_axis, kappa_axis)))
+    else:
+        alpha = 0
+    result.append(f"# Alpha {alpha:.3f} deg.")
+
+    for name, r in rotations.items():
+        # Find the first varying rotation axis
+        if len(r) > 1 and not np.all(r[()] == r[0]):
+            result.append(f"# {name} {r[nn].to('degree'):.4f} deg.")
+            result.append(f"# {name}_increment {(r[1] - r[0]).to('degree'):.4f} deg")
+            result.append(f"# Start_angle {r[nn].to('degree'):.4f} deg.")
+            result.append(f"# Angle_increment {(r[1] - r[0]).to('degree'):.4f} deg")
+        else:
+            result.append(f"# {name} {r[0].to('degree'):.4f} deg.")
+            result.append(f"# {name}_increment 0.0000 deg")
+
     result.append("# Oscillation_axis X.CW")
     result.append("# N_oscillations 1")
     result.append(";")
@@ -120,30 +112,72 @@ _array_data.header_contents
     return "\n".join(result)
 
 
-def make_cbf(in_name, template):
-    with h5py.File(in_name, "r") as f:
-        mask = None
-
+def make_cbf(
+    in_name: Path | str,
+    output_directory: Path,
+    template: str | None = None,
+    num_digits: int = 0,
+):
+    with h5py.File(in_name) as f:
         start_tag = binascii.unhexlify("0c1a04d5")
 
-        for j in range(len(f["/entry/sample/transformations/omega"][()])):
-            block = 1 + (j // 1000)
-            i = j % 1000
-            header = compute_cbf_header(f, j)
-            depth, height, width = f["/entry/data/data_%06d" % block].shape
+        nxmx = dxtbx.nexus.nxmx.NXmx(f)
+        nxsample = nxmx.entries[0].samples[0]
+        nxinstrument = nxmx.entries[0].instruments[0]
+        nxdetector = nxinstrument.detectors[0]
+        nxdata = nxmx.entries[0].data[0]
 
-            data = flex.int(np.int32(f["/entry/data/data_%06d" % block][i]))
-            good = data.as_1d() < 65535
-            data.as_1d().set_selected(~good, -2)
+        dependency_chain = dxtbx.nexus.nxmx.get_dependency_chain(nxsample.depends_on)
+        scan_axis = None
+        for t in dependency_chain:
+            # Find the first varying rotation axis
+            if (
+                t.transformation_type == "rotation"
+                and len(t) > 1
+                and not np.all(t[()] == t[0])
+            ):
+                scan_axis = t
+                break
+
+        if scan_axis is None:
+            # Fall back on the first varying axis of any type
+            for t in dependency_chain:
+                if len(t) > 1 and not np.all(t[()] == t[0]):
+                    scan_axis = t
+                    break
+
+        if scan_axis is None:
+            scan_axis = nxsample.depends_on
+
+        static_mask = dxtbx.nexus.get_static_mask(nxdetector)
+        bit_depth_readout = nxdetector.bit_depth_readout
+
+        if template is None:
+            template = Path(in_name).stem + "_"
+        num_images = len(scan_axis)
+        num_digits = max(num_digits, int(math.log10(num_images)) + 1)
+
+        print(f"Writing images to {template}{'#' * num_digits}.cbf:")
+        for j in tqdm(range(num_images), unit=" images"):
+            header = compute_cbf_header(nxmx, j)
+            (data,) = dxtbx.nexus.get_raw_data(nxdata, nxdetector, j)
+            if bit_depth_readout:
+                # if 32 bit then it is a signed int, I think if 8, 16 then it is
+                # unsigned with the highest two values assigned as masking values
+                if bit_depth_readout == 32:
+                    top = 2 ** 31
+                else:
+                    top = 2 ** bit_depth_readout
+                d1d = data.as_1d()
+                d1d.set_selected(d1d == top - 1, -1)
+                d1d.set_selected(d1d == top - 2, -2)
 
             # set the tile join regions to -1 - MOSFLM cares about this apparently
-            if mask is None:
-                mask = get_mask(width, height)
-            data.as_1d().set_selected(mask.as_1d(), -1)
-
+            if static_mask:
+                data.as_1d().set_selected(~static_mask[0].as_1d(), -1)
             compressed = compress(data)
 
-            mime = """
+            mime = f"""
 
 _array_data.data
 ;
@@ -151,21 +185,16 @@ _array_data.data
 Content-Type: application/octet-stream;
      conversions="x-CBF_BYTE_OFFSET"
 Content-Transfer-Encoding: BINARY
-X-Binary-Size: %d
+X-Binary-Size: {len(compressed)}
 X-Binary-ID: 1
 X-Binary-Element-Type: "signed 32-bit integer"
 X-Binary-Element-Byte-Order: LITTLE_ENDIAN
-X-Binary-Number-of-Elements: %d
-X-Binary-Size-Fastest-Dimension: %d
-X-Binary-Size-Second-Dimension: %d
+X-Binary-Number-of-Elements: {data.size()}
+X-Binary-Size-Fastest-Dimension: {data.focus()[1]}
+X-Binary-Size-Second-Dimension: {data.focus()[0]}
 X-Binary-Size-Padding: 4095
 
-""" % (
-                len(compressed),
-                data.size(),
-                data.focus()[1],
-                data.focus()[0],
-            )
+"""
 
             padding = (
                 bytearray(4095)
@@ -173,8 +202,8 @@ X-Binary-Size-Padding: 4095
 ;"""
             )
 
-            with open(template % (j + 1), "wb") as fout:
-                print(template % (j + 1))
+            filename = f"{template}{j + 1:0{num_digits}d}.cbf"
+            with open(output_directory / filename, "wb") as fout:
                 fout.write(
                     ("".join(header) + mime).replace("\n", "\r\n").encode("latin-1")
                 )
