757b060760b91f92484f3818212b60c2cde15bfd
Aaron S. Brewster <asbrewster@lbl.gov>
2022-09-09 16:29:34 -0700

    Add xfailing test for the JF16M (#541)
    Fixes #93
    
    Requires latest version of dials-data

---
+++ tests/nexus/test_jf16M.py
@@ -0,0 +1,30 @@
+import dxtbx
+from dxtbx.model.experiment_list import ExperimentList
+import pytest
+
+
+@pytest.mark.xfail(
+    raises=ValueError, reason="https://github.com/cctbx/dxtbx/issues/537"
+)
+def test_jf16M(tmp_path, dials_data):
+    try:
+        h5path = (
+            dials_data("lysozyme_JF16M_4img", pathlib=True)
+            / "lyso009a_0087.JF07T32V01_master_4img.h5"
+        )
+    except Exception as e:
+        print(type(e), str(e))
+        raise
+    img = dxtbx.load(h5path)
+
+    d1 = img.get_detector()
+
+    expts_path = (
+        dials_data("lysozyme_JF16M_4img", pathlib=True)
+        / "lyso009a_0087.JF07T32V01_master_4img_imported.expt"
+    )
+    expts = ExperimentList.from_file(expts_path, check_format=False)
+
+    d2 = expts[0].detector
+
+    assert d1 == d2
