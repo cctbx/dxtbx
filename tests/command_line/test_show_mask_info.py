9a31615ca81fa42353c75e9ddfa89a348b716239
Richard Gildea <richard.gildea@diamond.ac.uk>
2023-02-20 11:49:33 +0000

    Ignore deprecated pkg_resources.declare_namespace warnings
    As of setuptools v67.3.0 the use of pkg_resources.declare_namespace
    in lib/mpl_toolkits/__init__.py raises a DeprecationWarning.
    
    See also:
    https://github.com/matplotlib/matplotlib/issues/25244
    
    Run tests directly rather than as a subprocess so that pytest
    filterwarnings options have their intended effect.

--- tests/command_line/test_show_mask_info.py
+++ tests/command_line/test_show_mask_info.py
@@ -1,12 +1,12 @@
 from __future__ import annotations
 
-import procrunner
+from dxtbx.command_line import show_mask_info
 
 
-def test_show_mask_info(dials_data):
+def test_show_mask_info(dials_data, capsys):
     data = dials_data("image_examples", pathlib=True) / "dectris_eiger_master.h5"
 
-    result = procrunner.run(["dxtbx.show_mask_info", data])
-    assert not result.returncode and not result.stderr
-
-    assert b"Module 0 has 637992 masked pixels of 10166590" in result.stdout
+    show_mask_info.run([str(data)])
+    captured = capsys.readouterr()
+    assert not captured.err
+    assert "Module 0 has 637992 masked pixels of 10166590" in captured.out
