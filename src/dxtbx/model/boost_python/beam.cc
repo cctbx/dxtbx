3eb3be6d42555fd112f0140dbb504ea478d0ea92
Nicholas Devenish <ndevenish@gmail.com>
2023-03-03 14:31:22 +0000

    Move Beam declarations into child namespace

--- src/dxtbx/model/boost_python/beam.cc
+++ src/dxtbx/model/boost_python/beam.cc
@@ -19,162 +19,164 @@
 #include <scitbx/array_family/boost_python/flex_wrapper.h>
 
 namespace dxtbx { namespace model { namespace boost_python {
+  namespace beam_detail {
 
-  using namespace boost::python;
-  using scitbx::deg_as_rad;
-  using scitbx::rad_as_deg;
+    using namespace boost::python;
+    using scitbx::deg_as_rad;
+    using scitbx::rad_as_deg;
 
-  std::string beam_to_string(const Beam &beam) {
-    std::stringstream ss;
-    ss << beam;
-    return ss.str();
-  }
-
-  struct BeamPickleSuite : boost::python::pickle_suite {
-    static boost::python::tuple getinitargs(const Beam &obj) {
-      return boost::python::make_tuple(obj.get_sample_to_source_direction(),
-                                       obj.get_wavelength(),
-                                       obj.get_divergence(),
-                                       obj.get_sigma_divergence(),
-                                       obj.get_polarization_normal(),
-                                       obj.get_polarization_fraction(),
-                                       obj.get_flux(),
-                                       obj.get_transmission());
+    std::string beam_to_string(const Beam &beam) {
+      std::stringstream ss;
+      ss << beam;
+      return ss.str();
     }
 
-    static boost::python::tuple getstate(boost::python::object obj) {
-      const Beam &beam = boost::python::extract<const Beam &>(obj)();
-      return boost::python::make_tuple(obj.attr("__dict__"),
-                                       beam.get_s0_at_scan_points());
+    struct BeamPickleSuite : boost::python::pickle_suite {
+      static boost::python::tuple getinitargs(const Beam &obj) {
+        return boost::python::make_tuple(obj.get_sample_to_source_direction(),
+                                         obj.get_wavelength(),
+                                         obj.get_divergence(),
+                                         obj.get_sigma_divergence(),
+                                         obj.get_polarization_normal(),
+                                         obj.get_polarization_fraction(),
+                                         obj.get_flux(),
+                                         obj.get_transmission());
+      }
+
+      static boost::python::tuple getstate(boost::python::object obj) {
+        const Beam &beam = boost::python::extract<const Beam &>(obj)();
+        return boost::python::make_tuple(obj.attr("__dict__"),
+                                         beam.get_s0_at_scan_points());
+      }
+
+      static void setstate(boost::python::object obj, boost::python::tuple state) {
+        Beam &beam = boost::python::extract<Beam &>(obj)();
+        DXTBX_ASSERT(boost::python::len(state) == 2);
+
+        // restore the object's __dict__
+        boost::python::dict d =
+          boost::python::extract<boost::python::dict>(obj.attr("__dict__"))();
+        d.update(state[0]);
+
+        // restore the internal state of the C++ object
+        scitbx::af::const_ref<vec3<double> > s0_list =
+          boost::python::extract<scitbx::af::const_ref<vec3<double> > >(state[1]);
+        beam.set_s0_at_scan_points(s0_list);
+      }
+
+      static bool getstate_manages_dict() {
+        return true;
+      }
+    };
+
+    static Beam *make_beam(vec3<double> sample_to_source,
+                           double wavelength,
+                           double divergence,
+                           double sigma_divergence,
+                           bool deg) {
+      Beam *beam = NULL;
+      if (deg) {
+        beam = new Beam(sample_to_source,
+                        wavelength,
+                        deg_as_rad(divergence),
+                        deg_as_rad(sigma_divergence));
+      } else {
+        beam = new Beam(sample_to_source, wavelength, divergence, sigma_divergence);
+      }
+      return beam;
     }
 
-    static void setstate(boost::python::object obj, boost::python::tuple state) {
-      Beam &beam = boost::python::extract<Beam &>(obj)();
-      DXTBX_ASSERT(boost::python::len(state) == 2);
-
-      // restore the object's __dict__
-      boost::python::dict d =
-        boost::python::extract<boost::python::dict>(obj.attr("__dict__"))();
-      d.update(state[0]);
-
-      // restore the internal state of the C++ object
-      scitbx::af::const_ref<vec3<double> > s0_list =
-        boost::python::extract<scitbx::af::const_ref<vec3<double> > >(state[1]);
-      beam.set_s0_at_scan_points(s0_list);
+    static Beam *make_beam_w_s0(vec3<double> s0,
+                                double divergence,
+                                double sigma_divergence,
+                                bool deg) {
+      Beam *beam = NULL;
+      if (deg) {
+        beam = new Beam(s0, deg_as_rad(divergence), deg_as_rad(sigma_divergence));
+      } else {
+        beam = new Beam(s0, divergence, sigma_divergence);
+      }
+      return beam;
     }
 
-    static bool getstate_manages_dict() {
-      return true;
+    static Beam *make_beam_w_all(vec3<double> sample_to_source,
+                                 double wavelength,
+                                 double divergence,
+                                 double sigma_divergence,
+                                 vec3<double> polarization_normal,
+                                 double polarization_fraction,
+                                 double flux,
+                                 double transmission,
+                                 bool deg) {
+      Beam *beam = NULL;
+      if (deg) {
+        beam = new Beam(sample_to_source,
+                        wavelength,
+                        deg_as_rad(divergence),
+                        deg_as_rad(sigma_divergence),
+                        polarization_normal,
+                        polarization_fraction,
+                        flux,
+                        transmission);
+      } else {
+        beam = new Beam(sample_to_source,
+                        wavelength,
+                        divergence,
+                        sigma_divergence,
+                        polarization_normal,
+                        polarization_fraction,
+                        flux,
+                        transmission);
+      }
+      return beam;
     }
-  };
 
-  static Beam *make_beam(vec3<double> sample_to_source,
-                         double wavelength,
-                         double divergence,
-                         double sigma_divergence,
-                         bool deg) {
-    Beam *beam = NULL;
-    if (deg) {
-      beam = new Beam(sample_to_source,
-                      wavelength,
-                      deg_as_rad(divergence),
-                      deg_as_rad(sigma_divergence));
-    } else {
-      beam = new Beam(sample_to_source, wavelength, divergence, sigma_divergence);
+    static double get_divergence(const Beam &beam, bool deg) {
+      double divergence = beam.get_divergence();
+      return deg ? rad_as_deg(divergence) : divergence;
     }
-    return beam;
-  }
 
-  static Beam *make_beam_w_s0(vec3<double> s0,
-                              double divergence,
-                              double sigma_divergence,
-                              bool deg) {
-    Beam *beam = NULL;
-    if (deg) {
-      beam = new Beam(s0, deg_as_rad(divergence), deg_as_rad(sigma_divergence));
-    } else {
-      beam = new Beam(s0, divergence, sigma_divergence);
+    static double get_sigma_divergence(const Beam &beam, bool deg) {
+      double sigma_divergence = beam.get_sigma_divergence();
+      return deg ? rad_as_deg(sigma_divergence) : sigma_divergence;
     }
-    return beam;
-  }
 
-  static Beam *make_beam_w_all(vec3<double> sample_to_source,
-                               double wavelength,
-                               double divergence,
-                               double sigma_divergence,
-                               vec3<double> polarization_normal,
-                               double polarization_fraction,
-                               double flux,
-                               double transmission,
-                               bool deg) {
-    Beam *beam = NULL;
-    if (deg) {
-      beam = new Beam(sample_to_source,
-                      wavelength,
-                      deg_as_rad(divergence),
-                      deg_as_rad(sigma_divergence),
-                      polarization_normal,
-                      polarization_fraction,
-                      flux,
-                      transmission);
-    } else {
-      beam = new Beam(sample_to_source,
-                      wavelength,
-                      divergence,
-                      sigma_divergence,
-                      polarization_normal,
-                      polarization_fraction,
-                      flux,
-                      transmission);
+    static void set_divergence(Beam &beam, double divergence, bool deg) {
+      beam.set_divergence(deg ? deg_as_rad(divergence) : divergence);
     }
-    return beam;
-  }
 
-  static double get_divergence(const Beam &beam, bool deg) {
-    double divergence = beam.get_divergence();
-    return deg ? rad_as_deg(divergence) : divergence;
-  }
-
-  static double get_sigma_divergence(const Beam &beam, bool deg) {
-    double sigma_divergence = beam.get_sigma_divergence();
-    return deg ? rad_as_deg(sigma_divergence) : sigma_divergence;
-  }
-
-  static void set_divergence(Beam &beam, double divergence, bool deg) {
-    beam.set_divergence(deg ? deg_as_rad(divergence) : divergence);
-  }
-
-  static void set_sigma_divergence(Beam &beam, double sigma_divergence, bool deg) {
-    beam.set_sigma_divergence(deg ? deg_as_rad(sigma_divergence) : sigma_divergence);
-  }
-
-  static void rotate_around_origin(Beam &beam,
-                                   vec3<double> axis,
-                                   double angle,
-                                   bool deg) {
-    double angle_rad = deg ? deg_as_rad(angle) : angle;
-    beam.rotate_around_origin(axis, angle_rad);
-  }
-
-  static void Beam_set_s0_at_scan_points_from_tuple(Beam &beam,
-                                                    boost::python::tuple l) {
-    scitbx::af::shared<vec3<double> > s0_list;
-    for (std::size_t i = 0; i < boost::python::len(l); ++i) {
-      vec3<double> s0 = boost::python::extract<vec3<double> >(l[i]);
-      s0_list.push_back(s0);
+    static void set_sigma_divergence(Beam &beam, double sigma_divergence, bool deg) {
+      beam.set_sigma_divergence(deg ? deg_as_rad(sigma_divergence) : sigma_divergence);
     }
-    beam.set_s0_at_scan_points(s0_list.const_ref());
-  }
 
-  static void Beam_set_s0_at_scan_points_from_list(Beam &beam, boost::python::list l) {
-    scitbx::af::shared<vec3<double> > s0_list;
-    for (std::size_t i = 0; i < boost::python::len(l); ++i) {
-      vec3<double> s0 = boost::python::extract<vec3<double> >(l[i]);
-      s0_list.push_back(s0);
+    static void rotate_around_origin(Beam &beam,
+                                     vec3<double> axis,
+                                     double angle,
+                                     bool deg) {
+      double angle_rad = deg ? deg_as_rad(angle) : angle;
+      beam.rotate_around_origin(axis, angle_rad);
     }
-    beam.set_s0_at_scan_points(s0_list.const_ref());
-  }
 
+    static void Beam_set_s0_at_scan_points_from_tuple(Beam &beam,
+                                                      boost::python::tuple l) {
+      scitbx::af::shared<vec3<double> > s0_list;
+      for (std::size_t i = 0; i < boost::python::len(l); ++i) {
+        vec3<double> s0 = boost::python::extract<vec3<double> >(l[i]);
+        s0_list.push_back(s0);
+      }
+      beam.set_s0_at_scan_points(s0_list.const_ref());
+    }
+
+    static void Beam_set_s0_at_scan_points_from_list(Beam &beam,
+                                                     boost::python::list l) {
+      scitbx::af::shared<vec3<double> > s0_list;
+      for (std::size_t i = 0; i < boost::python::len(l); ++i) {
+        vec3<double> s0 = boost::python::extract<vec3<double> >(l[i]);
+        s0_list.push_back(s0);
+      }
+      beam.set_s0_at_scan_points(s0_list.const_ref());
+    }
+  }  // namespace beam_detail
   template <>
   boost::python::dict to_dict<Beam>(const Beam &obj) {
     boost::python::dict result;
@@ -214,13 +216,14 @@ namespace dxtbx { namespace model { namespace boost_python {
     if (obj.has_key("s0_at_scan_points")) {
       boost::python::list s0_at_scan_points =
         boost::python::extract<boost::python::list>(obj["s0_at_scan_points"]);
-      Beam_set_s0_at_scan_points_from_list(*b, s0_at_scan_points);
+      beam_detail::Beam_set_s0_at_scan_points_from_list(*b, s0_at_scan_points);
     }
     return b;
   }
 
   void export_beam() {
-    // Export BeamBase
+    using namespace beam_detail;
+
     class_<BeamBase, boost::noncopyable>("BeamBase", no_init)
       .def("get_sample_to_source_direction", &BeamBase::get_sample_to_source_direction)
       .def("set_direction", &BeamBase::set_direction)
