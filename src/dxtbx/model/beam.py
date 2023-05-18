9b56211e4ffa72460f5369421b9377c94ff6ca44
davidmcdonagh <davidnmcdonagh@gmail.com>
2023-05-18 14:01:31 +0100

    Corrected remaining references to PolyBeam. Updated newsfragment. Tidied BeamFactory.from_dict logic.

--- src/dxtbx/model/beam.py
+++ src/dxtbx/model/beam.py
@@ -130,7 +130,7 @@ class BeamFactory:
         joint.update(dict)
 
         # Create the model from the joint dictionary
-        if "__id__" in joint and joint["__id__"] == "polychromatic":
+        if joint.get("__id__", "") == "polychromatic":
             return PolychromaticBeam.from_dict(joint)
         return Beam.from_dict(joint)
 
