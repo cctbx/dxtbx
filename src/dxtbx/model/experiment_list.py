71d4ec04c95739c061d12b551d9cb9f86811134f
Nicholas Devenish <ndevenish@gmail.com>
2023-06-29 22:24:38 +0100

    Use importlib instead of pkg_resources (#643)
    pkg_resources is now deprecated, as importlib has covered
    most use-cases since python 3.8. This removes the code
    causing warnings in downstream tests.
    
    Also suppress numpy 'product' warnings when running pytest.

--- src/dxtbx/model/experiment_list.py
+++ src/dxtbx/model/experiment_list.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import collections
 import copy
 import errno
+import importlib.metadata
 import itertools
 import json
 import logging
@@ -10,8 +11,6 @@ import operator
 import os
 import pickle
 from typing import Any, Callable, Generator, Iterable
-
-import pkg_resources
 
 import dxtbx.datablock
 from dxtbx.datablock import (
@@ -323,7 +322,6 @@ class ExperimentListDict:
         # a sensible experiment.
         el = ExperimentList()
         for eobj in self._obj["experiment"]:
-
             # Get the models
             identifier = eobj.get("identifier", "")
             beam = self._lookup_model("beam", eobj)
@@ -467,7 +465,7 @@ class ExperimentListDict:
     @staticmethod
     def _scaling_model_from_dict(obj):
         """Get the scaling model from a dictionary."""
-        for entry_point in pkg_resources.iter_entry_points("dxtbx.scaling_model_ext"):
+        for entry_point in importlib.metadata.entry_points()["dxtbx.scaling_model_ext"]:
             if entry_point.name == obj["__id__"]:
                 return entry_point.load().from_dict(obj)
 
