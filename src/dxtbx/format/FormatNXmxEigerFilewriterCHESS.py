da798db40b26ee7b8355dbda522e439bf84604c0
Steve Meisburger <spmeisburger@gmail.com>
2024-02-07 14:43:24 -0500

    Update FormatNXmxEigerFilewriterCHESS.py
    PR #676 resolved the image dimension issue with Eiger Firmware Upgrade in parent class (FormatNXmxEigerFilewriter). Redundant code removed -- now, the custom format just sets the goniometer axis flip. Unfortunately, goniometer axis metadata is not yet supported by the Dectris Simplon API.

--- src/dxtbx/format/FormatNXmxEigerFilewriterCHESS.py
+++ src/dxtbx/format/FormatNXmxEigerFilewriterCHESS.py
@@ -8,9 +8,6 @@ from packaging import version
 
 from dxtbx.format.FormatNXmxEigerFilewriter import FormatNXmxEigerFilewriter
 
-DATA_FILE_RE = re.compile(r"data_\d{6}")
-
-
 class FormatNXmxEigerFilewriterCHESS(FormatNXmxEigerFilewriter):
     _cached_file_handle = None
 
@@ -24,26 +21,6 @@ class FormatNXmxEigerFilewriterCHESS(FormatNXmxEigerFilewriter):
                 ):
                     return True
         return False
-
-    def _get_nxmx(self, fh: h5py.File):
-        nxmx_obj = nxmx.NXmx(fh)
-        nxentry = nxmx_obj.entries[0]
-
-        nxdetector = nxentry.instruments[0].detectors[0]
-        if nxdetector.underload_value is None:
-            nxdetector.underload_value = 0
-
-        # older firmware versions had the detector dimensions inverted
-        fw_version_string = (
-            fh["/entry/instrument/detector/detectorSpecific/eiger_fw_version"][()]
-            .decode()
-            .replace("release-", "")
-        )
-
-        if version.parse(fw_version_string) < version.parse("2022.1.2"):
-            for module in nxdetector.modules:
-                module.data_size = module.data_size[::-1]
-        return nxmx_obj
 
     def _goniometer(self):
         return self._goniometer_factory.known_axis((-1, 0, 0))
