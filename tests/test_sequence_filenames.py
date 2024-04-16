6303168a4c00b700dce5fbb1ec1dfa1c91a21b6d
David Waterman <dgwaterman@gmail.com>
2024-04-16 16:46:48 +0100

    Add tests for locate_files_matching_template_string, which exercises all the added code

--- tests/test_sequence_filenames.py
+++ tests/test_sequence_filenames.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 
+import shutil
+
 import pytest
 
-from dxtbx.sequence_filenames import template_regex
+from dxtbx.sequence_filenames import template_image_range, template_regex
 
 
 @pytest.mark.parametrize(
@@ -20,3 +22,21 @@ from dxtbx.sequence_filenames import template_regex
 )
 def test_template_regex(filename, template, digits):
     assert template_regex(filename) == (template, digits)
+
+
+def test_template_image_range(dials_data):
+    template = str(dials_data("insulin", pathlib=True) / "insulin_1_###.img")
+    assert template_image_range(template) == (1, 45)
+
+
+def test_template_image_range_non_zero_padded(dials_data, tmp_path):
+    images = sorted(dials_data("insulin", pathlib=True).glob("insulin_1_0[0-1]*"))
+    # symlink if possible, copy if necessary
+    for i, image in enumerate(images):
+        try:
+            (tmp_path / f"insulin_1_{i + 1}.img").symlink_to(image)
+        except OSError:
+            shutil.copy(image, (tmp_path / f"insulin_1_{i + 1}.img"))
+
+    template = str(tmp_path / "insulin_1_#.img")
+    assert template_image_range(template) == (1, 19)
