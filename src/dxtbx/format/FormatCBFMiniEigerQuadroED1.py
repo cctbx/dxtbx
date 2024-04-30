89f78b1cdbf91ef4b8e6126aa54db5c7fb953299
David Waterman <dgwaterman@gmail.com>
2024-04-30 10:18:18 +0100

    Gain is not well known, so reset to 1.0

--- src/dxtbx/format/FormatCBFMiniEigerQuadroED1.py
+++ src/dxtbx/format/FormatCBFMiniEigerQuadroED1.py
@@ -131,11 +131,12 @@ class FormatCBFMiniEigerQuadroED1(FormatCBFMiniEiger):
             mask=[],
         )
 
-        # Here we set specifics, notably gain=3, also parallax correction and
+        # Here we set specifics: parallax correction and
         # QE correction are effectively disabled by setting the simple
-        # pixel-to-millimetre strategy and a very high mu value.
+        # pixel-to-millimetre strategy and a very high mu value. Gain is not
+        # precisely known. We think it might be around 1.7, but here will leave
+        # as 1.0 and expect the user to override at import.
         for panel in detector:
-            panel.set_gain(3)
             panel.set_thickness(thickness)
             panel.set_material(material)
             panel.set_identifier(identifier)
