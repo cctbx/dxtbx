88c7db30652aa15249e3837de8ff7db1f671caa5
DiamondLightSource-build-server <DiamondLightSource-build-server@users.noreply.github.com>
2024-05-12 05:10:21 +0100

    Clean Clutter

--- src/dxtbx/command_line/any2nexus.py
+++ src/dxtbx/command_line/any2nexus.py
@@ -1,5 +1,8 @@
-from dxtbx.format.nxmx_writer import run
+from __future__ import annotations
+
 import sys
+
+from dxtbx.format.nxmx_writer import run
 
 if __name__ == "__main__":
     run(sys.argv[1:])
