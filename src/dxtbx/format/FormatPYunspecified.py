cc7e6a1cdbd2fbf529a6a612594d02a3c8d6371d
Aaron Brewster <asbrewster@lbl.gov>
2024-07-30 17:31:50 -0700

    Extra checks for reading pickle files
    This allows the registry to ignore dials mask files for these classes

--- src/dxtbx/format/FormatPYunspecified.py
+++ src/dxtbx/format/FormatPYunspecified.py
@@ -27,7 +27,7 @@ class FormatPYunspecified(FormatPY):
             with FormatPYunspecified.open_file(image_file, "rb") as fh:
                 data = pickle.load(fh, encoding="bytes")
                 data = image_dict_to_unicode(data)
-        except OSError:
+        except (AttributeError, OSError):
             return False
 
         headers = set(data)
