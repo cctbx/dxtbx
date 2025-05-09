ad4dfb9b9e8291be04ec4ce4e5bd0c392751fcca
David Waterman <dagewa@users.noreply.github.com>
2025-05-09 19:01:51 +0100

    `dials_regression` no more (#815)
    * Remove `dials_regression`-related fixtures

--- conftest.py
+++ conftest.py
@@ -5,48 +5,9 @@
 
 from __future__ import annotations
 
-import os
-import socket
-
 import pytest
 
 collect_ignore = []
-
-
-def dials_regression_path():
-    """Return the absolute path to the dials_regression module as a string.
-    This function is used directly by tests/test_regression_images.py"""
-
-    if "DIALS_REGRESSION" in os.environ:
-        return os.environ["DIALS_REGRESSION"]
-
-    try:
-        import dials_regression as dr
-
-        return os.path.abspath(os.path.dirname(dr.__file__))
-    except ImportError:
-        pass  # dials_regression not configured
-
-    # Check if we are in a known location
-    reference_copy = (
-        "/dls/science/groups/scisoft/DIALS/repositories/git-reference/dials_regression"
-    )
-    if (
-        os.name == "posix"
-        and socket.gethostname().endswith(".diamond.ac.uk")
-        and os.path.exists(reference_copy)
-    ):
-        return reference_copy
-
-
-@pytest.fixture(scope="session")
-def dials_regression():
-    """Return the absolute path to the dials_regression module as a string.
-    Skip the test if dials_regression is not available."""
-    d_r = dials_regression_path()
-    if d_r:
-        return d_r
-    pytest.skip("dials_regression required for this test")
 
 
 def pytest_addoption(parser):
