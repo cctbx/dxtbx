fe3faa7cf3674213aeb33e4b95e7c95f52b725f8
David Waterman <dgwaterman@gmail.com>
2025-06-12 12:02:23 +0100

    Replace FormatBrukerPhoton, FormatBrukerED1 and FormatBrukerELA with a single Format class, FormatBrukerModern. Most code was shared between these classes anyway.

--- src/dxtbx/format/FormatBrukerModern.py
+++ src/dxtbx/format/FormatBrukerModern.py
@@ -1,10 +1,17 @@
+"""
+Format class for reading images from various non-CCD Bruker detectors, or
+DECTRIS dectors written to Bruker format, avoiding the use of the BrukerImage
+class, which is not suitable for these detectors
+(see https://github.com/cctbx/cctbx_project/issues/65).
+"""
+
 from __future__ import annotations
 
 from boost_adaptbx.boost.python import streambuf
 from scitbx import matrix
 from scitbx.array_family import flex
 
-from dxtbx import IncorrectFormatError
+from dxtbx import IncorrectFormatError, flumpy
 from dxtbx.ext import (
     is_big_endian,
     read_uint8,
@@ -18,7 +25,17 @@ from dxtbx.model import SimplePxMmStrategy
 from dxtbx.model.beam import Probe
 
 
-class FormatBrukerELA(FormatBruker):
+def _convert_int_to_uint32(array):
+    """Convert a flex.int array to a flex.uint32 array, without copying and
+    doing no checking that the values are non-negative. This should only be
+    called when the values are known to be non-negative."""
+
+    array = flumpy.to_numpy(array)
+    array = array.astype("uint32", copy=False)
+    return flumpy.from_numpy(array)
+
+
+class FormatBrukerModern(FormatBruker):
     @staticmethod
     def understand(image_file):
         try:
@@ -28,14 +45,13 @@ class FormatBrukerELA(FormatBruker):
 
         header_dic = FormatBruker.parse_header(header_lines)
 
-        dettype = header_dic.get("DETTYPE").upper()
-        if dettype is None:
-            return False
-
-        if "DECTRIS-ELA" not in dettype:
-            return False
-
-        return True
+        # Support Photon II/III, Eiger, Quadro and ELA detectors. I have not seen
+        # images from the Photon IV, so don't attempt to support it yet.
+        accepted_detectors = ["CMOS-PHOTONII", "EIGER", "QUADRO", "DECTRIS-ELA"]
+        for det in accepted_detectors:
+            if header_dic.get("DETTYPE", "").upper().startswith(det):
+                return True
+        return False
 
     def _start(self):
         try:
@@ -43,13 +59,7 @@ class FormatBrukerELA(FormatBruker):
         except OSError:
             return False
 
-        self.header_dict = FormatBrukerELA.parse_header(header_lines)
-
-        # The ELA format can't currently use BrukerImage, for the same reason
-        # as for the Photon-II/III (hardcoded image size is one factor)
-        # https://github.com/cctbx/cctbx_project/issues/65
-        # from iotbx.detectors.bruker import BrukerImage
-        # self.detectorbase = BrukerImage(self._image_file)
+        self.header_dict = FormatBrukerModern.parse_header(header_lines)
 
     def _goniometer(self):
         # goniometer angles in ANGLES are 2-theta, omega, phi, chi (FIXED)
@@ -64,8 +74,8 @@ class FormatBrukerELA(FormatBruker):
         if scan_axis is None:
             scan_axis = 2  # "OMEGA" default
 
-        # Axes here may be incorrect
-        axes = flex.vec3_double(((1, 0, 0), (0, 0, -1), (0, 1, 0)))
+        # https://journals.iucr.org/d/issues/2014/10/00/dz5309/dz5309sup1.pdf
+        axes = flex.vec3_double(((0, -1, 0), (0, 0, 1), (0, 1, 0)))
         omega -= 180
         angles = flex.double((phi, chi, omega))
 
@@ -73,21 +83,23 @@ class FormatBrukerELA(FormatBruker):
             axes, angles, names, scan_axis
         )
 
-        # It is preferable to return a single axis goniometer, as this can be
+        # For electron diffraction (assumed if wavelength < 0.1 Angstrom)
+        # it is preferable to return a single axis goniometer, as this can be
         # further optimised by dials.find_rotation_axis
-        return self._goniometer_factory.known_axis(g.get_rotation_axis())
+        wavelength = float(self.header_dic["WAVELEN"].split()[0])
+        if wavelength < 0.1:
+            g = self._goniometer_factory.known_axis(g.get_rotation_axis())
+
+        return g
 
     def _calculate_gain(self, wavelength):
-        """The CCDPARM header item contains 5 items. For an X-ray detector
-        are described by:
+        """The CCDPARM header item contains 5 items:
             1. readnoise
             2. e/ADU
             3. e/photon
             4. bias
             5. full scale
-        and the gain in ADU/X-ray is given by (e/photon) / (e/ADU).
-        It is not exactly clear what the gain is for this electron detector, but
-        these files seem to set it to 1
+        The gain in ADU/X-ray is given by (e/photon) / (e/ADU).
         """
         ccdparm = self.header_dict["CCDPARM"].split()
         e_ADU = float(ccdparm[1])
@@ -95,6 +107,13 @@ class FormatBrukerELA(FormatBruker):
         if e_ADU == 0:
             return 1.0
         gain = e_photon / e_ADU
+
+        # For the ED-1 QUADRO, gain should be 3.0 exactly, but some old files have
+        # this inverted. In the case of exactly 1/3 gain, invert it back.
+        detector_type = self.header_dict["DETTYPE"].upper()
+        if "EIGER" in detector_type or "QUADRO" in detector_type:
+            if gain == 1.0 / 3.0:
+                gain = 3.0
         return gain
 
     def _detector(self):
@@ -105,10 +124,22 @@ class FormatBrukerELA(FormatBruker):
         full_scale = float(self.header_dict["CCDPARM"].split()[-1])
         min_trusted_value = 0
 
-        fast = matrix.col((1, 0, 0))
-        slow = matrix.col((0, -1, 0))
-        beam = matrix.col((0, 0, 1))
-        pixel_mm = 0.075
+        # Here we get some detector specifics. It is unclear whether these are
+        # correct, or if we could read these value properly from the header.
+        if "CMOS-PHOTONII" in self.header_dict["DETTYPE"].upper():
+            fast = matrix.col((1, 0, 0))
+            slow = matrix.col((0, 1, 0))
+            beam = matrix.col((0, 0, 1))
+            pixel_mm = 5.0 / float(self.header_dict["DETTYPE"].split()[1])
+            # Not a CCD, but is an integrating detector. Photon II has a 90 um Gadox
+            # scintillator.
+            type_str = "CCD"
+        else:  # EIGER-type
+            fast = matrix.col((1, 0, 0))
+            slow = matrix.col((0, -1, 0))
+            beam = matrix.col((0, 0, 1))
+            pixel_mm = 0.075
+            type_str = "PAD"
         beam_pixel = [float(bp) for bp in self.header_dict["CENTER"].split()[:-3:-1]]
         distance_mm = 10.0 * float(self.header_dict["DISTANC"].split()[1])
         origin = (
@@ -128,21 +159,22 @@ class FormatBrukerELA(FormatBruker):
 
         gain = self._calculate_gain(float(self.header_dict["WAVELEN"].split()[0]))
         detector = self._detector_factory.complex(
-            "PAD",
+            type_str,
             origin.elems,
             fast.elems,
             slow.elems,
             pixel_size,
             image_size,
             (min_trusted_value, full_scale),
+            gain=gain,
         )
 
-        # Here we set specifics, notably parallax correction and
-        # QE correction are effectively disabled by setting the simple
+        # For the electron diffraction case (assumed if wavelength < 0.1 Angstrom)
+        # effectively disable parallax and QE corrections by setting the simple
         # pixel-to-millimetre strategy and a very high mu value.
         for panel in detector:
             panel.set_gain(gain)
-            panel.set_thickness(0.450)  # Assume same as Eiger
+            panel.set_thickness(0.450)
             panel.set_material("Si")
             panel.set_px_mm_strategy(SimplePxMmStrategy())
             panel.set_mu(1e10)
@@ -150,15 +182,18 @@ class FormatBrukerELA(FormatBruker):
         return detector
 
     def _beam(self):
-        """Make unpolarized beam"""
+        """Make unpolarized beam (home source, or microscope)"""
         wavelength = float(self.header_dict["WAVELEN"].split()[0])
-
+        if wavelength < 0.1:
+            probe = Probe.electron
+        else:
+            probe = Probe.xray
         return self._beam_factory.make_polarized_beam(
             sample_to_source=(0.0, 0.0, 1.0),
             wavelength=wavelength,
             polarization=(0, 1, 0),
             polarization_fraction=0.5,
-            probe=Probe.electron,
+            probe=probe,
         )
 
     def _scan(self):
@@ -185,7 +220,9 @@ class FormatBrukerELA(FormatBruker):
         # the understand method. Otherwise the user gets FormatBruker reading the
         # image improperly but without failing
         if self.header_dict["FORMAT"] != "100":
-            raise NotImplementedError("Only FORMAT 100 images are currently supported")
+            raise NotImplementedError(
+                "Only FORMAT 100 images from the Photon II are currently supported"
+            )
 
         f = self.open_file(self._image_file, "rb")
         header_size = int(self.header_dict["HDRBLKS"]) * 512
@@ -214,7 +251,7 @@ class FormatBrukerELA(FormatBruker):
         nrows = int(self.header_dict["NROWS"].split()[0])
         ncols = int(self.header_dict["NCOLS"].split()[0])
 
-        raw_data = read_data(streambuf(f), nrows * ncols)
+        raw_data = _convert_int_to_uint32(read_data(streambuf(f), nrows * ncols))
 
         image_size = (nrows, ncols)
         raw_data.reshape(flex.grid(*image_size))
@@ -227,7 +264,9 @@ class FormatBrukerELA(FormatBruker):
         if num_underflows > 0:
             # stored values are padded to a multiple of 16 bytes
             nbytes = num_underflows + 15 & ~(15)
-            underflow_vals = read_uint8(streambuf(f), nbytes)[:num_underflows]
+            underflow_vals = _convert_int_to_uint32(
+                read_uint8(streambuf(f), nbytes)[:num_underflows]
+            )
         else:
             underflow_vals = None
 
@@ -235,8 +274,10 @@ class FormatBrukerELA(FormatBruker):
         if num_2b_overflows > 0:
             # stored values are padded to a multiple of 16 bytes
             nbytes = num_2b_overflows * 2 + 15 & ~(15)
-            overflow_vals = read_2b(streambuf(f), nbytes // 2)[:num_2b_overflows]
-            overflow = flex.int(nrows * ncols, 0)
+            overflow_vals = _convert_int_to_uint32(
+                read_2b(streambuf(f), nbytes // 2)[:num_2b_overflows]
+            )
+            overflow = flex.uint32(nrows * ncols, 0)
             sel = (raw_data == 255).as_1d()
             overflow.set_selected(sel, overflow_vals - 255)
             overflow.reshape(flex.grid(*image_size))
@@ -247,7 +288,7 @@ class FormatBrukerELA(FormatBruker):
             # stored values are padded to a multiple of 16 bytes
             nbytes = num_4b_overflows * 4 + 15 & ~(15)
             overflow_vals = read_4b(streambuf(f), nbytes // 4)[:num_4b_overflows]
-            overflow = flex.int(nrows * ncols, 0)
+            overflow = flex.uint32(nrows * ncols, 0)
             sel = (raw_data == 65535).as_1d()
             overflow.set_selected(sel, overflow_vals - 65535)
             overflow.reshape(flex.grid(*image_size))
@@ -256,7 +297,7 @@ class FormatBrukerELA(FormatBruker):
         # handle underflows
         if underflow_vals is not None:
             sel = (raw_data == 0).as_1d()
-            underflow = flex.int(nrows * ncols, 0)
+            underflow = flex.uint32(nrows * ncols, 0)
             underflow.set_selected(sel, underflow_vals)
             underflow.reshape(flex.grid(*image_size))
             raw_data += underflow
