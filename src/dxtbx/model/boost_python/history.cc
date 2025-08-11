43dfd368da4389dc78e0450e802c047a26339538
David Waterman <dagewa@users.noreply.github.com>
2025-08-11 22:48:36 +0100

    Record history in experiment, consolidate on experiment list write (#816)
    * Working towards History as an object that can be shared between experiments
    
    * Make History pickleable
    
    * Add History to Experiment as a shareable object
    
    * Function to get unique set of history objects in the experiment list
    
    * Add append_history_item function that controls the format of the history
    string, and includes a UTC timestamp
    
    * bug fix
    
    * Add functions to (de)serialize and consolidate history.
    
    At the moment, consolidation is just sorting by timestamp. We could
    start combining history items though for cases including parallel
    file writes (dials.stills_process)
    
    * Change constructor to require history lines
    
    * Test for history
    
    * tidying
    
    * Add a type annotation and a docstring
    
    * News
    
    * Rename newsfragments/xxx.feature to newsfragments/816.feature
    
    * Bugfix for experiment lists with zero length history
    
    * Tidy up consolidation of histories
    
    This is better as a method of the ExperimentList class, providing
    an easier interface to history management. When you want to append a
    new history line to each experiment, just call el.consolidate_histories(),
    which gives you the now unique History object attached to each experiment
    and append the line to that object.
    
    * Missed line in SConscript
    
    * Fix issue when an ExperimentList is saved in an interactive session
    
    * Fix idiotic error in 7f1f770aa
    
    * Changes based on @phyy-nx's suggestion to:
    
    - correctly get dispatcher names for libtbx builds
    - get script name when run as main
    
    ---------
    
    Co-authored-by: DiamondLightSource-build-server <DiamondLightSource-build-server@users.noreply.github.com>

---
+++ src/dxtbx/model/boost_python/history.cc
@@ -0,0 +1,43 @@
+#include <boost/python.hpp>
+#include <boost/python/def.hpp>
+#include <boost/python/args.hpp>
+#include <dxtbx/model/history.h>
+#include <dxtbx/error.h>
+
+namespace dxtbx { namespace model { namespace boost_python {
+
+  struct HistoryPickleSuite : boost::python::pickle_suite {
+    static boost::python::tuple getstate(boost::python::object obj) {
+      const History &history = boost::python::extract<const History &>(obj)();
+      return boost::python::make_tuple(obj.attr("__dict__"),
+                                       history.get_history_as_list());
+    }
+
+    static void setstate(boost::python::object obj, boost::python::tuple state) {
+      History &history = boost::python::extract<History &>(obj)();
+      DXTBX_ASSERT(boost::python::len(state) == 2);
+
+      // restore the object's __dict__
+      boost::python::dict d =
+        boost::python::extract<boost::python::dict>(obj.attr("__dict__"))();
+      d.update(state[0]);
+
+      // restore the internal state of the C++ object
+      history.set_history_from_list(
+        boost::python::extract<boost::python::list>(state[1])());
+    }
+  };
+
+  void export_history() {
+    using boost::python::arg;
+
+    boost::python::class_<History>("History")
+      .def(boost::python::init<const boost::python::list &>())
+      .def("set_history", &History::set_history_from_list)
+      .def("get_history", &History::get_history_as_list)
+      .def("append_history_item",
+           &History::append_history_item,
+           (arg("dispatcher"), arg("version"), arg("flag")))
+      .def_pickle(HistoryPickleSuite());
+  }
+}}}  // namespace dxtbx::model::boost_python
